c8443900aeec033b07778e78ead58bf5f1698e5c
Qiming Sun <osirpt.sun@gmail.com>
2014-11-15 15:53:02 -0500

    add dmrgscf

---
+++ future/dmrgscf/__init__.py
@@ -0,0 +1,1 @@
+from dmrgscf import *
---
+++ future/dmrgscf/dmrgci.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+#
+# Author: Sandeep Sharma <sanshar@gmail.com>
+#
+
+from pyscf.fci.direct_spin0 import kernel
+from pyscf.fci.direct_spin0 import make_rdm12
