d0c32a1c33f5bd36f4f2991fbe351e5960fa4913
Qiming Sun <osirpt.sun@gmail.com>
2014-11-15 12:11:23 -0500

    import modules in pyscf.future

--- __init__.py
+++ __init__.py
@@ -5,9 +5,12 @@ pyscf
 
 __version__ = '0.7'
 
+import os
 import gto
 import lib
 import scf
 import ao2mo
 #import symm
 
+# modules in ./future are in test
+__path__.append(os.path.join(os.path.dirname(__file__), 'future'))
