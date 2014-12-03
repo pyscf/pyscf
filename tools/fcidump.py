6dab8b8e9321298a896c3fd1087e0447176c6df6
Qiming Sun <osirpt.sun@gmail.com>
2014-12-03 12:09:06 -0500

    avoid dumping 0 integrals in fcidump

--- tools/fcidump.py
+++ tools/fcidump.py
@@ -15,7 +15,7 @@ def write_head(fout, nmo, nelec, ms=0, orbsym=[]):
     fout.write(' &END\n')
 
 # 4-fold symmetry
-def write_eri(fout, eri, nmo):
+def write_eri(fout, eri, nmo, tol=1e-15):
     npair = nmo*(nmo+1)/2
     if eri.size == npair**2: # 4-fold symmetry
         ij = 0
@@ -24,8 +24,9 @@ def write_eri(fout, eri, nmo):
                 kl = 0
                 for k in range(0, nmo):
                     for l in range(0, k+1):
-                        fout.write(' %.16g %4d %4d %4d %4d\n' \
-                                   % (eri[ij,kl], i+1, j+1, k+1, l+1))
+                        if abs(eri[ij,kl]) > tol:
+                            fout.write(' %.16g %4d %4d %4d %4d\n' \
+                                       % (eri[ij,kl], i+1, j+1, k+1, l+1))
                         kl += 1
                 ij += 1
     else:
@@ -37,20 +38,22 @@ def write_eri(fout, eri, nmo):
                 for k in range(0, i+1):
                     for l in range(0, k+1):
                         if ij >= kl:
-                            fout.write(' %.16g %4d %4d %4d %4d\n' \
-                                       % (eri[ijkl], i+1, j+1, k+1, l+1))
+                            if abs(eri[ijkl]) > tol:
+                                fout.write(' %.16g %4d %4d %4d %4d\n' \
+                                           % (eri[ijkl], i+1, j+1, k+1, l+1))
                             ijkl += 1
                         kl += 1
                 ij += 1
 
-def write_hcore(fout, h, nmo):
+def write_hcore(fout, h, nmo, tol=1e-15):
     h = h.reshape(nmo,nmo)
     for i in range(nmo):
         for j in range(0, i+1):
-            fout.write(' %.16g %4d %4d  0  0\n' % (h[i,j], i+1, j+1))
+            if abs(h[i,j]) > tol:
+                fout.write(' %.16g %4d %4d  0  0\n' % (h[i,j], i+1, j+1))
 
 
-def from_chkfile(output, chkfile):
+def from_chkfile(output, chkfile, tol=1e-15):
     import pyscf.scf
     import pyscf.ao2mo
     import pyscf.symm
@@ -66,19 +69,19 @@ def from_chkfile(output, chkfile):
             write_head(fout, nmo, mol.nelectron, mol.spin)
 
         eri = pyscf.ao2mo.direct.full_iofree(mol, mo_coeff, verbose=0)
-        write_eri(fout, pyscf.ao2mo.restore(8, eri, nmo), nmo)
+        write_eri(fout, pyscf.ao2mo.restore(8, eri, nmo), nmo, tol=tol)
 
         t = mol.intor_symmetric('cint1e_kin_sph')
         v = mol.intor_symmetric('cint1e_nuc_sph')
         h = reduce(numpy.dot, (mo_coeff.T, t+v, mo_coeff))
-        write_hcore(fout, h, nmo)
+        write_hcore(fout, h, nmo, tol=tol)
         fout.write(' %.16g  0  0  0  0\n' % mol.nuclear_repulsion())
 
 def from_integrals(output, h1e, h2e, nmo, nelec, nuc=0, ms=0, orbsym=[]):
     with open(output, 'w') as fout:
         write_head(fout, nmo, nelec, ms, orbsym)
-        write_eri(fout, h2e, nmo)
-        write_hcore(fout, h1e, nmo)
+        write_eri(fout, h2e, nmo, tol=tol)
+        write_hcore(fout, h1e, nmo, tol=tol)
         fout.write(' %.16g  0  0  0  0\n' % nuc)
 
 
