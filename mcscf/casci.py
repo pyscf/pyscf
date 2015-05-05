fix bug in mcscf, the rotation of ci vector in natural orbitals

mcscf/casci.py
@@ -6,11 +6,10 @@
 import time
 from functools import reduce
 import numpy
-import scipy.linalg
 import pyscf.lib
 import pyscf.gto
 from pyscf.lib import logger
-import pyscf.scf
+from pyscf import scf
 from pyscf import ao2mo
 from pyscf import fci
 from pyscf.tools.mo_mapping import mo_1to1map
@@ -80,7 +79,8 @@ def analyze(casscf, mo_coeff=None, ci=None, verbose=logger.INFO):
             log.info('beta density matrix (on AO)')
             dump_mat.dump_tri(log.stdout, dm1b, label)
 
-        occ, ucas = scipy.linalg.eigh(-(casdm1a+casdm1b))
+        # note the last two args of ._eig for mc1step_symm
+        occ, ucas = casscf._eig(-(casdm1a+casdm1b), ncore, nocc)
         log.info('Natural occ %s', str(-occ))
         for i, k in enumerate(numpy.argmax(abs(ucas), axis=0)):
             if ucas[k,i] < 0:
@@ -140,19 +140,9 @@ def cas_natorb(mc, mo_coeff=None, ci=None, eris=None, verbose=None):
     nocc = ncore + ncas
     nelecas = mc.nelecas
     casdm1 = mc.fcisolver.make_rdm1(ci, ncas, nelecas)
-    occ, ucas = scipy.linalg.eigh(-casdm1)
+    occ, ucas = mc._eig(-casdm1, ncore, nocc)
     occ = -occ
     log.info('Natural occ %s', str(occ))
-# restore phase
-    for i, k in enumerate(numpy.argmax(abs(ucas), axis=0)):
-        if ucas[k,i] < 0:
-            ucas[:,i] *= -1
-    mo_coeff1 = mo_coeff.copy()
-    mo_coeff1[:,ncore:nocc] = numpy.dot(mo_coeff[:,ncore:nocc], ucas)
-    if log.verbose >= logger.INFO:
-        log.info('Natural orbital in CAS space')
-        label = ['%d%3s %s%-4s' % x for x in mc.mol.spheric_labels()]
-        dump_mat.dump_rec(log.stdout, mo_coeff1[:,ncore:nocc], label, start=1)
 
 # where_natorb gives the location of the natural orbital for the input cas
 # orbitals.  gen_strings4orblist map thes sorted strings (on CAS orbital) to
@@ -173,6 +163,17 @@ def cas_natorb(mc, mo_coeff=None, ci=None, eris=None, verbose=None):
     #old_det_idxb = numpy.argsort(guide_stringsb)
     #ci0 = ci[old_det_idxa[:,None],old_det_idxb]
     ci0 = fci.addons.reorder(ci, nelecas, where_natorb)
+
+# restore phase, to ensure the reordered ci vector is the correct initial guess
+    for i, k in enumerate(where_natorb):
+        if ucas[i,k] < 0:
+            ucas[:,k] *= -1
+    mo_coeff1 = mo_coeff.copy()
+    mo_coeff1[:,ncore:nocc] = numpy.dot(mo_coeff[:,ncore:nocc], ucas)
+    if log.verbose >= logger.INFO:
+        log.info('Natural orbital in CAS space')
+        label = ['%d%3s %s%-4s' % x for x in mc.mol.spheric_labels()]
+        dump_mat.dump_rec(log.stdout, mo_coeff1[:,ncore:nocc], label, start=1)
 
     h1eff =(reduce(numpy.dot, (mo_coeff[:,ncore:nocc].T, mc.get_hcore(),
                                mo_coeff[:,ncore:nocc]))
@@ -334,6 +335,9 @@ class CASCI(object):
         vj, vk = self._scf.get_jk(mol, dm, hermi=hermi)
         return vj - vk * .5
 
+    def _eig(self, h, *args):
+        return scf.hf.eig(h, None)
+
     def get_h2cas(self, mo_coeff=None):
         return self.ao2mo(mo_coeff)
     def get_h2eff(self, mo_coeff=None):
@@ -397,6 +401,10 @@ class CASCI(object):
         log = logger.Logger(self.stdout, self.verbose)
         return analyze(self, mo_coeff, ci, verbose=log)
 
+    def sort_mo(self, caslst, mo_coeff=None, base=1):
+        from pyscf.mcscf import addons
+        if mo_coeff is None: mo_ceff = self.mo_coeff
+        return addons.sort_mo(self, mo_coeff, caslst, base)
 
 
 if __name__ == '__main__':

mcscf/mc1step.py
@@ -583,13 +583,16 @@ def canonicalize(mc, mo_coeff=None, ci=None, eris=None, verbose=logger.NOTE):
     nmo = mo_coeff.shape[1]
     mo_coeff1 = numpy.empty_like(mo_coeff)
     mo_coeff1[:,ncore:nocc] = mo_coeff[:,ncore:nocc]
+# Keep the active space unchanged by default.  The rotation in active space
+# may cause problem for external FCI solver.
     #mo_coeff1, ci, occ = mc.cas_natorb(mo_coeff, ci, eris, verbose)
     fock = mc.get_fock(mo_coeff, ci, eris)
     if ncore > 0:
-        w, c1 = scipy.linalg.eigh(fock[:ncore,:ncore])
+        # note the last two args of ._eig for mc1step_symm
+        w, c1 = mc._eig(fock[:ncore,:ncore], 0, ncore)
         mo_coeff1[:,:ncore] = numpy.dot(mo_coeff[:,:ncore], c1)
     if nmo-nocc > 0:
-        w, c1 = scipy.linalg.eigh(fock[nocc:,nocc:])
+        w, c1 = mc._eig(fock[nocc:,nocc:], nocc, nmo)
         mo_coeff1[:,nocc:] = numpy.dot(mo_coeff[:,nocc:], c1)
 # still return ci coefficients, in case the canonicalization funciton changed
 # cas orbitals, the ci coefficients should also be updated.
@@ -765,6 +768,7 @@ class CASSCF(casci.CASCI):
         log.info('augmented hessian linear dependence = %g', self.ah_lindep)
         log.info('augmented hessian level shift = %d', self.ah_level_shift)
         log.info('diis = %s', self.diis)
+        log.info('chkfile = %s', self.chkfile)
         log.info('max_memory %d MB', self.max_memory)
         try:
             self.fcisolver.dump_flags(self.verbose)
@@ -923,6 +927,11 @@ class CASSCF(casci.CASCI):
             return mc_ao2mo._ERIS(self, mo, 'incore', 0)
         else:
             return mc_ao2mo._ERIS(self, mo, 'incore', 1)
+
+    def get_h2eff(self, mo_coeff=None):
+        return self.get_h2cas(mo_coeff)
+    def get_h2cas(self, mo_coeff=None):
+        return casci.CASCI.ao2mo(self, mo_coeff)
 
     def update_jk_in_ah(self, mo, r, casdm1, eris):
 # J3 = eri_popc * pc + eri_cppo * cp

mcscf/mc1step_symm.py
@@ -4,13 +4,12 @@
 #
 
 import numpy
-import scipy.linalg
 import pyscf.lib.logger as logger
 import pyscf.gto
-import pyscf.scf
 import pyscf.symm
 from pyscf.mcscf import mc1step
 from pyscf.mcscf import mc2step
+from pyscf import scf
 from pyscf import ao2mo
 from pyscf import fci
 from pyscf.tools.mo_mapping import mo_1to1map
@@ -118,64 +117,17 @@ class CASSCF(mc1step.CASSCF):
                 in mc1step.rotate_orb_cc(self, mo, fcasdm1, fcasdm2, eris, verbose):
             yield _symmetrize(u, self.orbsym, self.mol.groupname), g_orb, njk
 
-    def cas_natorb(self, mo_coeff=None, ci=None, eris=None, verbose=None):
-        log = logger.Logger(self.stdout, self.verbose)
-        if mo_coeff is None: mo_coeff = self.mo_coeff
-        if ci is None: ci = self.ci
-        if eris is None: eris = self.ao2mo(mo_coeff)
-        ncore = self.ncore
-        ncas = self.ncas
-        nocc = ncore + ncas
-        nelecas = self.nelecas
-        casdm1 = self.fcisolver.make_rdm1(ci, ncas, nelecas)
-        occ, ucas = _symm_eigh(-casdm1, self.orbsym[ncore:nocc])
-        occ = -occ
-        log.info('Natural occ %s', str(occ))
-# restore phase
-        for i, k in enumerate(numpy.argmax(abs(ucas), axis=0)):
-            if ucas[k,i] < 0:
-                ucas[:,i] *= -1
-        mo_coeff1 = mo_coeff.copy()
-        mo_coeff1[:,ncore:nocc] = numpy.dot(mo_coeff[:,ncore:nocc], ucas)
-
-        where_natorb = mo_1to1map(ucas)
-        ci0 = fci.addons.reorder(ci, nelecas, where_natorb)
-
-        h1eff =(reduce(numpy.dot, (mo_coeff[:,ncore:nocc].T, self.get_hcore(),
-                                   mo_coeff[:,ncore:nocc]))
-              + eris.vhf_c[ncore:nocc,ncore:nocc])
-        h1eff = reduce(numpy.dot, (ucas.T, h1eff, ucas))
-        aaaa = eris.aapp[:,:,ncore:nocc,ncore:nocc].copy()
-        aaaa = ao2mo.incore.full(ao2mo.restore(8, aaaa, ncas), ucas)
-        e_cas, fcivec = self.fcisolver.kernel(h1eff, aaaa, ncas, nelecas, ci0=ci0)
-        log.debug('In Natural orbital, CI energy = %.12g', e_cas)
-        return mo_coeff1, fcivec, occ
-    def cas_natorb_(self, mo_coeff=None, ci=None, eris=None, verbose=None):
-        self.mo_coeff, self.ci, occ = self.cas_natorb(mo_coeff, ci, eris, verbose)
-        return self.ci, self.mo_coeff
-
-    def canonicalize(self, mo_coeff=None, ci=None, eris=None, verbose=None):
-        log = logger.Logger(self.stdout, self.verbose)
-        if mo_coeff is None: mo_coeff = self.mo_coeff
-        if ci is None: ci = self.ci
-        if eris is None: eris = self.ao2mo(mo_coeff)
-        ncore = self.ncore
-        nocc = ncore + self.ncas
-        nmo = mo_coeff.shape[1]
-        mo_coeff1 = numpy.empty_like(mo_coeff)
-        mo_coeff1[:,ncore:nocc] = mo_coeff[:,ncore:nocc]
-        #mo_coeff1, ci, occ = mc.cas_natorb(mo_coeff, ci, eris, verbose)
-        fock = self.get_fock(mo_coeff, ci, eris)
-        if ncore > 0:
-            w, c1 = _symm_eigh(fock[:ncore,:ncore], self.orbsym[:ncore])
-            mo_coeff1[:,:ncore] = numpy.dot(mo_coeff[:,:ncore], c1)
-        if nmo-nocc > 0:
-            w, c1 = _symm_eigh(fock[nocc:,nocc:], self.orbsym[nocc:])
-            mo_coeff1[:,nocc:] = numpy.dot(mo_coeff[:,nocc:], c1)
-        return mo_coeff1, ci
-    def canonicalize_(self, mo_coeff=None, ci=None, eris=None, verbose=None):
-        self.mo_coeff, self.ci = self.canonicalize(self, mo_coeff, ci, verbose=verbose)
-        return self.mo_coeff, self.ci
+    def _eig(self, mat, b0, b1):
+        orbsym = numpy.array(self.orbsym[b0:b1])
+        norb = mat.shape[0]
+        e = numpy.zeros(norb)
+        c = numpy.zeros((norb,norb))
+        for i0 in set(orbsym):
+            lst = numpy.where(orbsym == i0)[0]
+            w, v = scf.hf.eig(mat[lst[:,None],lst], None)
+            e[lst] = w
+            c[lst[:,None],lst] = v
+        return e, c
 
 def _symmetrize(mat, orbsym, groupname, wfnsym=0):
     if wfnsym != 0:
@@ -186,18 +138,6 @@ def _symmetrize(mat, orbsym, groupname, wfnsym=0):
         lst = numpy.where(orbsym == i0)[0]
         mat1[lst[:,None],lst] = mat[lst[:,None],lst]
     return mat1
-
-def _symm_eigh(mat, orbsym):
-    orbsym = numpy.array(orbsym)
-    norb = mat.shape[0]
-    e = numpy.zeros(norb)
-    c = numpy.zeros((norb,norb))
-    for i0 in set(orbsym):
-        lst = numpy.where(orbsym == i0)[0]
-        w, v = scipy.linalg.eigh(mat[lst[:,None],lst])
-        e[lst] = w
-        c[lst[:,None],lst] = v
-    return e, c
 
 
 if __name__ == '__main__':

scf/hf.py
@@ -646,6 +646,17 @@ def mulliken_pop_meta_lowdin_ao(mol, dm, verbose=logger.DEBUG,
     return mulliken_pop(mol, dm, numpy.eye(orth_coeff.shape[0]), log)
 
 
+def eig(h, s):
+    '''Solver for generalized eigenvalue problem
+
+    .. math:: HC = SCE
+    '''
+    e, c = scipy.linalg.eigh(h, s)
+    idx = numpy.argmax(abs(c.real), axis=0)
+    c[:,c[idx,numpy.arange(len(e))].real<0] *= -1
+    return e, c
+
+
 class SCF(object):
     '''SCF base class.   non-relativistic RHF.
 
@@ -766,14 +777,7 @@ class SCF(object):
 
 
     def eig(self, h, s):
-        '''Solver for generalized eigenvalue problem
-
-        .. math:: HC = SCE
-        '''
-        e, c = scipy.linalg.eigh(h, s)
-        idx = numpy.argmax(abs(c.real), axis=0)
-        c[:,c[idx,numpy.arange(len(e))].real<0] *= -1
-        return e, c
+        return eig(h, s)
 
     def get_hcore(self, mol=None):
         if mol is None: mol = self.mol