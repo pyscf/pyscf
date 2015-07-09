0f06530e7123e253cab404047b03bf3efa212713
George Booth <george.booth24@gmail.com>
2015-07-10 00:50:18 +0100

    update test to calculate dipole moments in HF or NO basis

--- future/fciqmcscf/test/test_fciqmc_lih_dipoles.py
+++ future/fciqmcscf/test/test_fciqmc_lih_dipoles.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+
+import unittest
 import numpy
 from pyscf import gto
 from pyscf import scf
@@ -18,13 +20,12 @@ basis = {'H': 'sto-3g', 'Li': 'sto-3g'},
 symmetry = 'C2v'
 )
 
+m = scf.RHF(mol)
+m.conv_tol = 1e-9
+m.scf()
 
 class KnowValues(unittest.TestCase):
     def test_dipoles_hfbasis(self):
-        m = scf.RHF(mol)
-        m.conv_tol = 1e-9
-        m.scf()
-
         fciqmc_calc = fciqmcscf.FCIQMCCI(mol)
         fciqmc_calc.tau = 0.01
         fciqmc_calc.RDMSamples = 2000
@@ -36,16 +37,13 @@ class KnowValues(unittest.TestCase):
         self.assertAlmostEqual(e,-7.787146064428100, 5)
         self.assertAlmostEqual(dips[0],0.0,7)
         self.assertAlmostEqual(dips[1],0.0,7)
-        self.assertAlmostEqual(dips[2],1.85781390006,5)
+        self.assertAlmostEqual(dips[2],1.85781390006,4)
 
     def test_dipoles_casscfbasis(self):
-        m = scf.RHF(mol)
-        m.conv_tol = 1e-9
-        m.scf()
 
-        mc = mcscf.CASSCF(m,5,4)    #There are only 6 orbitals, 4 electrons, so this is close to the whole space. 
-                                    #However, the default behaviour seems to change for the whole space, and returns the mf mos.
-        emc,e_ci,fcivec,casscf_mo = mc.mc2step(m.mo_coeff)[0]
+        mc = mcscf.CASSCF(m,6,4)    #There are only 6 orbitals, 4 electrons, so this is the full space, giving the exact NO basis
+        mc.natorb = True            #Ensures that casscf_mo returns the natural orbital basis in the active space
+        emc,e_ci,fcivec,casscf_mo = mc.mc2step(m.mo_coeff)
 
         fciqmc_calc = fciqmcscf.FCIQMCCI(mol)
         fciqmc_calc.tau = 0.01
@@ -54,6 +52,10 @@ class KnowValues(unittest.TestCase):
         e = fciqmcscf.fciqmc.run_standalone(fciqmc_calc, casscf_mo)   #Run from CASSCF natural orbitals
         dips = fciqmc_calc.dipoles(casscf_mo, fcivec=None, norb=norb, nelec=mol.nelectron)
 
+        self.assertAlmostEqual(e,-7.787146064428100, 5)
+        self.assertAlmostEqual(dips[0],0.0,7)
+        self.assertAlmostEqual(dips[1],0.0,7)
+        self.assertAlmostEqual(dips[2],1.85781390006,4)
 
 if __name__ == "__main__":
     print('Tests for dipole moments from standalone FCIQMC calculation in HF and natural orbital basis')
