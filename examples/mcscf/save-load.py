0f14410b320911b2d52ea671119e151c0f1d419b
Qiming Sun <osirpt.sun@gmail.com>
2015-07-22 10:48:55 -0400

    mcscf attribute changes: max_orb_stepsize -> max_stepsize

--- examples/mcscf/save-load.py
+++ examples/mcscf/save-load.py
@@ -30,7 +30,7 @@ def save_mo_coeff(envs):
         print('Save MO of step %d-%d in file %s' % (imacro+1, imicro+1, fname))
         numpy.save(fname, envs['mo_coeff'])
 mc.callback = save_mo_coeff
-mc.max_orb_stepsize = .01 # max. orbital-rotation angle
+mc.max_stepsize = .01 # max. orbital-rotation angle
 mc.max_cycle_micro = 1    # small value for frequently calling CI solver
 mc.max_cycle_macro = 10
 mc.conv_tol = 1e-4
@@ -38,7 +38,7 @@ e1 = mc.kernel()[0]       # take 1-step CASSCF algorithm by default
 
 mc = mcscf.CASSCF(m, 6, (4,2))
 mc.fcisolver = pyscf.fci.direct_spin1
-mc.max_orb_stepsize = .05
+mc.max_stepsize = .05
 mc.max_cycle_micro = 3
 mc.conv_tol = 1e-8
 mo = numpy.load('mcscf-mo-6-1.npy')
