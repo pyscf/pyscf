Fixed minor bug in test for gamma points (kscf.py) that placed atoms in the wrong place in the gamma point calculation
Replaced incorrect .nbas with .nao_nr() in various places

Fixed comments relating to doubling of basis functions at k=0 (no
longer valid)

pbc/kscf.py
@@ -16,7 +16,7 @@ def get_ovlp(mf, cell, kpts):
     K pt version of get_ovlp
     '''
     nkpts=kpts.shape[0]
-    nao=cell.nbas
+    nao=cell.nao_nr()
     ovlp_kpts=numpy.zeros([nkpts,nao,nao], numpy.complex128)
     for k in range(nkpts):
         kpt=numpy.reshape(kpts[k,:], (3,1))
@@ -39,7 +39,7 @@ def get_j(mf, cell, dm_kpts, kpts):
     nkpts=kpts.shape[0]
 
     ngs=coords.shape[0]
-    nao=cell.nbas
+    nao=cell.nao_nr()
     aoR_kpts=numpy.zeros((nkpts, ngs, nao),numpy.complex128)
     rhoR=numpy.zeros([ngs])
 
@@ -338,7 +338,7 @@ class _KNumInt(pbc._NumInt):
         '''
         nkpts=self.kpts.shape[0]
         ngs=coords.shape[0]
-        nao=mol.nbas
+        nao=mol.nao_nr()
 
         ao_kpts=numpy.empty([nkpts, ngs, nao],numpy.complex128)
         for k in range(nkpts):
@@ -436,7 +436,7 @@ def test_kscf_gamma(atom, ncells):
     
     # place atom in middle of big box
     for i in range(ncells):
-        mol.atom.extend([[atom, (.5+(i+1)*Lunit*B,0.5*Ly*B,0.5*Lz*B)]])
+        mol.atom.extend([[atom, ((.5+i)*Lunit*B,0.5*Ly*B,0.5*Lz*B)]])
 
     # these are some exponents which are 
     # not hard to integrate
@@ -458,7 +458,7 @@ def test_kscf_gamma(atom, ncells):
     cell.build()
     
     # points in grid (x,y,z)
-    gs=np.array([20*ncells,40,40])
+    gs=np.array([40*ncells,40,40])
 
     # Ewald parameters
     precision=1.e-9

pbc/pbc.py
@@ -176,7 +176,7 @@ def get_aoR(cell, coords, kpt=None, isgga=False, relativity=0, bastart=0,
             The real-space grid point coordinates.
 
     Returns:
-        aoR : ([4,] nx*ny*nz, nao=2*cell.nbas) ndarray [nao=cell.nbas if k==0]
+        aoR : ([4,] nx*ny*nz, nao=cell.nao_nr()) ndarray 
             The value of the AO crystal orbitals on the real-space grid. If
             isgga=True, also contains the value of the orbitals gradient in the
             x, y, and z directions.
@@ -194,7 +194,7 @@ def get_aoR(cell, coords, kpt=None, isgga=False, relativity=0, bastart=0,
                   for k in range(-nimgs[2],nimgs[2]+1)
                   if i**2+j**2+k**2 <= 1./3*np.dot(nimgs,nimgs)]
     
-    nao = cell.nbas
+    nao = cell.nao_nr()
     if isgga:
         aoR = np.zeros([4,coords.shape[0], nao], np.complex128)
     else:
@@ -216,7 +216,7 @@ def get_rhoR(mol, ao, dm, non0tab=None,
     Args:
         mol : instance of :class:`Mole` or :class:`Cell`
 
-        ao : ([4,] nx*ny*nz, nao=2*cell.nbas) ndarray [nao=cell.nbas if k==0]
+        ao : ([4,] nx*ny*nz, nao=2*cell.nao_nr()) ndarray 
             The value of the AO crystal orbitals on the real-space grid. If
             isgga=True, also contains the value of the gradient in the x, y,
             and z directions.
@@ -307,7 +307,7 @@ def eval_mat(mol, ao, weight, rho, vrho, vsigma=None, non0tab=None,
     Args:
         mol : instance of :class:`Mole` or :class:`Cell`
 
-        ao : ([4,] nx*ny*nz, nao=2*cell.nbas) ndarray [nao=cell.nbas if k==0]
+        ao : ([4,] nx*ny*nz, nao=cell.nao_nr()) ndarray 
             The value of the AO crystal orbitals on the real-space grid. If
             isgga=True, also contains the value of the gradient in the x, y,
             and z directions.