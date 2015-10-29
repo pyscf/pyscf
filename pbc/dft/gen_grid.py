- Fixed NL pseudopotential sign error - Updated examples to use lda,vwn - Made cell.Gv, cell.vol, cell._h accessible and used throughout

pbc/dft/gen_grid.py
@@ -15,7 +15,7 @@ def gen_uniform_grids(cell):
     ngs = 2*np.asarray(cell.gs)+1
     qv = cartesian_prod([np.arange(x) for x in ngs])
     invN = np.diag(1./ngs)
-    coords = np.dot(qv, np.dot(cell.lattice_vectors(), invN).T)
+    coords = np.dot(qv, np.dot(cell._h, invN).T)
     return coords
 
 class UniformGrids(object):
@@ -33,7 +33,7 @@ class UniformGrids(object):
 
         self.coords = gen_uniform_grids(self.cell)
         self.weights = np.ones(self.coords.shape[0]) 
-        self.weights *= cell.vol()/self.weights.shape[0]
+        self.weights *= cell.vol/self.weights.shape[0]
 
         return self.coords, self.weights
 

pbc/dft/numint.py
@@ -39,7 +39,7 @@ def eval_ao(cell, coords, kpt=None, isgga=False, relativity=0, bastart=0,
         aoR = np.zeros([coords.shape[0], nao], np.complex128)
 
     for T in Ts:
-        L = np.dot(cell.lattice_vectors(), T)
+        L = np.dot(cell._h, T)
         aoR += (np.exp(1j*np.dot(kpt.T,L)) *
                 pyscf.dft.numint.eval_ao(cell, coords-L,
                                          isgga, relativity, 
@@ -48,7 +48,7 @@ def eval_ao(cell, coords, kpt=None, isgga=False, relativity=0, bastart=0,
 
     if cell.ke_cutoff is not None:
 
-        ke = .5* np.einsum('ri,ri->i', cell._Gv, cell._Gv)
+        ke = .5* np.einsum('ri,ri->i', cell.Gv, cell.Gv)
         ke_mask = ke < cell.ke_cutoff
 
         aoG = np.zeros_like(aoR)

pbc/gto/cell.py
@@ -134,7 +134,9 @@ class Cell(pyscf.gto.Mole):
 
 ##################################################
 # don't modify the following variables, they are not input arguments
-        self._Gv = None
+        self.Gv = None
+        self.vol = None
+        self._h = None
         self._pseudo = None
         self._keys = set(self.__dict__.keys())
 
@@ -212,7 +214,9 @@ class Cell(pyscf.gto.Mole):
         if self.ew_eta is None or self.ew_cut is None:
             self.ew_eta, self.ew_cut = self.get_ewald_params(self.precision)
 
-        self._Gv = self.get_Gv()
+        self.Gv = self.get_Gv()
+        self.vol = scipy.linalg.det(self.lattice_vectors())
+        self._h = self.lattice_vectors()
 
     def format_pseudo(self, pseudo_tab):
         return format_pseudo(pseudo_tab)
@@ -322,10 +326,10 @@ class Cell(pyscf.gto.Mole):
                 The structure factor for each atom at each G-vector.
 
         '''
-        ngs = self._Gv.shape[1]
+        ngs = self.Gv.shape[1]
         SI = np.empty([self.natm, ngs], np.complex128)
         for ia in range(self.natm):
-            SI[ia,:] = np.exp(-1j*np.dot(self._Gv.T, self.atom_coord(ia)))
+            SI[ia,:] = np.exp(-1j*np.dot(self.Gv.T, self.atom_coord(ia)))
         return SI
 
     def lattice_vectors(self):
@@ -335,9 +339,6 @@ class Cell(pyscf.gto.Mole):
             return np.asarray(self.h) * (1./param.BOHR)
         else:
             return np.asarray(self.h) * (1./self.unit)
-
-    def vol(self):
-        return scipy.linalg.det(self.lattice_vectors())
 
     def copy(self):
         return copy(self)

pbc/scf/scfint.py
@@ -26,7 +26,7 @@ def get_lattice_Ls(cell, nimgs):
                   for k in range(-nimgs[2],nimgs[2]+1)
                   if i**2+j**2+k**2 <= 1./3*np.dot(nimgs,nimgs)]
     Ts = np.array(Ts)
-    Ls = np.dot(cell.lattice_vectors(), Ts.T).T
+    Ls = np.dot(cell._h, Ts.T).T
     return Ls
 
 def get_hcore(mf, cell, kpt=None):

pbc/tests/test_ke_cutoff.py
@@ -33,7 +33,7 @@ def test_ke_cutoff(pseudo=None):
         cell.build()
         mf = pbcrks.RKS(cell)
 
-        max_ke.append(np.max(.5*np.einsum('ri,ri->i', cell._Gv, cell._Gv)))
+        max_ke.append(np.max(.5*np.einsum('ri,ri->i', cell.Gv, cell.Gv)))
 
         eke_cut.append(mf.scf())
 