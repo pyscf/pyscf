6cc74603c23d6c6a7f34a63172ac11b7461ae90d
Qiming Sun <osirpt.sun@gmail.com>
2015-11-02 08:31:15 -0500

    Add Becke grid

--- pbc/dft/gen_grid.py
+++ pbc/dft/gen_grid.py
@@ -1,4 +1,6 @@
 import numpy as np
+from pyscf import dft
+import pyscf.pbc.scf.scfint
 from pyscf.lib import logger
 from pyscf.lib.numpy_helper import cartesian_prod
 
@@ -45,3 +47,73 @@ class UniformGrids(object):
         self.dump_flags()
         return self.setup_grids()
 
+
+def super_cell(cell, nimgs):
+    Ls = pyscf.pbc.scf.scfint.get_lattice_Ls(cell, nimgs)
+    scell = cell.copy()
+    scell.atom = []
+    for L in Ls:
+        atom1 = []
+        for ia in range(cell.natm):
+            atom1.append([cell._atom[ia][0], cell._atom[ia][1]+L])
+        scell.atom.extend(atom1)
+    scell.unit = 'B'
+    scell.build(False, False, verbose=0)
+    return scell
+
+def gen_becke_grids(cell, atom_grid={}, radi_method=dft.radi.gauss_chebyshev,
+                    level=3, prune_scheme=dft.gen_grid.treutler_prune):
+    '''real-space grids using Becke scheme
+
+    Args:
+        cell : instance of :class:`Cell`
+
+    Returns:
+        coords : (ngx*ngy*ngz, 3) ndarray
+            The real-space grid point coordinates.
+        weights : (ngx*ngy*ngz) ndarray
+    '''
+    scell = super_cell(cell, cell.nimgs)
+    atom_grids_tab = dft.gen_grid.gen_atomic_grids(scell, atom_grid, radi_method,
+                                                   level, prune_scheme)
+    coords, weights = dft.gen_grid.gen_partition(scell, atom_grids_tab)
+
+    # search for grids in unit cell
+    #b1,b2,b3 = np.linalg.inv(h).T  # reciprocal lattice
+    #np.einsum('kj,ij->ki', coords, (b1,b2,b3))
+    c111 = cell._h[:,0] + cell._h[:,1] + cell._h[:,2]
+    d1 = np.dot(coords, np.linalg.inv(cell._h))
+    d2 = np.dot(coords-c111, np.linalg.inv(cell._h))
+    # d1[:,0]>0 and d1[:,1] > 0 and d1[:,2] > 0 and
+    # d2[:,0]<0 and d2[:,1] < 0 and d2[:,2] < 0
+    mask = np.logical_and(reduce(np.logical_and, (d1>=0).T),
+                          reduce(np.logical_and, (d2< 0).T))
+    return coords[mask], weights[mask]
+
+
+class BeckeGrids(pyscf.dft.gen_grid.Grids):
+    '''Becke, JCP, 88, 2547 (1988)'''
+    def __init__(self, cell):
+        self.cell = cell
+        pyscf.dft.gen_grid.Grids.__init__(self, cell)
+
+    def setup_grids_(self, cell=None):
+        if cell is None: cell = self.cell
+        self.coords, self.weights = gen_becke_grids(self.cell, self.atom_grid)
+        return self.coords, self.weights
+
+if __name__ == '__main__':
+    import pyscf.pbc.gto as pgto
+
+    L = 4.
+    n = 30
+    cell = pgto.Cell()
+    cell.h = np.diag([L,L,L])
+    cell.gs = np.array([n,n,n])
+
+    cell.atom = '''He     0.    0.       1.
+                   He     1.    0.       1.'''
+    cell.basis = {'He': [[0, (1.0, 1.0)]]}
+    cell.build()
+    g = BeckeGrids(cell)
+    g.build_()
