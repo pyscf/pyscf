a272ee7ea4752d6a8c688ba8558d50e3c88e84ca
Qiming Sun <osirpt.sun@gmail.com>
2015-11-04 22:39:17 -0500

    Approximate J of model density for Poisson Gaussian, total energy correct.

--- pbc/df/poisson_gaussian.py
+++ pbc/df/poisson_gaussian.py
@@ -45,6 +45,53 @@ def get_j(cell, dm, auxcell, modcell):
     vj -= v1[idx].sum()/cell.vol * ovlp
     return vj
 
+# model potential
+def get_jmod(cell, auxcell):
+    idxb = []
+    for ib in range(auxcell.nbas):
+        l = auxcell.bas_angular(ib)
+        e = auxcell.bas_exp(ib)[0]
+        if l == 0 and abs(e-3.3) < .1:
+            idxb.append(ib)
+    chg = numpy.ones(2)*2
+    modcell = auxcell.copy()
+    modcell._bas = auxcell._bas[idxb].copy()
+    modcell.nbas = len(modcell._bas)
+    rhok = eval_rhok(chg, modcell)
+    coulG = tools.get_coulG(auxcell)
+    v2 = rhok * coulG
+    # weight = vol/N,  1/vol * weight = 1/N
+    # ifft has 1/N
+    vw = tools.ifft(v2, cell.gs)
+
+    coords = pyscf.pbc.dft.gen_grid.gen_uniform_grids(cell)
+    kpt = None
+    aoR = pyscf.pbc.dft.numint.eval_ao(cell, coords, kpt)
+    vj = np.dot(aoR.T.conj(), vw.reshape(-1,1)*aoR)#.real
+#############
+#    ip = 0
+## normalize aux basis
+#    for ib in range(auxcell.nbas):
+#        l = auxcell.bas_angular(ib)
+#        if l == 0:
+#            e = auxcell.bas_exp(ib)[0]
+#            ptr = auxcell._bas[ib,gto.PTR_COEFF]
+#            auxcell._env[ptr] = 1/np.sqrt(4*np.pi)/gto.mole._gaussian_int(2,e)
+#            ip += 1
+#        else:
+#            ip += 2*l+1
+#    idx = where2(auxcell)
+#    chg = numpy.ones(2)*2
+#    s1 = pscf.scfint.get_ovlp(auxcell)
+#    rho = numpy.einsum('ij,j->i', s1[:,idx], chg)
+#    c2 = pscf.scfint.get_t(auxcell)
+#    v1 = np.linalg.solve(c2, 2*np.pi*rho)
+#
+#    nao = cell.nao_nr()
+#    c3 = df.aux_e2(cell, auxcell, 'cint3c1e_sph').reshape(nao,nao,-1)
+#    vj = np.einsum('ijk,k->ij', c3, v1)
+    return vj
+
 def eval_rhok(rho, cell):
     rhok = numpy.zeros(cell.Gv.shape[0], dtype=numpy.complex)
     k2 = numpy.einsum('ij,ij->i', cell.Gv, cell.Gv)
@@ -134,25 +181,17 @@ if __name__ == '__main__':
     mf = pdft.RKS(cell)
     mf.xc = 'LDA,VWN'
     #auxbasis = {'He': df.genbas(3., 2.8, (10.,0.1), 0)+df.genbas(3., 2. , (10.,0.4), 1)}
-    auxbasis = {'He': df.genbas(3.3, 1.8, (1000000.,0.1), 0)
+    auxbasis = {'He': df.genbas(3.3, 1.8, (10000.,0.1), 0)
                      +df.genbas(3.3, 2. , (10.,0.3), 1)
                      +df.genbas(3.3, 2. , (10.,0.3), 2)}
     auxcell = df.format_aux_basis(cell, auxbasis)
     print auxcell.nimgs
     auxcell.nimgs = [2,2,2]
     #auxcell.nimgs = [4,4,4]
-    mf.get_j = lambda cell, dm, *args: get_j(cell, dm, auxcell, None)
-    mf.get_hcore = lambda cell, *args: get_nuc(cell, auxcell, None) + pscf.hf.get_t(cell)
+    vjmod = get_jmod(cell, auxcell)
+    mf.get_j = lambda cell, dm, *args: get_j(cell, dm, auxcell, None) + vjmod
+    mf.get_hcore = lambda cell, *args: get_nuc(cell, auxcell, None)-vjmod + pscf.hf.get_t(cell)
     e1 = mf.scf()
     print e1 # ~ -4.32022187118
 #    print mf.mo_energy
 
-    mf1 = pyscf.dft.RKS(cell)
-    mf1.kernel()
-    dm = mf1.make_rdm1()
-    vj1 = get_j(cell, dm, auxcell, None)
-    v1 = get_nuc(cell, auxcell, None)
-    print vj1 + v1
-    vj0 = mf.get_j(cell, dm)
-    v0 = pscf.hf.get_nuc(cell)
-    print v0 + vj0
