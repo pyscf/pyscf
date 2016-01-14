Cleanup and fixed ewald precision bug for Madelung constant.

pbc/scf/hf.py
@@ -279,7 +279,6 @@ def get_vkR_(mf, cell, aoR_k1, aoR_k2, kpt1, kpt2):
     coords = pyscf.pbc.dft.gen_grid.gen_uniform_grids(cell)
     ngs, nao = aoR_k1.shape
 
-    #coulG = tools.get_coulG(cell, kpt1-kpt2)
     coulG = tools.get_coulG(cell, kpt1-kpt2, exx=True, mf=mf)
 
     vR = np.zeros((ngs,nao,nao), dtype=np.complex128)
@@ -491,7 +490,7 @@ class RHF(pyscf.scf.hf.RHF):
         analytic_int : bool
             Whether to use analytic (libcint) integrals instead of grid-based. 
     '''
-    def __init__(self, cell, kpt=None, analytic_int=None, exxdiv='vcut_sph'):
+    def __init__(self, cell, kpt=None, analytic_int=None, exxdiv='ewald'):
         if not cell._built:
             sys.stderr.write('Warning: cell.build() is not called in input\n')
             cell.build()

pbc/scf/khf.py
@@ -246,7 +246,7 @@ class KRHF(pbchf.RHF):
         kpts : (nks,3) ndarray
             The sampling k-points in Cartesian coordinates, in units of 1/Bohr.
     '''
-    def __init__(self, cell, kpts, exxdiv='vcut_sph'):
+    def __init__(self, cell, kpts, exxdiv='ewald'):
         pbchf.RHF.__init__(self, cell, kpts, exxdiv=exxdiv)
         self.kpts = kpts
         self.mo_occ = []
@@ -281,12 +281,11 @@ class KRHF(pbchf.RHF):
         Rin = Lc.min() / 2.0
         print "# Rin =", Rin
         # ASE:
-        alpha = 5./Rin # sqrt(-ln eps) / Rc, eps ~ 10^{-9}
+        alpha = 5./Rin # sqrt(-ln eps) / Rc, eps ~ 10^{-11}
         kcell.gs = np.array([2*int(L*alpha*3.0) for L in Lc]) 
         # QE:
         #alpha = 3./Rin * np.sqrt(0.5)
         #kcell.gs = (4*alpha*np.linalg.norm(kcell.h,axis=0)).astype(int)
-        #kcell.gs = 2 * self.cell.gs #FIXME 
         print "# kcell.gs FFT =", kcell.gs
         kcell.build(False,False)
         vR = tools.ifft( tools.get_coulG(kcell), kcell.gs )
@@ -315,8 +314,6 @@ class KRHF(pbchf.RHF):
         if key.lower() == '1e':
             return self.init_guess_by_1e(cell)
         elif key.lower() == 'chkfile':
-            # FIXME: hf.py's init_guess_by_chkfile is inherited, but
-            # doesn't make correct DM with kpts, like below
             return self.init_guess_by_chkfile() 
         else:
             dm = pyscf.scf.hf.get_init_guess(cell, key)

pbc/scf/test/test_khf.py
@@ -29,26 +29,28 @@ def make_primitive_cell(ngs):
 
 class KnowValues(unittest.TestCase):
     def test_kpt_vs_supercell(self):
-        ngs = 3
+        ngs = 8
         nk = (3, 1, 1)
         cell = make_primitive_cell(ngs)
+        print "cell gs =", cell.gs
         scaled_kpts = ase.dft.kpoints.monkhorst_pack(nk)
         abs_kpts = cell.get_abs_kpts(scaled_kpts)
         kmf = pbchf.KRHF(cell, abs_kpts)
-        #kmf.verbose = 7
+        kmf.verbose = 7
         ekpt = kmf.scf()
 
         supcell = pyscf.pbc.tools.super_cell(cell, nk)
         supcell.gs = np.array([nk[0]*ngs + (nk[0]-1)//2, 
                                nk[1]*ngs + (nk[1]-1)//2,
                                nk[2]*ngs + (nk[2]-1)//2])
+        print "supcell gs =", supcell.gs
         #supcell.verbose = 7
         supcell.build()
 
         scaled_gamma = ase.dft.kpoints.monkhorst_pack((1,1,1))
         gamma = supcell.get_abs_kpts(scaled_gamma)
         mf = pbchf.KRHF(supcell, gamma)
-        #mf.verbose = 7
+        mf.verbose = 7
         esup = mf.scf()/np.prod(nk)
 
         print "kpt sampling energy =", ekpt
@@ -59,3 +61,4 @@ class KnowValues(unittest.TestCase):
 if __name__ == '__main__':
     print("Full Tests for pbc.dft.krks")
     unittest.main()
+

pbc/tools/pbc.py
@@ -107,9 +107,6 @@ def get_coulG(cell, k=np.zeros(3), exx=False, mf=None):
             coulG = 4*np.pi/absG2*(1.0 - np.cos(np.sqrt(absG2)*Rc))
         if np.linalg.norm(k) < 1e-8:
             coulG[0] = 4*np.pi*0.5*Rc**2
-        #print "# SPH k =", k
-        #for i,kGi in enumerate(kG):
-        #    print np.linalg.norm(kGi), coulG[i].real
     elif mf.exxdiv == 'ewald':
         with np.errstate(divide='ignore'):
             coulG = 4*np.pi/absG2
@@ -132,10 +129,6 @@ def get_coulG(cell, k=np.zeros(3), exx=False, mf=None):
         is_lt_maxqv = (abs(kG) <= maxqv).all(axis=1)
         coulG += mf.exx_vq[qidx] * is_lt_maxqv
 
-        #print "# WS k =", k
-        #for i,kGi in enumerate(kG):
-        #    print np.linalg.norm(kGi), coulG[i].real
-
     return coulG
 
 
@@ -148,6 +141,7 @@ def madelung(cell, kpts):
     ecell.atom = 'H 0. 0. 0.'
     ecell.spin = 1
     ecell.gs = cell.gs
+    ecell.precision = 1e-16
     ecell.unit = 'B'
     ecell.h = cell._h * Nk
     ecell.build(False,False)