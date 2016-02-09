47d10f338501ac05523ee8fb728d8030c2618b70
Qiming Sun <osirpt.sun@gmail.com>
2016-02-08 21:02:56 -0500

    Add DFT TDA gradients

--- future/tddft/rhf.py
+++ future/tddft/rhf.py
@@ -12,7 +12,7 @@ import numpy
 import pyscf.lib
 
 
-class TDHF(pyscf.lib.StreamObject):
+class TDA(pyscf.lib.StreamObject):
     def __init__(self, mf):
         self.verbose = mf.verbose
         self.stdout = mf.stdout
@@ -21,7 +21,7 @@ class TDHF(pyscf.lib.StreamObject):
         self._scf = mf
 
         self.conv_tol = 1e-9
-        self.nstates = 5
+        self.nstates = 3
         self.singlet = True
         self.lindep = 1e-12
         self.level_shift = 0
@@ -30,8 +30,10 @@ class TDHF(pyscf.lib.StreamObject):
         self.max_memory = mf.max_memory
         self.chkfile = mf.chkfile
 
-        self.x = None
-        self.y = None
+        # xy = (X,Y), normlized to 1/2: 2(XX-YY) = 1
+        # In TDA or TDHF, Y = 0
+        self.e = None
+        self.xy = None
         self._keys = set(self.__dict__.keys())
 
     def dump_flags(self):
@@ -40,10 +42,10 @@ class TDHF(pyscf.lib.StreamObject):
         log.info('******** %s for %s ********',
                  self.__class__, self._scf.__class__)
         log.info('nstates = %d', self.nstates)
-        #if self.singlet:
-        #    log.info('Singlet')
-        #else:
-        #    log.info('Triplet')
+        if self.singlet:
+            log.info('Singlet')
+        else:
+            log.info('Triplet')
         log.info('conv_tol = %g', self.conv_tol)
         log.info('eigh lindep = %g', self.lindep)
         log.info('eigh level_shift = %g', self.level_shift)
@@ -62,14 +64,17 @@ class TDHF(pyscf.lib.StreamObject):
         mo_coeff = self._scf.mo_coeff
         mo_energy = self._scf.mo_energy
         nocc = (self._scf.mo_occ>0).sum()
-        eai = pyscf.lib.direct_sum('a-i->ai', mo_energy[nocc:], mo_energy[:nocc])
-        z = z.reshape(eai.shape)
-
         orbv = mo_coeff[:,nocc:]
         orbo = mo_coeff[:,:nocc]
-        dm = reduce(numpy.dot, (orbv, z, orbo.T))
-        vj, vk = self._scf.get_jk(mol, dm, hermi=0)
-        v1ao = vj*2 - vk
+        eai = pyscf.lib.direct_sum('a-i->ai', mo_energy[nocc:], mo_energy[:nocc])
+        z = z.reshape(eai.shape)
+        dmvo = reduce(numpy.dot, (orbv, z, orbo.T))
+        vj, vk = self._scf.get_jk(mol, dmvo, hermi=0)
+
+        if self.singlet:
+            v1ao = vj*2 - vk
+        else:
+            v1ao = -vk
 
         v1vo = reduce(numpy.dot, (orbv.T, v1ao, orbo))
         v1vo = eai*z + v1vo
@@ -83,8 +88,10 @@ class TDHF(pyscf.lib.StreamObject):
         return precond
 
     def kernel(self, x0=None):
-        '''TDHF diagonalization solver
+        '''TDA diagonalization solver
         '''
+        self.check_sanity()
+
         mol = self.mol
         mo_coeff = self._scf.mo_coeff
         mo_energy = self._scf.mo_energy
@@ -108,23 +115,61 @@ class TDHF(pyscf.lib.StreamObject):
                                    nroots=self.nstates, lindep=self.lindep,
                                    max_space=self.max_space,
                                    verbose=self.verbose)
-        if self.nstates == 1:
-            x1 = [x1.reshape(eai.shape)]
-        else:
-            x1 = [xi.reshape(eai.shape) for xi in x1]
-
         self.e = w
-        self.x = x1
-        return w, x1
+# 1/sqrt(2) because self.x is for alpha excitation amplitude and 2(X^+*X) = 1
+        if self.nstates == 1:
+            self.xy = [(x1.reshape(eai.shape)*numpy.sqrt(.5),0)]
+        else:
+            self.xy = [(xi.reshape(eai.shape)*numpy.sqrt(.5),0) for xi in x1]
+        return self.e, self.xy
+CIS = TDA
 
-## ================
-## stream functions
-## ================
-#    def run_(self, *args, **kwargs):
-#        args, kwargs = self._format_args(args, kwargs, (('x0', None),))
-#        self.set(**kwargs)
-#        self.kernel(*args)
-#        return self
+
+class TDHF(TDA):
+    # z_{ai} = [(A-B)^{-1/2}(X+Y)]_{ai}
+    def get_vind(self, z):
+        mol = self.mol
+        mo_coeff = self._scf.mo_coeff
+        mo_energy = self._scf.mo_energy
+        nocc = (self._scf.mo_occ>0).sum()
+        orbv = mo_coeff[:,nocc:]
+        orbo = mo_coeff[:,:nocc]
+        eai = pyscf.lib.direct_sum('a-i->ai', mo_energy[nocc:], mo_energy[:nocc])
+        z = z.reshape(eai.shape)
+        dmvo = reduce(numpy.dot, (orbv, z, orbo.T))
+
+        raise NotImplementedError
+        if self.singlet:
+            vj, vk = self._scf.get_jk(mol, (dmvo, dmvo.T))
+
+        else: # Triplet
+            pass
+
+        return v1vo.ravel()
+
+    def kernel(self, x0=None):
+        '''TDHF diagonalization solver
+        '''
+        w2, x1 = TDA.kernel(self, x0)
+        self.e = numpy.sqrt(w2)
+
+        mo_energy = self._scf.mo_energy
+        nocc = (self._scf.mo_occ>0).sum()
+        eai = pyscf.lib.direct_sum('a-i->ai', mo_energy[nocc:], mo_energy[:nocc])
+        eai = numpy.sqrt(eai)
+        def norm_xy(w, z):
+            zp = eai * z[0].reshape(eai.shape)
+            zm = w/eai * z[0].reshape(eai.shape)
+            x = (zp + zm) * .5
+            y = (zp - zm) * .5
+            norm = 2*(pyscf.lib.norm(x)**2 - pyscf.lib.norm(y)**2)
+            norm = 1/numpy.sqrt(norm)
+            return x*norm,y*norm
+
+        self.xy = [norm_xy(self.e[i], z) for i, z in enumerate(x1)]
+
+        return self.e, self.xy
+
 
 
 if __name__ == '__main__':
@@ -142,10 +187,11 @@ if __name__ == '__main__':
 
     mf = scf.RHF(mol)
     mf.scf()
-    td = TDHF(mf)
+    td = TDA(mf)
     td.verbose = 5
     print td.kernel()[0] * 27.2114
-# [ 11.90276464  11.90276464  16.86036434  33.88245939  33.88245939]
+# [ 11.90276464  11.90276464  16.86036434]
 
-
-
+    td.singlet = False
+    print td.kernel()[0] * 27.2114
+# [ 11.01747918  11.01747918  13.16955056]
