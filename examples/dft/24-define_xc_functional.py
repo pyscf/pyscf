ca599efc198b5c145b3559143e50f071c197f00c
Qiming Sun <osirpt.sun@gmail.com>
2016-02-18 10:46:27 -0500

    Update dft example

--- examples/dft/24-define_xc_functional.py
+++ examples/dft/24-define_xc_functional.py
@@ -3,7 +3,7 @@
 '''
 User defined XC functional
 
-See also `pyscf.dft.vxc.define_xc_` function
+See also `pyscf.dft.libxc.parse_xc` function
 '''
 
 from pyscf import gto
@@ -23,11 +23,13 @@ mol = gto.M(
 # * The functional description is case-insensitive.
 # * The functional description string has two parts, separated by ",".  The
 #   first part describes the exchange functional, the second is the correlation
-#   functional.  If "," not appeared in string, entire string is considered as
-#   X functional.  There is no way to neglect X functional (just apply C
-#   functional)
+#   functional.
+#   - If "," not appeared in string, the entire string is considered as
+#     X functional.
+#   - To neglect X functional (just apply C functional), leave blank in the
+#     first part, eg description=',vwn' for pure VWN functional
 # * The functional name can be placed in arbitrary order.  Two name needs to
-#   be separated by operations + or -.  Blank spaces are ignored.
+#   be separated by operators + or -.  Blank spaces are ignored.
 #   NOTE the parser only reads operators + - *.  / is not in support.
 # * A functional name is associated with one factor.  If the factor is not
 #   given, it is assumed equaling 1.
@@ -38,15 +40,14 @@ mol = gto.M(
 #
 
 mf = dft.RKS(mol)
-b3lyp = '.2*HF + .08*LDA + .72*B88, .81*LYP + .19*VWN'
-mf.define_xc_(b3lyp)
+mf.xc = 'HF*0.2 + .08*LDA + .72*B88, .81*LYP + .19*VWN'
 e1 = mf.kernel()
 print('E = %.15g  ref = -76.3832244350081' % e1)
 
 #
 # No correlation functional
 #
-mf.define_xc_('.2*HF + .08*LDA + .72*B88')
+mf.xc = '.2*HF + .08*LDA + .72*B88'
 e1 = mf.kernel()
 print('E = %.15g  ref = -75.9807850596666' % e1)
 
@@ -54,11 +55,11 @@ print('E = %.15g  ref = -75.9807850596666' % e1)
 # If not given, the factor for each functional equals 1 by default.
 #
 mf = dft.RKS(mol)
-mf.define_xc_('b88,lyp')
+mf.xc = 'b88,lyp'
 e1 = mf.kernel()
 
 mf = dft.RKS(mol)
-mf.xc = 'b88,lyp'
+mf.xc = 'b88*1,lyp*1'
 eref = mf.kernel()
 print('%.15g == %.15g' % (e1, eref))
 
