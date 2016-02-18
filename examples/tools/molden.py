157a87406ce3ad5d1443edfa62843a77ba208441
Qiming Sun <osirpt.sun@gmail.com>
2016-02-18 09:00:16 -0500

    Handle h function in molden

--- examples/tools/molden.py
+++ examples/tools/molden.py
@@ -14,18 +14,18 @@ Write orbitals in molden format
 
 mol = gto.M(
     atom = '''
-  C  3.2883    3.3891    0.2345       
-  C  1.9047    3.5333    0.2237       
-  C  3.8560    2.1213    0.1612       
-  C  1.0888    2.4099    0.1396       
-  C  3.0401    0.9977    0.0771       
-  C  1.6565    1.1421    0.0663       
-  H  3.9303    4.2734    0.3007       
-  H  1.4582    4.5312    0.2815       
-  H  4.9448    2.0077    0.1699       
-  H  0.0000    2.5234    0.1311       
-  H  3.4870    0.0000    0.0197       
-  H  1.0145    0.2578    0.0000       
+  C  3.2883    3.3891    0.2345
+  C  1.9047    3.5333    0.2237
+  C  3.8560    2.1213    0.1612
+  C  1.0888    2.4099    0.1396
+  C  3.0401    0.9977    0.0771
+  C  1.6565    1.1421    0.0663
+  H  3.9303    4.2734    0.3007
+  H  1.4582    4.5312    0.2815
+  H  4.9448    2.0077    0.1699
+  H  0.0000    2.5234    0.1311
+  H  3.4870    0.0000    0.0197
+  H  1.0145    0.2578    0.0000
            ''',
     basis = 'cc-pvdz',
     symmetry = 1)
@@ -46,3 +46,18 @@ with open('C6H6mo.molden', 'w') as f1:
 c_loc_orth = lo.orth.orth_ao(mol)
 molden.from_mo(mol, 'C6H6loc.molden', c_loc_orth)
 
+
+#
+# Molden format does not support high angular momentum basis.  To handle the
+# orbitals which have l>=5 functions, a hacky way is to call molden.remove_high_l
+# function.  However, the resultant orbitals may not be orthnormal.
+#
+mol = gto.M(
+    atom = 'He 0 0 0',
+    basis = {'He': gto.expand_etbs(((0, 3, 1., 2.), (5, 2, 1., 2.)))})
+mf = scf.RHF(mol).run()
+try:
+    molden.from_mo(mol, 'He_without_h.molden', mf.mo_coeff)
+except RuntimeError:
+    print('    Found l=5 in basis.')
+    molden.from_mo(mol, 'He_without_h.molden', mf.mo_coeff, ignore_h=True)
