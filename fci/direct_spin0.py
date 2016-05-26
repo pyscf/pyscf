d68791e3dfe5ff5f6aa3fb0c54ffed63d197da76
Qiming Sun <osirpt.sun@gmail.com>
2016-05-26 19:00:06 -0400

    Fix initialization bug in fci spin0.contract1e

--- fci/direct_spin0.py
+++ fci/direct_spin0.py
@@ -32,7 +32,7 @@ def contract_1e(f1e, fcivec, norb, nelec, link_index=None):
     link_index = _unpack(norb, nelec, link_index)
     na, nlink = link_index.shape[:2]
     assert(fcivec.size == na**2)
-    ci1 = numpy.empty_like((na,na))
+    ci1 = numpy.empty_like(fcivec)
     f1e_tril = pyscf.lib.pack_tril(f1e)
     libfci.FCIcontract_1e_spin0(f1e_tril.ctypes.data_as(ctypes.c_void_p),
                                 fcivec.ctypes.data_as(ctypes.c_void_p),
