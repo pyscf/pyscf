a08c04151e88eb95ae05484adb8b3f64ef91bd87
Qiming Sun <osirpt.sun@gmail.com>
2016-09-06 23:43:18 -0700

    Update example

--- examples/tools/print_mo_and_dm.py
+++ examples/tools/print_mo_and_dm.py
@@ -5,7 +5,7 @@
 
 import sys
 import numpy
-from pyscf import gto, tools
+from pyscf import gto, scf, tools
 
 '''
 Formatted output for 2D array
@@ -33,3 +33,11 @@ mol = gto.M(atom='C 0 0 0',basis='6-31g')
 dm = numpy.eye(mol.nao_nr())
 tools.dump_mat.dump_rec(sys.stdout, dm, label=mol.spheric_labels(True),
                         ncol=9, digits=2)
+
+
+#
+# Change the default output format of .analyze function.
+#
+mol = gto.M(atom='H 0 0 0; F 0 0 1', basis='ccpvdz')
+mf = scf.RHF(mol).run()
+mf.analyze(verbose=5, ncol=10, digits=9)
