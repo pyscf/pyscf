9544532076db0d2b5527928f00039d1b49f491ff
Qiming Sun <osirpt.sun@gmail.com>
2016-10-30 00:11:07 -0700

    Restore CCSD nocc nmo attributes

--- cc/ccsd.py
+++ cc/ccsd.py
@@ -455,42 +455,63 @@ http://sunqm.net/pyscf/code-rule.html#api-rules for the details of API conventio
         self.mo_occ = mo_occ
         self._conv = False
         self.emp2 = None
-        self.ecc = None
         self.e_corr = None
         self.t1 = None
         self.t2 = None
         self.l1 = None
         self.l2 = None
+        self._nocc = None
+        self._nmo = None
 
         self._keys = set(self.__dict__.keys())
 
-    def nocc(self):
-        if isinstance(self.frozen, (int, numpy.integer)):
-            self._nocc = int(self.mo_occ.sum()) // 2 - self.frozen
-        else:
-            mo_occ = self.mo_occ.copy()
-            if len(self.frozen) > 0:
-                mo_occ[numpy.asarray(self.frozen)] = 0
-            self._nocc = int(mo_occ.sum()) // 2
-        return self._nocc
+    @property
+    def ecc(self):
+        return self.e_corr
 
-    def nmo(self):
-        if isinstance(self.frozen, (int, numpy.integer)):
-            self._nmo = len(self.mo_energy) - self.frozen
+    @property
+    def e_tot(self):
+        return self.e_corr + self._scf.e_tot
+
+    @property
+    def nocc(self):
+        if self._nocc is not None:
+            return self._nocc
+        elif isinstance(self.frozen, (int, numpy.integer)):
+            return int(self.mo_occ.sum()) // 2 - self.frozen
+        elif self.frozen:
+            occ_idx = self.mo_occ > 0
+            occ_idx[numpy.asarray(self.frozen)] = False
+            return numpy.count_nonzero(occ_idx)
         else:
-            self._nmo = len(self.mo_energy) - len(self.frozen)
-        return self._nmo
+            return int(self.mo_occ.sum()) // 2
+    @nocc.setter
+    def nocc(self, n):
+        self._nocc = n
+
+    @property
+    def nmo(self):
+        if self._nmo is not None:
+            return self._nmo
+        if isinstance(self.frozen, (int, numpy.integer)):
+            return len(self.mo_energy) - self.frozen
+        else:
+            return len(self.mo_energy) - len(self.frozen)
+    @nmo.setter
+    def nmo(self, n):
+        self._nmo = n
 
     def dump_flags(self):
         log = logger.Logger(self.stdout, self.verbose)
         log.info('')
         log.info('******** %s flags ********', self.__class__)
-        nocc = self.nocc()
-        nvir = self.nmo() - nocc
+        nocc = self.nocc
+        nvir = self.nmo - nocc
         log.info('CCSD nocc = %d, nvir = %d', nocc, nvir)
         if self.frozen:
             log.info('frozen orbitals %s', str(self.frozen))
         log.info('max_cycle = %d', self.max_cycle)
+        log.info('direct = %d', self.direct)
         log.info('conv_tol = %g', self.conv_tol)
         log.info('conv_tol_normt = %s', self.conv_tol_normt)
         log.info('diis_space = %d', self.diis_space)
@@ -501,7 +522,7 @@ http://sunqm.net/pyscf/code-rule.html#api-rules for the details of API conventio
     def init_amps(self, eris):
         time0 = time.clock(), time.time()
         mo_e = eris.fock.diagonal()
-        nocc = self.nocc()
+        nocc = self.nocc
         nvir = mo_e.size - nocc
         eia = mo_e[:nocc,None] - mo_e[None,nocc:]
         t1 = eris.fock[:nocc,nocc:] / eia
@@ -529,21 +550,20 @@ http://sunqm.net/pyscf/code-rule.html#api-rules for the details of API conventio
 
         if eris is None:
             eris = self.ao2mo(mo_coeff)
-        self._conv, self.ecc, self.t1, self.t2 = \
+        self._conv, self.e_corr, self.t1, self.t2 = \
                 kernel(self, eris, t1, t2, max_cycle=self.max_cycle,
                        tol=self.conv_tol, tolnormt=self.conv_tol_normt,
                        verbose=self.verbose)
-        self.e_corr = self.ecc
         if self._conv:
             logger.info(self, 'CCSD converged')
         else:
             logger.info(self, 'CCSD not converge')
         if self._scf.e_tot == 0:
-            logger.note(self, 'E_corr = %.16g', self.ecc)
+            logger.note(self, 'E_corr = %.16g', self.e_corr)
         else:
             logger.note(self, 'E(CCSD) = %.16g  E_corr = %.16g',
-                        self.ecc+self._scf.e_tot, self.ecc)
-        return self.ecc, self.t1, self.t2
+                        self.e_tot, self.e_corr)
+        return self.e_corr, self.t1, self.t2
 
     def solve_lambda(self, t1=None, t2=None, l1=None, l2=None, mo_coeff=None,
                      eris=None):
@@ -579,8 +599,8 @@ http://sunqm.net/pyscf/code-rule.html#api-rules for the details of API conventio
         return ccsd_rdm.make_rdm2(self, t1, t2, l1, l2)
 
     def ao2mo(self, mo_coeff=None):
-        #nocc = self.nocc()
-        #nmo = self.nmo()
+        #nocc = self.nocc
+        #nmo = self.nmo
         #nvir = nmo - nocc
         #eri1 = ao2mo.incore.full(self._scf._eri, mo_coeff)
         #eri1 = ao2mo.restore(1, eri1, nmo)
@@ -770,8 +790,8 @@ class _ERIS:
             fockao = cc._scf.get_hcore() + cc._scf.get_veff(cc.mol, dm)
             self.fock = reduce(numpy.dot, (mo_coeff.T, fockao, mo_coeff))
 
-        nocc = cc.nocc()
-        nmo = cc.nmo()
+        nocc = cc.nocc
+        nmo = cc.nmo
         nvir = nmo - nocc
         mem_incore, mem_outcore, mem_basic = _mem_usage(nocc, nvir)
         mem_now = lib.current_memory()[0]
