275b0696a3c26a7af14cfc296106afa7bebb4b2a
Peter Koval <koval.peter@gmail.com>
2017-03-05 09:28:04 +0100

    strings

--- lib/siesta/test/test_siesta.py
+++ lib/siesta/test/test_siesta.py
@@ -1,18 +1,34 @@
-#!/usr/bin/env python
+#####!/usr/bin/env python
 
 import os
 import ctypes
 import numpy
 from pyscf import lib
 
-# FIXME
+from ctypes import CDLL, POINTER, c_int64, c_double, byref, c_char
+from numpy import empty
 
-libsiesta = lib.load_library('siesta')
+dll = CDLL('../siesta.so')
 
 if __name__ == '__main__':
-    print('Full Tests for siesta')
-    a = 1
-    d = 1.0
-    libsiesta(a,d)
+    print('A test')
+    fname = numpy.array("file_name\x00", dtype="c")
+    dll.c_get_string(fname.ctypes.data_as(POINTER(c_char)))
+    
+    
+    
+    npy = 8000
+    a = c_int64(33)
+    n = c_int64(npy)
+    d = c_double(2.0)
+    dat = empty(npy, dtype="double")
+    dll.c_hello_world(byref(a), byref(d), byref(n), dat.ctypes.data_as(POINTER(c_double)))
+    
+    
+
+    print(dat[0])
+    print("after c_hello_world")
+    #print(libsiesta)
+    #print(libsiesta.hello_world)
 
 
