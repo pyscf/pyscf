b7a7d8d6f6b80753090465a514765631949ce09a
marc barbry <marc.barbarosa@gmail.com>
2017-03-30 15:06:27 +0200

    correction ion_xml reader

--- nao/m_ao_log.py
+++ nao/m_ao_log.py
@@ -39,34 +39,21 @@ class ao_log_c():
     self.sp2ion = sp2ion
     _siesta_ion_add_sp2(self, sp2ion)
     
-    npts = max(max(sp2ion[sp]["npts"]) for sp in range(self.nspecies))
+    npts = max(max(sp2ion[sp]["paos"]["npts"]) for sp in range(self.nspecies))
     self.nr = next235( max(2.0*npts, 1024.0) ) if nr==None else nr
     assert(self.nr>2)
 
-    min_delta = []
+    #min_delta = []
 
-    # dmin = min(min(sp2ion[sp]["delta"]) for sp in range(self.nspecies))
-    # Get the minimum delta while avoiding 0.0 that would give Nan
-    for ion in sp2ion:
-      print('sp2ion[sp]["delta"] = ', ion["delta"])
-      if any(ion["delta"] == 0.0):
-        delta = []
-        for dlt in ion["delta"]:
-          if dlt != 0.0:
-            delta.append(dlt)
-        if len(delta) > 0:
-          min_delta.append(min(delta))
-      else:
-        min_delta.append(min(ion['delta']))
+    dmin = min(min(sp2ion[sp]["paos"]["delta"]) for sp in range(self.nspecies))
 
-    dmin = min(min_delta)
     if dmin == 0.0:
       raise ValueError('Error in ion file, dmin = 0.0!!!')
     
     self.rmin = dmin if rmin is None else rmin
     self.kmax = 1.0/dmin/numpy.pi if kmax==None else kmax
     
-    dmax = 2.3*max(max(sp2ion[sp]["cutoff"]) for sp in range(self.nspecies))
+    dmax = 2.3*max(max(sp2ion[sp]["paos"]["cutoff"]) for sp in range(self.nspecies))
     if dmax == 0.0:
       raise ValueError('Error in ion file, dmax = 0.0!!!')
     
@@ -77,13 +64,13 @@ class ao_log_c():
     self.psi_log = siesta_ion_interp(self.rr, sp2ion, 1)
     self.psi_log_rl = siesta_ion_interp(self.rr, sp2ion, 0)
     
-    self.sp2rcut = numpy.array([max(sp2ion[sp]["cutoff"]) for sp in range(self.nspecies)], dtype='float64')
+    self.sp2rcut = numpy.array([max(sp2ion[sp]["paos"]["cutoff"]) for sp in range(self.nspecies)], dtype='float64')
     
     self.sp_mu2rcut = numpy.empty((self.nspecies,self.psi_log.shape[1]), dtype='float64', order='F')
     self.sp_mu2rcut.fill(-999.0)
     
     for sp in range(self.nspecies):
-      self.sp_mu2rcut[sp,0:self.sp2nmult[sp]] = sp2ion[sp]["cutoff"][0:self.sp2nmult[sp]]
+      self.sp_mu2rcut[sp,0:self.sp2nmult[sp]] = sp2ion[sp]["paos"]["cutoff"][0:self.sp2nmult[sp]]
 
     #call sp2ion_to_psi_log(sv%sp2ion, sv%rr, sv%psi_log)
     #call init_psi_log_rl(sv%psi_log, sv%rr, sv%uc%mu_sp2j, sv%uc%sp2nmult, sv%psi_log_rl)
