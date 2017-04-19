0c6ea0ff98d2edb895b619202b617843a0a934a4
George Booth <george.booth24@gmail.com>
2017-04-19 18:01:09 +0100

    Modifications to fciqmc interface:  - Allow for UHF and ROHF molecular orbitals  - Integral dumping for UHF  - Allow for high spin systems  - Modifications to work with ECPs  - HPHF default off when working with UHF or high spin systems

--- future/fciqmcscf/fciqmc.py
+++ future/fciqmcscf/fciqmc.py
@@ -209,7 +209,15 @@ def run_standalone(fciqmcci, scf_obj, orbs=None, restart=None):
     if orbs is None:
         orbs = scf_obj.mo_coeff
     tol = 1e-9
-    nmo = orbs.shape[1]
+    if isinstance(orbs,tuple):
+        # Assume UHF
+        print 'uhf orbitals detected'
+        nmo = orbs[0].shape[1]
+        tUHF = True
+    else:
+        print 'rhf orbitals detected'
+        nmo = orbs.shape[1]
+        tUHF = False
     nelec = fciqmcci.mol.nelectron
     fciqmcci.dump_flags(verbose=5)
 
@@ -223,12 +231,23 @@ def run_standalone(fciqmcci, scf_obj, orbs=None, restart=None):
         else:
             # We need the AO basis overlap matrix to calculate the
             # symmetries.
-            s = fciqmcci.mol.intor_symmetric('cint1e_ovlp_sph')
-            fciqmcci.orbsym = pyscf.symm.label_orb_symm(fciqmcci.mol,
-                    fciqmcci.mol.irrep_name, fciqmcci.mol.symm_orb,
-                    orbs, s=s)
-            orbsym = [param.IRREP_ID_TABLE[fciqmcci.groupname][i]+1 for
-                      i in fciqmcci.orbsym]
+            if tUHF:
+                fciqmcci.orbsym = pyscf.symm.label_orb_symm(fciqmcci.mol,
+                        fciqmcci.mol.irrep_name, fciqmcci.mol.symm_orb,
+                        orbs[0])
+                tmp_orblist = fciqmcci.orbsym.tolist()
+                tmp_orblist += pyscf.symm.label_orb_symm(fciqmcci.mol,
+                        fciqmcci.mol.irrep_name, fciqmcci.mol.symm_orb,
+                        orbs[1]).tolist()
+                fciqmcci.orbsym = numpy.array(tmp_orblist)
+                orbsym = [param.IRREP_ID_TABLE[fciqmcci.groupname][i]+1 for
+                          i in fciqmcci.orbsym]
+            else:
+                fciqmcci.orbsym = pyscf.symm.label_orb_symm(fciqmcci.mol,
+                        fciqmcci.mol.irrep_name, fciqmcci.mol.symm_orb,
+                        orbs)
+                orbsym = [param.IRREP_ID_TABLE[fciqmcci.groupname][i]+1 for
+                          i in fciqmcci.orbsym]
 #            pyscf.tools.fcidump.write_head(fout, nmo, nelec,
 #                                           fciqmcci.mol.spin, orbsym)
     else:
@@ -237,14 +256,18 @@ def run_standalone(fciqmcci, scf_obj, orbs=None, restart=None):
 #    eri = pyscf.ao2mo.outcore.full(fciqmcci.mol, orbs, verbose=0)
     # Lookup and return the relevant 1-electron integrals, and print out
     # the FCIDUMP file.
-    eri = pyscf.ao2mo.incore.general(scf_obj._eri, (orbs,)*4, compact=False)
-    t = fciqmcci.mol.intor_symmetric('cint1e_kin_sph')
-    v = fciqmcci.mol.intor_symmetric('cint1e_nuc_sph')
-    h = reduce(numpy.dot, (orbs.T, t+v, orbs))
+    if tUHF:
+        write_uhf_integrals_neci(fciqmcci,scf_obj,nmo,nelec,orbs,orbsym,tol=tol)
+    else:
+        eri = pyscf.ao2mo.incore.general(scf_obj._eri, (orbs,)*4, compact=False)
+        h_core = scf_obj.get_hcore(fciqmcci.mol)
+#        t = fciqmcci.mol.intor_symmetric('cint1e_kin_sph')
+#        v = fciqmcci.mol.intor_symmetric('cint1e_nuc_sph')
+        h = reduce(numpy.dot, (orbs.T, h_core, orbs))
 
-    pyscf.tools.fcidump.from_integrals(fciqmcci.integralFile, h, 
-            pyscf.ao2mo.restore(8,eri,nmo), nmo, nelec, fciqmcci.mol.energy_nuc(),
-            fciqmcci.mol.spin, orbsym, tol=tol)
+        pyscf.tools.fcidump.from_integrals(fciqmcci.integralFile, h, 
+                pyscf.ao2mo.restore(8,eri,nmo), nmo, nelec, fciqmcci.mol.energy_nuc(),
+                fciqmcci.mol.spin, orbsym, tol=tol)
 
 #    pyscf.tools.fcidump.write_eri(fout, pyscf.ao2mo.restore(8,eri,nmo),
 #                                  nmo, tol=tol)
@@ -259,7 +282,7 @@ def run_standalone(fciqmcci, scf_obj, orbs=None, restart=None):
         neleca, nelecb = nelec
 
     if fciqmcci.generate_neci_input:
-        write_fciqmc_config_file(fciqmcci, neleca, nelecb, restart)
+        write_fciqmc_config_file(fciqmcci, neleca, nelecb, restart, tUHF)
 
     if fciqmcci.verbose >= logger.DEBUG1:
         in_file = fciqmcci.configFile
@@ -276,8 +299,77 @@ def run_standalone(fciqmcci, scf_obj, orbs=None, restart=None):
 
     return rdm_energy
 
+def write_uhf_integrals_neci(fciqmcci,scf_obj,nmo,nelec,orbs,orbsym,tol=1e-15):
+    ''' nmo is number of MO orbitals per spin channel
+        note that ordering is abababa...   '''
 
-def write_fciqmc_config_file(fciqmcci, neleca, nelecb, restart):
+    eri_aaaa = pyscf.ao2mo.restore(8,pyscf.ao2mo.incore.general(scf_obj._eri, (orbs[0],orbs[0],orbs[0],orbs[0]), compact=False),nmo)
+    eri_bbbb = pyscf.ao2mo.restore(8,pyscf.ao2mo.incore.general(scf_obj._eri, (orbs[1],orbs[1],orbs[1],orbs[1]), compact=False),nmo)
+    eri_aabb = pyscf.ao2mo.restore(8,pyscf.ao2mo.incore.general(scf_obj._eri, (orbs[0],orbs[0],orbs[1],orbs[1]), compact=False),nmo)
+    eri_bbaa = pyscf.ao2mo.restore(8,pyscf.ao2mo.incore.general(scf_obj._eri, (orbs[1],orbs[1],orbs[0],orbs[0]), compact=False),nmo)
+    h_core = scf_obj.get_hcore(fciqmcci.mol)
+#    t = fciqmcci.mol.intor_symmetric('cint1e_kin_sph')
+#    v = fciqmcci.mol.intor_symmetric('cint1e_nuc_sph')
+    h_aa = reduce(numpy.dot, (orbs[0].T, h_core, orbs[0]))
+    h_bb = reduce(numpy.dot, (orbs[1].T, h_core, orbs[1]))
+    nuc = fciqmcci.mol.energy_nuc()
+    float_format = ' %.16g'
+
+    # Stupidly, NECI wants its orbitals as a,b,a,b,a,b rather than aaaabbbb
+    # Reorder things so this is the case
+    assert(len(orbsym) % 2 == 0)
+    orbsym_reorder = [i for tup in zip(orbsym[:len(orbsym)/2], orbsym[len(orbsym)/2:]) for i in tup]
+    a_inds = [i*2+1 for i in range(orbs[0].shape[1])]
+    b_inds = [i*2+2 for i in range(orbs[1].shape[1])]
+
+    with open(fciqmcci.integralFile, 'w') as fout:
+        if not isinstance(nelec, (int, numpy.number)):
+            ms = abs(nelec[0] - nelec[1])
+            nelec = nelec[0] + nelec[1]
+        else: ms=0
+        fout.write(' &FCI NORB=%4d,NELEC=%2d,MS2=%d,\n' % (nmo*2, nelec, ms))
+        if orbsym is not None and len(orbsym_reorder) > 0:
+            fout.write('  ORBSYM=%s\n' % ','.join([str(x) for x in orbsym_reorder]))
+        else:
+            fout.write('  ORBSYM=%s\n' % ('1,' * 2*nmo))
+        fout.write('  ISYM=1, UHF=TRUE\n')
+        fout.write(' &END\n')
+        # Assume 8-fold symmetry
+        npair = nmo*(nmo+1)//2
+        output_format = float_format + ' %4d %4d %4d %4d\n'
+        ij = 0
+        ijkl = 0
+        for i in range(nmo):
+            for j in range(0, i+1):
+                kl = 0
+                for k in range(0, i+1):
+                    for l in range(0, k+1):
+                        if ij >= kl:
+                            if abs(eri_aaaa[ijkl]) > tol:
+                                fout.write(output_format % (eri_aaaa[ijkl], a_inds[i], a_inds[j], a_inds[k], a_inds[l]))
+                            if abs(eri_bbbb[ijkl]) > tol:
+                                fout.write(output_format % (eri_bbbb[ijkl], b_inds[i], b_inds[j], b_inds[k], b_inds[l]))
+                            if abs(eri_aabb[ijkl]) > tol:
+                                fout.write(output_format % (eri_aabb[ijkl], a_inds[i], a_inds[j], b_inds[k], b_inds[l]))
+                            if abs(eri_bbaa[ijkl]) > tol:
+                                fout.write(output_format % (eri_bbaa[ijkl], b_inds[i], b_inds[j], a_inds[k], a_inds[l]))
+                            ijkl += 1
+                        kl += 1
+                ij += 1
+        h_aa = h_aa.reshape(nmo,nmo)
+        h_bb = h_bb.reshape(nmo,nmo)
+        output_format = float_format + ' %4d %4d  0  0\n'
+        for i in range(nmo):
+            for j in range(0, i+1):
+                if abs(h_aa[i,j]) > tol:
+                    fout.write(output_format % (h_aa[i,j], a_inds[i], a_inds[j]))
+                if abs(h_bb[i,j]) > tol:
+                    fout.write(output_format % (h_bb[i,j], b_inds[i], b_inds[j]))
+        output_format = float_format + '  0  0  0  0\n'
+        fout.write(output_format % nuc)
+    return 
+
+def write_fciqmc_config_file(fciqmcci, neleca, nelecb, restart, tUHF=False):
     '''Write an input file for a NECI calculation.
 
     Args:
@@ -304,10 +396,11 @@ def write_fciqmc_config_file(fciqmcci, neleca, nelecb, restart):
     f.write('freeformat\n')
     f.write('electrons %d\n' % (neleca+nelecb))
     # fci-core requires these two options.
-    f.write('spin-restrict 0\n')
+    f.write('spin-restrict %d\n' %(-fciqmcci.mol.spin))
     f.write('sym %d 0 0 0\n' % (fciqmcci.pg_symmetry-1))
     f.write('nonuniformrandexcits 4ind-weighted\n')
-    f.write('hphf 0\n')
+    if not (tUHF or fciqmcci.mol.spin != 0):
+        f.write('hphf 0\n')
     f.write('nobrillouintheorem\n')
     if nstates > 1:
         f.write('system-replicas %d\n' % (2*nstates))
