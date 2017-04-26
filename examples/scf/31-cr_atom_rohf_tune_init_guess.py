48b7e8802f2707d8a8099ef636038ccd933b0067
Qiming Sun <osirpt.sun@gmail.com>
2017-04-26 13:00:30 -0700

    update the example for atom SCF calculation

--- examples/scf/31-cr_atom_rohf_tune_init_guess.py
+++ examples/scf/31-cr_atom_rohf_tune_init_guess.py
@@ -3,13 +3,13 @@
 # Author: Qiming Sun <osirpt.sun@gmail.com>
 #
 
-import numpy
-from pyscf import gto
-from pyscf import scf
-
 '''
 Control the SCF procedure by different initial guess.
 '''
+
+import numpy
+from pyscf import gto
+from pyscf import scf
 
 mol = gto.Mole()
 mol.build(
@@ -69,3 +69,31 @@ rdm1 = mf.make_rdm1()
 mf = scf.RHF(mol)
 mf.irrep_nelec = {'Ag': (6,3), 'B1g': (1,0), 'B2g': (1,0), 'B3g': (1,0)}
 mf.scf(rdm1)
+
+
+#
+# The third way to force the calculation strictly following the correct
+# configurations is the second order SCF optimizaiton.  In the following code,
+# we call a calculation on cation for a correct HF configuration with spherical
+# symmetry.  This HF configuration is next pass to second order SCF solver
+# scf.newton to solve X2C-ROHF model of the open shell atom.
+#
+mol = gto.M(
+    verbose = 4,
+    symmetry = True,
+    atom = [['Cr',(0, 0, 0)], ],
+    basis = 'cc-pvdz-dk',
+    charge = 6,
+    spin = 0)
+mf = scf.sfx2c1e(scf.RHF(mol)).run()
+mo, mo_occ = mf.mo_coeff, mf.mo_occ
+
+mol.charge = 0
+mol.spin = 6
+mol.build(False,False)
+
+mf = scf.newton(scf.sfx2c1e(scf.RHF(mol)))
+mo_occ[9:15] = 1
+mf.kernel(mo, mo_occ)
+#mf.analyze()
+
