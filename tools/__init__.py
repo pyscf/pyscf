3afcfa7ff16b8057f5e43614995b064c5a569f89
marc barbry <marc.barbarosa@gmail.com>
2017-04-27 12:09:03 +0200

    forgot devnull

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,3 +1,33 @@
 from pyscf.tools import fcidump
 from pyscf.tools import molden
 from pyscf.tools import dump_mat
+
+#
+#
+#
+class DevNull:
+    """
+        Class use in the testing routines.
+        Coming from ase/ase/utils/__init__.py
+    """
+    encoding = 'UTF-8'
+    
+    def write(self, string):
+        pass
+    
+    def flush(self):
+        pass
+    
+    def seek(self, offset, whence=0):
+        return 0
+    
+    def tell(self):
+        return 0
+    
+    def close(self):
+        pass
+    
+    def isatty(self):
+        return False
+
+devnull = DevNull()
