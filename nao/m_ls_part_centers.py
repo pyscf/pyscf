8bc0e53ea968841fc05c2de42d439f90bcc0d09b
Peter Koval <koval.peter@gmail.com>
2017-05-24 22:08:24 +0200

    lens

--- nao/m_ls_part_centers.py
+++ nao/m_ls_part_centers.py
@@ -52,31 +52,65 @@ def ls_part_centers(sv, ia1, ia2, ac_rcut_coeff=1.0):
 
   return sorted(list(set(ls)))
 
+#
+#
+#
+def get_rmid_rcut(rv1,rcut1, rv2,rcut2):
+  """
+    For a given atom pair delivers coordinates of a center and 
+    radius of product.
+  """
+  d = sqrt(sum((rv1-rv2)**2))
+  dp2 = d**2
+  dif2 = rcut2**2-rcut1**2+dp2
+  a = dif2/2/dp2
+  rmax2_nocond = rcut1**2 - dp2*a**2
+  rmid_nocond = rv1*(a)+rv2*(1-a)
 
+  a = a if a<1.0 else 1.0
+  a = a if a>0.0 else 0.0
+  rmid = rv1*(a)+rv2*(1-a)
+  rmax2 = min(rcut1**2, rcut2**2)
+  rmid = rv1*(a)+rv2*(1-a)
+  return rmid, sqrt(rmax2)
+
+#
+#
+#
+def is_overlapping(rv1,rcut1, rv2,rcut2, rv3,rcut3):
+  """ For a given atom pair (1,2) and a center 3 tell whether function at center overlaps with the product."""
+  cond = sum((rv2-rv3)**2)<(rcut2+rcut3)**2 and sum((rv1-rv3)**2)<(rcut1+rcut3)**2
+  return cond
+  
+#
+#
+#
 if __name__ == '__main__':
   from pyscf import gto
   from pyscf.nao import system_vars_c
   import matplotlib.pyplot as plt
   import pylab
-  mol = gto.M(atom='C 0 0 -1.0; H 0 0 0; H 0 0 1; H 0 0 2; H 0 0 3; H 0 0 6; H 0 0 8;', basis='ccpvdz', unit='bohr')
-  sv = system_vars_c(gto=mol)
-  print(sv.ao_log.sp2charge)
-  sv.ao_log.sp_mu2rcut[0].fill(3.5)
-  sv.ao_log.sp2rcut[0] = 3.5
-  sv.ao_log.sp_mu2rcut[1].fill(3.0)
-  sv.ao_log.sp2rcut[1]=3.0
-  
-  ia1 = 0
-  ia2 = 1
-  print(ls_part_centers(sv, ia1, ia2))
 
-  a2c = sv.atom2coord
-  
+  rv1,rcut1 = array([3.0, 0.0, 0.0]), 3.0
+  rv2,rcut2 = array([0.0, 0.0, 0.0]), 5.0
+  rv3,rcut3 = array([-0.5, -3.0, 0.0]), 2.0
+    
   axes = pylab.axes()
-  axes.add_patch(pylab.Circle((a2c[ia1][2], a2c[ia1][1]), radius=sv.ao_log.sp2rcut[sv.atom2sp[ia1]], alpha=.5, color='r'))
-  axes.add_patch(pylab.Circle((a2c[ia2][2], a2c[ia2][1]), radius=sv.ao_log.sp2rcut[sv.atom2sp[ia2]], alpha=.5, color='r'))
-  for coo,sp in zip(sv.atom2coord, sv.atom2sp):
-    axes.add_patch(pylab.Circle((coo[2], coo[1]), radius=sv.ao_log.sp2rcut[sp], alpha=.2))
+  axes.add_patch(pylab.Circle((rv1[0], rv1[1]), radius=rcut1, alpha=.33, color='r', edgecolor='y', lw=3))
+  axes.add_patch(pylab.Circle((rv2[0], rv2[1]), radius=rcut2, alpha=.33, color='r', edgecolor='y', lw=3))
+  axes.add_patch(pylab.Circle((rv3[0], rv3[1]), radius=rcut3, alpha=.33, color='blue'))
   pylab.axis('scaled')
+
+  print(rv1, rcut1)
+  print(rv2, rcut2)
+  print(rv3, rcut3)
+  print(is_overlapping(rv1,rcut1, rv2,rcut2, rv3,rcut3))
+
   pylab.show()
+
+  
+  #for coo,sp in zip(sv.atom2coord, sv.atom2sp):
+  #  axes.add_patch(pylab.Circle((coo[2], coo[1]), radius=sv.ao_log.sp2rcut[sp], alpha=.2))
+  #pylab.axis('scaled')
+  #pylab.show()
 
