594db6b246b61eead36e7382d243e6742928b776
Qiming Sun <osirpt.sun@gmail.com>
2017-05-29 00:51:54 -0700

    Fix #78 for KUHF get_bands

--- pbc/scf/kuhf.py
+++ pbc/scf/kuhf.py
@@ -154,7 +154,7 @@ def canonicalize(mf, mo_coeff_kpts, mo_occ_kpts, fock=None):
         viridxb = ~occidxb
         eig_(fock[1][k], mo, occidxb, mo_e[1,k], mo)
         eig_(fock[1][k], mo, viridxb, mo_e[1,k], mo)
-    return mo_e, mo
+    return mo_e, mo_coeff_kpts
 
 def init_guess_by_chkfile(cell, chkfile_name, project=True, kpts=None):
     '''Read the KHF results from checkpoint file, then project it to the
@@ -328,8 +328,8 @@ class KUHF(uhf.UHF, khf.KRHF):
         fock = self.get_hcore(cell, kpts_band)
         fock = fock + self.get_veff(cell, dm_kpts, kpts=kpts, kpts_band=kpts_band)
         s1e = self.get_ovlp(cell, kpts_band)
-        e_a, c_a = self.eig(fock[0], s1e[0])
-        e_b, c_b = self.eig(fock[1], s1e[1])
+        e_a, c_a = khf.KRHF.eig(self, fock[0], s1e)
+        e_b, c_b = khf.KRHF.eig(self, fock[1], s1e)
         if single_kpt_band:
             e_a = e_a[0]
             e_b = e_b[0]
