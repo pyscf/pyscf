15b744c555c7277a880dec9f89a6cbe3ae0ccb2b
Peter Koval <koval.peter@gmail.com>
2017-07-06 09:42:52 +0200

    non-inter works. The problem was by the wavefunctions conversion

--- nao/m_siesta2blanko_denvec.py
+++ nao/m_siesta2blanko_denvec.py
@@ -6,19 +6,19 @@ from numpy import empty
 #
 def _siesta2blanko_denvec(orb2m, vec, orb_sc2orb_uc=None):
 
-  nreim,n = vec.shape
+  n,nreim = vec.shape
   if(nreim==1):
     for i in range(n): 
       j = i
       if(j>=n): j = orb_sc2orb_uc[i]
-      vec[0,i] = vec[0,i]*(-1.0)**orb2m[j]
+      vec[i,0] = vec[i,0]*(-1.0)**orb2m[j]
   elif(nreim==2):
     for i in range(n):
       j = i
       if(j>=n): j = orb_sc2orb_uc[i]
-      v = numpy.complex_(vec[0:1,i])*(-1.0)**orb2m[j]
-      vec[0,i] = v.real
-      vec[1,i] = v.imag
+      v = numpy.complex_(vec[i,0:])*(-1.0)**orb2m[j]
+      vec[i,0] = v.real
+      vec[i,1] = v.imag
   else:
     raise SystemError('!nreim')
 
