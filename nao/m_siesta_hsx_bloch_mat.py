ffed4f2db37e6a00a30dcd48bf637d6f0dbe51e6
Peter Koval <koval.peter@gmail.com>
2017-07-10 21:38:08 +0200

    4x faster trace over BZ, but still 0.13 sec per evaluation...

--- nao/m_siesta_hsx_bloch_mat.py
+++ nao/m_siesta_hsx_bloch_mat.py
@@ -6,29 +6,6 @@ from numpy import empty
 #
 #
 #
-def siesta_hsx_bloch_mat_vecs(csr, hsx, kvec=[0.0,0.0,0.0], prec=64):
-  assert(type(prec)==int)
-  
-  if(csr.nnz!=hsx.x4.shape[0]): 
-    print(csr.nnz, hsx.x4.shape)
-    raise SystemError('!nnz')
-
-  if(hsx.norbs!=len(csr.indptr)-1): raise SystemError('!csr')
-
-  den_bloch = numpy.zeros((hsx.norbs,hsx.norbs), dtype='complex'+str(2*prec))
-
-  for row in range(hsx.norbs):
-    s,f = csr.indptr[row],csr.indptr[row+1]
-    rvecs = hsx.x4[s:f,:]
-    cols  = hsx.orb_sc2orb_uc[csr.indices[s:f]]
-    phase_facs = numpy.exp(1.0j*numpy.dot(rvecs,kvec))
-    den_bloch[row,cols]=den_bloch[row,cols]+csr.data[s:f]*phase_facs
-
-  return den_bloch
-
-#
-#
-#
 def siesta_hsx_bloch_mat(csr, hsx, kvec=[0.0,0.0,0.0], prec=64):
   assert(type(prec)==int)
   
@@ -38,15 +15,13 @@ def siesta_hsx_bloch_mat(csr, hsx, kvec=[0.0,0.0,0.0], prec=64):
 
   if(hsx.norbs!=len(csr.indptr)-1): raise SystemError('!csr')
 
-  den_bloch = numpy.zeros((hsx.norbs,hsx.norbs), dtype='complex'+str(2*prec))
+  caux = numpy.exp(1.0j*numpy.dot(hsx.x4,kvec)) * csr.data
 
+  den_bloch = numpy.zeros((hsx.norbs,hsx.norbs), dtype='complex'+str(2*prec))    
   for row in range(hsx.norbs):
-    for ind in range(csr.indptr[row],csr.indptr[row+1]):
-      rvec = hsx.x4[ind,:]
-      col_sc = csr.indices[ind]
-      col = hsx.orb_sc2orb_uc[col_sc]
-      phase_fac = numpy.exp(1.0j*numpy.dot(rvec,kvec))
-      den_bloch[col,row]=den_bloch[col,row]+csr.data[ind]*phase_fac
+    for ind in range(csr.indptr[row], csr.indptr[row+1]):
+      col = hsx.orb_sc2orb_uc[csr.indices[ind]]
+      den_bloch[col,row]=den_bloch[col,row]+caux[ind]
 
   return den_bloch
 
