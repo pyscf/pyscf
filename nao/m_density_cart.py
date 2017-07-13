f55ca76f4cd78cb96873ce67f3fa74594e146d4a
Peter Koval <koval.peter@gmail.com>
2017-07-13 17:42:08 +0200

    fxc

---
+++ nao/m_density_cart.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+import numpy as np
+
+#
+#
+#
+def density_cart(sv, crds, sab2dm):
+  """
+    Compute the values of atomic orbitals on given grid points
+    Args:
+      sv   : instance of system_vars_c class
+      crds : vector where the atomic orbitals from "ao" are centered
+    Returns:
+      res[ncoord] : array of density
+  """
+
