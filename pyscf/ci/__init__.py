9a99e61f06ed8f16fde186aac6b6978c6dda1401
Lucas K. Wagner <lucas.wagner@gmail.com>
2017-07-24 06:27:37 -0500

    One final fix on CI

--- pyscf/ci/__init__.py
+++ pyscf/ci/__init__.py
@@ -1,6 +1,6 @@
 from pyscf.ci import cisd
 
-def CISD(mf, frozen=[], mo_coeff=None, mo_occ=None):		
+def CISD(mf, frozen=[], mo_coeff=None, mo_occ=None):
     from pyscf import scf
     if isinstance(mf, (scf.uhf.UHF, scf.rohf.ROHF)):
         raise NotImplementedError('RO-CISD, UCISD are not available in this pyscf version')
