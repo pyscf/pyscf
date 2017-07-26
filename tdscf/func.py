3dd50ffb997b7efe7dec9845e4a0ded4e1edec61
Kevin Koh <jkoh@nd.edu>
2017-07-25 20:28:58 -0400

    Dev (#98)
    * RTTDDFT code
    
    * Corrected to pyscf format
    
    * replaced most of print with
    
    * no more print

---
+++ tdscf/func.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def TransMat(M,U,inv = 1):
+        if inv == 1:
+                # U.t() * M * U
+                Mtilde = np.dot(np.dot(U.T.conj(),M),U)
+        elif inv == -1:
+                # U * M * U.t()
+                Mtilde = np.dot(np.dot(U,M),U.T.conj())
+        return Mtilde
+
+def TrDot(A,B):
+        C = np.trace(np.dot(A,B))
+        return C
+
+def MatrixPower(A,p,PrintCondition=False):
+        ''' Raise a Hermitian Matrix to a possibly fractional power. '''
+        u,s,v = np.linalg.svd(A)
+        if (PrintCondition):
+                print "MatrixPower: Minimal Eigenvalue =", np.min(s)
+        for i in range(len(s)):
+                if (abs(s[i]) < np.power(10.0,-14.0)):
+                        s[i] = np.power(10.0,-14.0)
+        return np.dot(u,np.dot(np.diag(np.power(s,p)),v))
