1567359f0dc8d3b29216baf45c3c1b73f8eef5cd
Qiming Sun <osirpt.sun@gmail.com>
2017-08-03 19:43:21 -0700

    Fix #100.  Add UCISD example.

--- pyscf/ci/__init__.py
+++ pyscf/ci/__init__.py
@@ -5,6 +5,8 @@ def CISD(mf, frozen=[], mo_coeff=None, mo_occ=None):
     from pyscf import lib
     from pyscf import scf
     if isinstance(mf, scf.uhf.UHF):
+        if len(frozen) == 0:
+            frozen = [[],[]]
         return ucisd.UCISD(mf, frozen, mo_coeff, mo_occ)
     elif isinstance(mf, scf.rohf.ROHF):
         lib.logger.warn(mf, 'RCISD method does not support ROHF method. ROHF object '
