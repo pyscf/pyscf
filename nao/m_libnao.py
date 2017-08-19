cc8b0b41191d08cd1c92ce7b0af55af35c53bf46
marc barbry <marc.barbarosa@gmail.com>
2017-08-19 13:17:56 +0200

    progress with GPU

--- nao/m_libnao.py
+++ nao/m_libnao.py
@@ -1,2 +1,3 @@
 from pyscf.lib import misc
 libnao = misc.load_library("libnao")
+libnao_gpu = misc.load_library("libnao_gpu")
--- nao/m_tddft_iter.py
+++ nao/m_tddft_iter.py
@@ -2,6 +2,22 @@ from __future__ import print_function, division
 import numpy as np
 from scipy.sparse import csr_matrix
 from timeit import default_timer as timer
+import sys
+
+try:
+    import pycuda.autoinit
+    import pycuda.driver as drv
+    import skcuda.linalg as culinalg
+    import pycuda.gpuarray as gpuarray
+    import skcuda.misc as cumisc
+    from m_iter_div_eigenenergy_cuda import div_eigenenergy_cuda
+
+    culinalg.init()
+    print("Device compute capability: ", cumisc.get_compute_capability(pycuda.autoinit.device))
+    GPU_import = True
+except:
+    GPU_import = False
+
 
 try:
     import numba
@@ -23,19 +39,6 @@ class tddft_iter_c():
     assert tddft_iter_tol>1e-6
     assert type(tddft_iter_broadening)==float
     assert sv.wfsx.x.shape[-1]==1 # i.e. real eigenvectors we accept here
-
-    if GPU:
-        try:
-            import skcuda.linalg as culinalg
-            import pycuda.gpuarray as gpuarray
-            culinalg.init()
-            self.GPU=True
-            print("GPU initialization")
-        except:
-            print("GPU lib failed to load, continuating with CPU")
-            self.GPU = False
-    else:
-        self.GPU = False
 
 
     self.rf0_ncalls = 0
@@ -75,6 +78,41 @@ class tddft_iter_c():
     self.xocc = self.x[0,0,0:self.nfermi,:,0]  # does python creates a copy at this point ?
     self.xvrt = self.x[0,0,self.vstart:,:,0]   # does python creates a copy at this point ?
     
+    if GPU and GPU_import:
+        self.GPU=True
+        print("GPU initialization")
+        self.xvrt_gpu = gpuarray.to_gpu(self.xvrt)
+        self.xvrt_gpu_tr = gpuarray.to_gpu(np.transpose(self.xvrt))
+        self.ksn2e_gpu = gpuarray.to_gpu(self.ksn2e[0, 0, :])
+        self.ksn2f_gpu = gpuarray.to_gpu(self.ksn2f[0, 0, :])
+
+        self.block_size = np.array([32, 32], dtype=int) # threads by block
+        self.grid_size = np.array([0, 0], dtype=int) # number of blocks
+        dimensions = [self.nfermi, self.ksn2f.shape[2]]
+        print("self.ksn2f.shape = ", self.ksn2f.shape)
+        for i in range(2):
+            if dimensions[i] <= self.block_size[i]:
+                self.block_size[i] = dimensions[i]
+                self.grid_size[i] = 1
+            else:
+                self.grid_size[i] = dimensions[i]/self.block_size[i] + 1
+        print("Python: block_size = ", self.block_size, " grid_size = ", self.grid_size, "nfermi = ", self.nfermi)
+    elif GPU and not GPU_import:
+        raise ValueError("GPU lib failed to initialize!")
+    else:
+        self.GPU = False
+
+
+  def finalize_gpu(self):
+    self.xvrt_gpu.gpudata.free()
+    self.ksn2e_gpu.gpudata.free()
+    self.ksn2f_gpu.gpudata.free()
+
+    self.nb2v_gpu.gpudata.free()
+    self.nm2v_gpu_real.gpudata.free()
+    self.nm2v_gpu_imag.gpudata.free()
+
+
   def apply_rf0(self, v, comega=1j*0.0):
     """ This applies the non-interacting response function to a vector (a set of vectors?) """
     assert len(v)==len(self.moms0), "%r, %r "%(len(v), len(self.moms0))
@@ -86,25 +124,39 @@ class tddft_iter_c():
     #nm2v = np.zeros([self.nfermi,len(self.xvrt)], dtype=np.complex64)
 
     if self.GPU:
-        xvrt_gpu = gpuarray.to_gpu(np.transpose(self.xvrt))
-        nb2v_gpu = gpuarray.to_gpu(nb2v)
+        self.nb2v_gpu = gpuarray.to_gpu(nb2v.real)
 
-        nm2v_gpu = culinalg.dot(nb2v, np.transpose(self.xvrt))
-        nm2v = nm2v_gpu.get()
+        self.nm2v_gpu_real = culinalg.dot(self.nb2v_gpu, self.xvrt_gpu_tr)
+        self.nb2v_gpu = gpuarray.to_gpu(nb2v.imag)
+        self.nm2v_gpu_imag = culinalg.dot(self.nb2v_gpu, self.xvrt_gpu_tr)
+        nm2v_bis = np.dot(nb2v, np.transpose(self.xvrt))
+        div_eigenenergy_cuda(self.ksn2e_gpu, self.ksn2f_gpu, self.nfermi, self.vstart,
+                comega, self.nm2v_gpu_real, self.nm2v_gpu_imag, self.block_size, self.grid_size)
 
-        xvrt_gpu.gpudata.free()
-        nb2v_gpu.gpudata.free()
-        nm2v_gpu.gpudata.free()
-
-        if use_numba:
-            div_eigenenergy_numba(self.ksn2e, self.ksn2f, self.nfermi, self.vstart, comega, nm2v)
-        else:
-            for n,[en,fn] in enumerate(zip(self.ksn2e[0,0,:self.nfermi],self.ksn2f[0,0,:self.nfermi])):
-              for j,[em,fm] in enumerate(zip(self.ksn2e[0,0,n+1:],self.ksn2f[0,0,n+1:])):
+        for n,[en,fn] in enumerate(zip(self.ksn2e[0,0,:self.nfermi],self.ksn2f[0,0,:self.nfermi])):
+            for j,[em,fm] in enumerate(zip(self.ksn2e[0,0,n+1:],self.ksn2f[0,0,n+1:])):
                 m = j+n+1-self.vstart
-                nm2v[n,m] = nm2v[n,m] * (fn-fm) * ( 1.0 / (comega - (em - en)) - 1.0 / (comega + (em - en)) )
+                if m >0:
+                    nm2v_bis[n,m] = nm2v_bis[n,m] * (fn - fm) * (( 1.0 / (comega - (em - en))) - (1.0 / (comega + (em - en)) ))
+        print("nm2v.flags: ", nm2v_bis.flags)
+        print("xvrt.flags: ", self.xvrt.flags)
+        print("nm2v_gpu.flags: ", self.nm2v_gpu_real.flags)
+        print("xvrt_gpu.flags: ", self.xvrt_gpu.flags)
+        self.nb2v_gpu = culinalg.dot(self.nm2v_gpu_real, self.xvrt_gpu)
+        self.nm2v_gpu_real = self.nb2v_gpu
 
-        nb2v = np.dot(nm2v,self.xvrt)
+        self.nb2v_gpu = culinalg.dot(self.nm2v_gpu_imag, self.xvrt_gpu)
+        self.nm2v_gpu_imag = self.nb2v_gpu
+
+        nm2v = 1j*self.nm2v_gpu_imag.get()
+        nm2v += self.nm2v_gpu_real.get()
+
+        nb2v = np.dot(nm2v_bis,self.xvrt)
+        print("sum(nm2v_gpu): = ", np.sum(abs(nm2v.real)), np.sum(abs(nm2v.imag)))
+        print("sum(nm2v_cpu): = ", np.sum(abs(nb2v.real)), np.sum(abs(nb2v.imag)))
+        print("error: ", np.sum(abs(nb2v-nm2v)))
+        sys.exit()
+
         ab2v = np.dot(np.transpose(self.xocc),nb2v).reshape(no*no)
     else:
         nm2v = np.dot(nb2v, np.transpose(self.xvrt))
@@ -115,7 +167,10 @@ class tddft_iter_c():
             for n,[en,fn] in enumerate(zip(self.ksn2e[0,0,:self.nfermi],self.ksn2f[0,0,:self.nfermi])):
               for j,[em,fm] in enumerate(zip(self.ksn2e[0,0,n+1:],self.ksn2f[0,0,n+1:])):
                 m = j+n+1-self.vstart
-                nm2v[n,m] = nm2v[n,m] * (fn-fm) * ( 1.0 / (comega - (em - en)) - 1.0 / (comega + (em - en)) )
+                # m can be negative, I think we should drop the value if m < 0??
+                if m > 0:
+                    nm2v[n,m] = nm2v[n,m] * (fn-fm) *\
+                      ( 1.0 / (comega - (em - en)) - 1.0 / (comega + (em - en)) )
 
         nb2v = np.dot(nm2v,self.xvrt)
         ab2v = np.dot(np.transpose(self.xocc),nb2v).reshape(no*no)
@@ -145,4 +200,8 @@ class tddft_iter_c():
       veff,info = self.comp_veff(self.moms1[:,0], comega)
       #print(iw, info, veff.sum())
       polariz[iw] = np.dot(self.moms1[:,0], self.apply_rf0( veff, comega ))
+
+    if self.GPU:
+        self.finalize_gpu()
+
     return polariz
