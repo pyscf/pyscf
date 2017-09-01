f03c95345d521b7b47fd36ffbf3ad7a87ca7cda7
Peter Koval <koval.peter@gmail.com>
2017-09-01 19:41:23 +0200

    system_vars_ados started

--- pyscf/nao/m_system_vars_dos.py
+++ pyscf/nao/m_system_vars_dos.py
@@ -37,3 +37,27 @@ def system_vars_pdos(sv, zomegas, nkpoints=1):
       pdos[j,iw] = (jksn2w[j,:,:,:]/(zw - sv.wfsx.ksn2e[:,:,:])).sum().imag
     
   return -pdos/np.pi/nkpoints
+
+
+def system_vars_ados(sv, zomegas, ls_atom_groups, nkpoints=1): 
+  """ Compute a Partial Density of States (resolved in atomic indices) using the eigenvalues and eigenvectors in wfsx """
+  from timeit import default_timer as timer
+  
+  iksn2w = zeros([2]+list(sv.wfsx.ksn2e.shape))
+  over = sv.hsx.s4_csr.toarray()
+  
+  orb2id_group = sv.get_orb2j()
+  
+  for id_group,group in enumerate(ls_atom_groups):
+    mask = (orb2group==id_group)
+    for k,kvec in enumerate(sv.wfsx.k2xyz):
+      for s in range(sv.nspin):
+        for n in range(sv.norbs):
+          jksn2w[j,k,s,n] = np.dot( np.dot(mask*sv.wfsx.x[k,s,n,:,0], over), sv.wfsx.x[k,s,n,:,0])
+  
+  pdos = zeros((jmx+1,len(zomegas)))
+  for j in range(jmx+1):
+    for iw,zw in enumerate(zomegas):
+      pdos[j,iw] = (jksn2w[j,:,:,:]/(zw - sv.wfsx.ksn2e[:,:,:])).sum().imag
+    
+  return -pdos/np.pi/nkpoints
