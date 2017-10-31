c97e5ece04369a66b4b0d855ca6321e89d259c1c
Qiming Sun <osirpt.sun@gmail.com>
2017-10-30 20:45:14 -0700

    A hack to setuptools for OS X

--- setup.py
+++ setup.py
@@ -68,6 +68,25 @@ if (sys.platform.startswith('linux') or
 elif sys.platform.startswith('darwin'):
     so_ext = '.dylib'
     LD_LIBRARY_PATH = 'DYLD_LIBRARY_PATH'
+    from distutils import sysconfig
+    conf_vars = sysconfig.get_config_vars()
+# setuptools/pip install by default generate "bundled" library.  Bundled
+# library cannot be linked at compile time
+# https://stackoverflow.com/questions/24519863/what-are-the-g-flags-to-build-a-true-so-mh-bundle-shared-library-on-mac-osx
+# configs LDSHARED and CCSHARED and SO are hard coded in lib/pythonX.X/_sysconfigdata.py
+# In some Python version, steuptools may correct these configs for OS X on the
+# fly by _customize_compiler_for_shlib function or setup_shlib_compiler function
+# in lib/pythonX.X/site-packages/setuptools/command/build_ext.py.
+# The hacks as below are to ensure that the OS X compiler does not generate
+# bundle libraries.  Relevant code:
+#    lib/pythonX.X/_sysconfigdata.py
+#    lib/pythonX.X/distutils/command/build_ext.py
+#    lib/pythonX.X/distutils/sysconfig.py,  get_config_vars()
+#    lib/pythonX.X/distutils/ccompiler.py,  link_shared_object()
+#    lib/pythonX.X/distutils/unixcompiler.py,  link()
+    conf_vars['LDSHARED'] = conf_vars['LDSHARED'].replace('-bundle', '-dynamiclib')
+    conf_vars['CCSHARED'] = " -dynamiclib"
+    conf_vars['SO'] = '.dylib'
 elif sys.platform.startswith('win'):
     so_ext = '.dll'
 else:
