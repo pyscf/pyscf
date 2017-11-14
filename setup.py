b57e758f23b01f330afa47ebcd1829faef3369fe
Qiming Sun <osirpt.sun@gmail.com>
2017-11-13 18:40:40 -0800

    Update setup.py to support pip install on mac

--- setup.py
+++ setup.py
@@ -68,8 +68,8 @@ if (sys.platform.startswith('linux') or
 elif sys.platform.startswith('darwin'):
     so_ext = '.dylib'
     LD_LIBRARY_PATH = 'DYLD_LIBRARY_PATH'
-    from distutils import sysconfig
-    conf_vars = sysconfig.get_config_vars()
+    from distutils.sysconfig import get_config_vars
+    conf_vars = get_config_vars()
 # setuptools/pip install by default generate "bundled" library.  Bundled
 # library cannot be linked at compile time
 # https://stackoverflow.com/questions/24519863/what-are-the-g-flags-to-build-a-true-so-mh-bundle-shared-library-on-mac-osx
@@ -135,17 +135,20 @@ if 'LDFLAGS' in os.environ:
 else:
     blas_found = False
 
-if blas_found:
-    blas_include = []
-    blas_lib_dir = []
-    blas_libraries = []
-else:
+blas_include = []
+blas_lib_dir = []
+blas_libraries = []
+blas_extra_link_flags = []
+blas_extra_compile_flags = []
+if not blas_found:
     np_blas = numpy.__config__.get_info('blas_opt')
     blas_include = np_blas.get('include_dirs', [])
     blas_lib_dir = np_blas.get('library_dirs', [])
     blas_libraries = np_blas.get('libraries', [])
     blas_path_guess = [search_lib_path('lib'+x+so_ext, blas_lib_dir)
                        for x in blas_libraries]
+    blas_extra_link_flags = np_blas.get('extra_link_args', [])
+    blas_extra_compile_flags = np_blas.get('extra_compile_args', [])
     if None not in blas_path_guess:
         blas_found = True
         blas_lib_dir = list(set(blas_path_guess))
@@ -204,7 +207,8 @@ with open(os.path.join(topdir, 'build', 'config.h'), 'w') as f:
 ''')
 
 def make_ext(pkg_name, relpath, srcs, libraries=[], library_dirs=default_lib_dir,
-             include_dirs=default_include, **kwargs):
+             include_dirs=default_include, extra_compile_flags=[],
+             extra_link_flags=[], **kwargs):
     if '/' in relpath:
         relpath = os.path.join(*relpath.split('/'))
     if (os.path.isfile(os.path.join(pyscf_lib_dir, 'build', 'CMakeCache.txt')) and
@@ -212,18 +216,20 @@ def make_ext(pkg_name, relpath, srcs, libraries=[], library_dirs=default_lib_dir
         return None
     else:
         if sys.platform.startswith('darwin'):
-            openmp_flag = []
+            soname = pkg_name.split('.')[-1]
+            extra_link_flags = extra_link_flags + ['-install_name', '@loader_path/'+soname+so_ext]
             runtime_library_dirs = []
         else:
-            openmp_flag = ['-fopenmp']
+            extra_compile_flags = extra_compile_flags + ['-fopenmp']
+            extra_link_flags = extra_link_flags + ['-fopenmp']
             runtime_library_dirs = ['$ORIGIN', '.']
         srcs = make_src(relpath, srcs)
         return Extension(pkg_name, srcs,
                          libraries = libraries,
                          library_dirs = library_dirs,
                          include_dirs = include_dirs + [os.path.join(pyscf_lib_dir,relpath)],
-                         extra_compile_args = openmp_flag,
-                         extra_link_args = openmp_flag,
+                         extra_compile_args = extra_compile_flags,
+                         extra_link_args = extra_link_flags,
 # Be careful with the ld flag "-Wl,-R$ORIGIN" in the shell.
 # When numpy.distutils is imported, the default CCompiler of distutils will be
 # overwritten. Compilation is executed in shell and $ORIGIN will be converted to ''
@@ -265,7 +271,9 @@ autocode/gaunt1.c autocode/grad1.c autocode/intor2.c autocode/intor3.c
 autocode/hess.c autocode/intor1.c autocode/grad2.c'''
         if os.path.exists(os.path.join(pyscf_lib_dir, 'libcint')):
             extensions.append(
-                make_ext('pyscf.lib.libcint', 'libcint/src', srcs, blas_libraries)
+                make_ext('pyscf.lib.libcint', 'libcint/src', srcs, blas_libraries,
+                         extra_compile_flags=blas_extra_compile_flags,
+                         extra_link_flags=blas_extra_link_flags)
             )
             default_include.append(os.path.join(pyscf_lib_dir, 'libcint','src'))
         else:
@@ -281,7 +289,9 @@ autocode/hess.c autocode/intor1.c autocode/grad2.c'''
 extensions += [
     make_ext('pyscf.lib.libnp_helper', 'np_helper',
              'condense.c npdot.c omp_reduce.c pack_tril.c transpose.c',
-             blas_libraries),
+             blas_libraries,
+	     extra_compile_flags=blas_extra_compile_flags,
+	     extra_link_flags=blas_extra_link_flags),
     make_ext('pyscf.lib.libcgto', 'gto',
              '''fill_int2c.c fill_nr_3c.c fill_r_3c.c fill_int2e.c ft_ao.c
              grid_ao_drv.c fastexp.c deriv1.c deriv2.c nr_ecp.c autocode/auto_eval1.c''',
