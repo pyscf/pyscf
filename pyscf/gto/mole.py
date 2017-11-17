a27d22d63dc2c4bcc74bc740cff8a33d19b0582f
Qiming Sun <osirpt.sun@gmail.com>
2017-11-17 10:31:24 -0800

    typo in doc

--- pyscf/gto/mole.py
+++ pyscf/gto/mole.py
@@ -1176,11 +1176,11 @@ def search_ao_label(mol, label):
     Examples:
 
     >>> mol = gto.M(atom='H 0 0 0; Cl 0 0 1', basis='ccpvtz')
-    >>> mol.parse_aolabel('Cl.*p')
+    >>> mol.search_ao_label('Cl.*p')
     [19 20 21 22 23 24 25 26 27 28 29 30]
-    >>> mol.parse_aolabel('Cl 2p')
+    >>> mol.search_ao_label('Cl 2p')
     [19 20 21]
-    >>> mol.parse_aolabel(['Cl.*d', 'Cl 4p'])
+    >>> mol.search_ao_label(['Cl.*d', 'Cl 4p'])
     [25 26 27 31 32 33 34 35 36 37 38 39 40]
     '''
     return _aolabels2baslst(mol, label)
