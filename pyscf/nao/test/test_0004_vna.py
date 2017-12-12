ad761afea33f596f1f428b6ac37e29ae3cd1ff34
Peter Koval <koval.peter@gmail.com>
2017-12-12 11:50:47 +0100

    vna LiH

--- pyscf/nao/test/test_0004_vna.py
+++ pyscf/nao/test/test_0004_vna.py
@@ -19,5 +19,19 @@ class KnowValues(unittest.TestCase):
     #np.savetxt('vna_n2_0004.txt', np.row_stack((sgrid, vna)).T)
     ref = np.loadtxt(dname+'/vna_n2_0004.txt-ref')
     for r,d in zip(ref[:,1],vna): self.assertAlmostEqual(r,d)
+
+  def test_vna(self):
+    dname = dirname(abspath(__file__))
+    n = nao(label='lih', cd=dname)
+    m = 200
+    dvec,midv = 2*(n.atom2coord[1] - n.atom2coord[0])/m,  (n.atom2coord[1] + n.atom2coord[0])/2.0
+    vgrid = np.tensordot(np.array(range(-m,m+1)), dvec, axes=0) + midv
+    sgrid = np.array(range(-m,m+1)) * np.sqrt((dvec*dvec).sum())
+    
+    vna = n.vna(vgrid)
+    #print(vna.shape, sgrid.shape)
+    np.savetxt('vna_lih_0004.txt', np.row_stack((sgrid, vna)).T)
+    ref = np.loadtxt(dname+'/vna_lih_0004.txt-ref')
+    for r,d in zip(ref[:,1],vna): self.assertAlmostEqual(r,d)
     
 if __name__ == "__main__": unittest.main()
