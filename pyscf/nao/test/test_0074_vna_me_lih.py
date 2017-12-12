f911d0b2ac6af9dbb3cba40c6b131a637e0a1423
Peter Koval <koval.peter@gmail.com>
2017-12-12 11:45:14 +0100

    LiH example

---
+++ pyscf/nao/test/test_0074_vna_me_lih.py
@@ -0,0 +1,23 @@
+from __future__ import print_function, division
+import os,unittest,numpy as np
+
+
+class KnowValues(unittest.TestCase):
+
+  def test_vneutral_atom_matrix_elements(self):
+    """ reSCF then G0W0 """
+    from pyscf.nao import mf as mf_c
+    
+    dname = os.path.dirname(os.path.abspath(__file__))
+    mf = mf_c(label='lih', cd=dname)
+    vna = mf.vna_coo(level=1).toarray()
+    rdm = mf.make_rdm1()[0,0,:,:,0]
+    Ena = 27.2114*(-0.5)*(vna*rdm).sum()
+    #print('Ena   = ', Ena)
+    self.assertAlmostEqual(Ena, 133.242031288)
+    #ove = mf.overlap_coo().toarray()
+    #print(ove.shape)
+    #print(rdm.shape)    
+    #print('nelec = ', (ove*rdm).sum())    
+
+if __name__ == "__main__": unittest.main()
