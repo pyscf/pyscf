d43b871b09b5e2e317cd00c34a69f5d510f8d2f3
Qiming Sun <osirpt.sun@gmail.com>
2017-12-13 01:14:12 -0800

    Update ccsd_grad tests

--- pyscf/cc/test/test_ccsd_grad.py
+++ pyscf/cc/test/test_ccsd_grad.py
@@ -28,54 +28,6 @@ mf.kernel()
 
 
 class KnownValues(unittest.TestCase):
-    def test_IX_intermediates(self):
-        mol = gto.M()
-        mf = scf.RHF(mol)
-        mycc = cc.ccsd.CCSD(mf)
-        numpy.random.seed(2)
-        nocc = 5
-        nmo = 12
-        nvir = nmo - nocc
-        eri0 = numpy.random.random((nmo,nmo,nmo,nmo))
-        eri0 = ao2mo.restore(1, ao2mo.restore(8, eri0, nmo), nmo)
-        fock0 = numpy.random.random((nmo,nmo))
-        fock0 = fock0 + fock0.T + numpy.diag(range(nmo))*20
-        t1 = numpy.random.random((nocc,nvir))
-        t2 = numpy.random.random((nocc,nocc,nvir,nvir))
-        t2 = t2 + t2.transpose(1,0,3,2)
-        l1 = numpy.random.random((nocc,nvir))
-        l2 = numpy.random.random((nocc,nocc,nvir,nvir))
-        l2 = l2 + l2.transpose(1,0,3,2)
-
-        h1 = fock0 - (numpy.einsum('kkpq->pq', eri0[:nocc,:nocc])*2
-                    - numpy.einsum('pkkq->pq', eri0[:,:nocc,:nocc]))
-        eris = lambda:None
-        idx = numpy.tril_indices(nvir)
-        eris.oooo = eri0[:nocc,:nocc,:nocc,:nocc].copy()
-        eris.ovoo = eri0[:nocc,nocc:,:nocc,:nocc].copy()
-        eris.ovvo = eri0[:nocc,nocc:,nocc:,:nocc].copy()
-        eris.oovv = eri0[:nocc,:nocc,nocc:,nocc:].copy()
-        eris.ovvv = eri0[:nocc,nocc:,nocc:,nocc:][:,:,idx[0],idx[1]].copy()
-        eris.vvvv = eri0[nocc:,nocc:,nocc:,nocc:][idx[0],idx[1]][:,idx[0],idx[1]].copy()
-        eris.fock = fock0
-
-        Ioo, Ivv, Ivo, Xvo = ccsd_grad.IX_intermediates(mycc, t1, t2, l1, l2, eris)
-        numpy.random.seed(1)
-        h1 = numpy.random.random((nmo,nmo))
-        h1 = h1 + h1.T
-        self.assertAlmostEqual(numpy.einsum('ij,ij', h1[:nocc,:nocc], Ioo), 2613213.0346526774, 7)
-        self.assertAlmostEqual(numpy.einsum('ab,ab', h1[nocc:,nocc:], Ivv), 6873038.9907923322, 7)
-        self.assertAlmostEqual(numpy.einsum('ai,ai', h1[nocc:,:nocc], Ivo), 4353360.4241635408, 7)
-        self.assertAlmostEqual(numpy.einsum('ai,ai', h1[nocc:,:nocc], Xvo), 203575.42337558540, 7)
-        dm1 = ccsd_grad.response_dm1(mycc, t1, t2, l1, l2, eris)
-        self.assertAlmostEqual(numpy.einsum('pq,pq', h1[nocc:,:nocc], dm1[nocc:,:nocc]), -486.638981725713393, 7)
-
-        fd2intermediate = lib.H5TmpFile()
-        d2 = cc.ccsd_rdm._gamma2_outcore(mycc, t1, t2, l1, l2, fd2intermediate)
-        mo_coeff = numpy.random.random((nmo,nmo)) - .5
-        dm2 = ccsd_grad._rdm2_mo2ao(mycc, d2, mo_coeff)
-        self.assertAlmostEqual(lib.finger(dm2), -2279.6732000822421, 9)
-
     def test_ccsd_grad(self):
         mycc = cc.ccsd.CCSD(mf)
         mycc.max_memory = 1
@@ -118,19 +70,6 @@ class KnownValues(unittest.TestCase):
         l1, l2 = mycc.solve_lambda()
         g1 = ccsd_grad.kernel(mycc, t1, t2, l1, l2, mf_grad=grad.RHF(mf))
         self.assertAlmostEqual(lib.finger(g1), 0.10599503839207361, 6)
-
-    def test_frozen_eris(self):
-        mycc = cc.ccsd.CCSD(mf)
-        mycc.frozen = [0,1,10,11,12]
-        eris1 = ccsd_grad._make_frozen_orbital_eris(mycc)
-        mycc.max_memory = 1
-        eris2 = ccsd_grad._make_frozen_orbital_eris(mycc)
-        self.assertAlmostEqual(abs(numpy.asarray(eris2.fooo) - eris1.fooo).max(), 0, 9)
-        self.assertAlmostEqual(abs(numpy.asarray(eris2.fvoo) - eris1.fvoo).max(), 0, 9)
-        self.assertAlmostEqual(abs(numpy.asarray(eris2.voof) - eris1.voof).max(), 0, 9)
-        self.assertAlmostEqual(abs(numpy.asarray(eris2.vovf) - eris1.vovf).max(), 0, 9)
-        self.assertAlmostEqual(abs(numpy.asarray(eris2.vvof) - eris1.vvof).max(), 0, 9)
-        self.assertAlmostEqual(abs(numpy.asarray(eris2.vvvf) - eris1.vvvf).max(), 0, 9)
 
 
 if __name__ == "__main__":
