805156c46853d1386cdc0f59cc5070d2a1aaf7f2
Qiming Sun <osirpt.sun@gmail.com>
2017-12-21 01:48:10 -0800

    Solve the import error in x2c

--- pyscf/pbc/x2c/test/test_x2c.py
+++ pyscf/pbc/x2c/test/test_x2c.py
@@ -18,30 +18,38 @@ cell.build(unit = 'B',
            verbose = 0,
            basis='sto3g')
 
+class light_speed(object):
+    def __init__(self, c):
+        self.bak = lib.param.LIGHT_SPEED
+        self.c = c
+    def __enter__(self):
+        lib.param.LIGHT_SPEED = self.c
+        return self.c
+    def __exit__(self, type, value, traceback):
+        lib.param.LIGHT_SPEED = self.bak
+
 class KnownValues(unittest.TestCase):
     def test_hf(self):
-        lib.param.LIGHT_SPEED, c = 2, lib.param.LIGHT_SPEED
-        mf = scf.RHF(cell).sfx2c1e()
-        mf.with_df = df.PWDF(cell)
-        dm = mf.get_init_guess()
-        h1 = mf.get_hcore()
-        self.assertAlmostEqual(numpy.einsum('ij,ji', dm, h1), -0.47578184212352159+0j, 9)
-        kpts = cell.make_kpts([3,1,1])
-        h1 = mf.get_hcore(kpt=kpts[1])
-        self.assertAlmostEqual(numpy.einsum('ij,ji', dm, h1), -0.09637799091491725+0j, 9)
-        lib.param.LIGHT_SPEED = c
+        with light_speed(2) as c:
+            mf = scf.RHF(cell).sfx2c1e()
+            mf.with_df = df.PWDF(cell)
+            dm = mf.get_init_guess()
+            h1 = mf.get_hcore()
+            self.assertAlmostEqual(numpy.einsum('ij,ji', dm, h1), -0.47578184212352159+0j, 9)
+            kpts = cell.make_kpts([3,1,1])
+            h1 = mf.get_hcore(kpt=kpts[1])
+            self.assertAlmostEqual(numpy.einsum('ij,ji', dm, h1), -0.09637799091491725+0j, 9)
 
     def test_khf(self):
-        lib.param.LIGHT_SPEED, c = 2, lib.param.LIGHT_SPEED
-        mf = scf.KRHF(cell).sfx2c1e()
-        mf.with_df = df.PWDF(cell)
-        mf.kpts = cell.make_kpts([3,1,1])
-        dm = mf.get_init_guess()
-        h1 = mf.get_hcore()
-        self.assertAlmostEqual(numpy.einsum('ij,ji', dm[0], h1[0]),-0.47578184212352159+0j, 9)
-        self.assertAlmostEqual(numpy.einsum('ij,ji', dm[1], h1[1]),-0.09637799091491725+0j, 9)
-        self.assertAlmostEqual(numpy.einsum('ij,ji', dm[2], h1[2]),-0.09637799091491725+0j, 9)
-        lib.param.LIGHT_SPEED = c
+        with light_speed(2) as c:
+            mf = scf.KRHF(cell).sfx2c1e()
+            mf.with_df = df.PWDF(cell)
+            mf.kpts = cell.make_kpts([3,1,1])
+            dm = mf.get_init_guess()
+            h1 = mf.get_hcore()
+            self.assertAlmostEqual(numpy.einsum('ij,ji', dm[0], h1[0]),-0.47578184212352159+0j, 9)
+            self.assertAlmostEqual(numpy.einsum('ij,ji', dm[1], h1[1]),-0.09637799091491725+0j, 9)
+            self.assertAlmostEqual(numpy.einsum('ij,ji', dm[2], h1[2]),-0.09637799091491725+0j, 9)
 
 
 if __name__ == '__main__':
