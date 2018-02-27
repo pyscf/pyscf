063c57ac506b8312751f0a9ca52187faa41ae515
Qiming Sun <osirpt.sun@gmail.com>
2018-02-27 12:02:39 -0800

    Examples to access 3-index integral tensor for PBC-GDF method

--- examples/pbc/30-ao_integrals.py
+++ examples/pbc/30-ao_integrals.py
@@ -57,10 +57,46 @@ eri = mydf.get_eri((ki,kj,kk,kl))
 print('ERI shape (%d,%d)' % eri.shape)
 
 #
-# Use MDF method to get better accuracy.
+# Use MDF method to get better accuracy for valence basis.
 #
 mydf = df.MDF(cell, kpts)
 mydf.auxbasis = 'weigend'
 eri = mydf.get_eri((ki,kj,kk,kl))
 print('ERI shape (%d,%d)' % eri.shape)
+
+#
+# Using .loop method to access the 3-index ERI tensor at gamma point.  In the
+# 3-index tensor Lpq, orbital basis index (p,q) are compressed (p>=q)
+#
+mydf = df.FFTDF(cell)
+eri_3d = numpy.vstack([Lpq.copy() for Lpq in mydf.loop()])
+# Test 2-e integrals
+eri = numpy.dot(eri_3d.T, eri_3d)
+print(abs(eri - mydf.get_eri(compact=True)).max())
+
+mydf = df.MDF(cell)
+eri_3d = numpy.vstack([Lpq.copy() for Lpq in mydf.loop()])
+# Test 2-e integrals
+eri = numpy.dot(eri_3d.T, eri_3d)
+print(abs(eri - mydf.get_eri(compact=True)).max())
+
+
+#
+# Using .sr_loop method to access the 3-index tensor of gaussian density
+# fitting (GDF) for arbitrary k-points
+#
+nao = cell.nao_nr()
+mydf = df.DF(cell, kpts=kpts)
+eri_3d_kpts = []
+for i, kpti in enumerate(kpts):
+    eri_3d_kpts.append([])
+    for j, kptj in enumerate(kpts):
+        eri_3d = []
+        for LpqR, LpqI in mydf.sr_loop([kpti,kptj], compact=False):
+            eri_3d.append(LpqR+LpqI*1j)
+        eri_3d = numpy.vstack(eri_3d).reshape(-1,nao,nao)
+        eri_3d_kpts[i].append(eri_3d)
+# Test 2-e integrals
+eri = numpy.einsum('Lpq,Lrs->pqrs', eri_3d_kpts[0][3], eri_3d_kpts[3][0])
+print(abs(eri - mydf.get_eri([kpts[0],kpts[3],kpts[3],kpts[0]]).reshape([nao]*4)).max())
 
