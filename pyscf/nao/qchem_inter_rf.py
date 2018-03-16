c81b975f83cd4811dd2b1540753b3d4d359a015d
Peter Koval <koval.peter@gmail.com>
2018-03-16 20:30:26 +0100

    ddd

--- pyscf/nao/qchem_inter_rf.py
+++ pyscf/nao/qchem_inter_rf.py
@@ -1,6 +1,6 @@
 from __future__ import print_function, division
 import sys, numpy as np
-from numpy import diagflat, zeros, einsum
+from numpy import concatenate, ravel, diagflat, zeros, einsum, stack
 from pyscf.nao import scf
 from copy import copy
 
@@ -15,7 +15,57 @@ class qchem_inter_rf(scf):
     nv = self.norbs-self.vstart[0]
     self.FmE = np.add.outer(self.ksn2e[0,0,self.vstart[0]:],-self.ksn2e[0,0,:self.nfermi[0]])
     self.sqrt_FmE = np.sqrt(self.FmE).reshape([nv*nf])
-    self.kernel_qchem_inter_rf()
+    self.kernel_qchem_inter_rf_pos()
+
+  def kernel_qchem_inter_rf_pos(self, **kw):
+    """ This is constructing the E_m-E_n and E_n-E_m matrices """
+    h_rpa = diagflat(self.FmE)
+    print(h_rpa.shape)
+
+    nf = self.nfermi[0]
+    nv = self.norbs-self.vstart[0]
+    vs = self.vstart[0]
+    neh = nf*nv
+    x = self.mo_coeff[0,0,:,:,0]
+    pab2v = self.pb.get_ac_vertex_array()
+    self.pmn2v = pmn2v = einsum('nb,pmb->pmn', x[:nf,:], einsum('ma,pab->pmb', x[vs:,:], pab2v))
+    pmn2c = einsum('qp,pmn->qmn', self.hkernel_den, pmn2v)
+    meri = einsum('pmn,pik->mnik', pmn2c, pmn2v).reshape((nf*nv,nf*nv))
+    print(meri.shape)
+    #meri.fill(0.0)
+    h_rpa = h_rpa+2.0*meri
+    esq, s2z = np.linalg.eigh(h_rpa)
+    print(abs(h_rpa-h_rpa.transpose()).sum())
+    print('esq', esq*27.2114)
+    
+    return 
+
+  def kernel_qchem_inter_rf_pos_neg(self, **kw):
+    """ This is constructing the E_m-E_n and E_n-E_m matrices """
+    h_rpa = diagflat(concatenate((ravel(self.FmE),-ravel(self.FmE))))
+    print(h_rpa.shape)
+
+
+    nf = self.nfermi[0]
+    nv = self.norbs-self.vstart[0]
+    vs = self.vstart[0]
+    neh = nf*nv
+    x = self.mo_coeff[0,0,:,:,0]
+    pab2v = self.pb.get_ac_vertex_array()
+    self.pmn2v = pmn2v = einsum('nb,pmb->pmn', x[:nf,:], einsum('ma,pab->pmb', x[vs:,:], pab2v))
+    pmn2c = einsum('qp,pmn->qmn', self.hkernel_den, pmn2v)
+    meri = einsum('pmn,pik->mnik', pmn2c, pmn2v).reshape((nf*nv,nf*nv))
+    print(meri.shape)
+    meri.fill(0.0)
+    h_rpa[:neh, :neh] = h_rpa[:neh, :neh]+meri
+    h_rpa[:neh, neh:] = h_rpa[:neh, neh:]+meri
+    h_rpa[neh:, :neh] = h_rpa[neh:, :neh]+meri
+    h_rpa[neh:, neh:] = h_rpa[neh:, neh:]+meri
+    esq, s2z = np.linalg.eigh(h_rpa)
+    print(abs(h_rpa-h_rpa.transpose()).sum())
+    print('esq', esq*27.2114)
+    
+    return 
   
   def inter_rf(self, ww):
     """ This delivers the interacting response function in the product basis"""
@@ -27,7 +77,6 @@ class qchem_inter_rf(scf):
         p2z = p2v*(2.0/(w-Omega)-2.0/(w+Omega))
         rf[iw] += np.outer(p2z,p2v)
     return rf
-
   
   def kernel_qchem_inter_rf(self, **kw):
     """ This is constructing A B matrices and diagonalizes the problem """
--- pyscf/nao/test/test_0096_h2_ae_qchem_irf.py
+++ pyscf/nao/test/test_0096_h2_ae_qchem_irf.py
@@ -1,41 +1,32 @@
 from __future__ import print_function, division
 import unittest, numpy as np
-from pyscf import gto, scf, gw as gto_gw
+from pyscf import gto, scf
+from pyscf.tddft import TDDFT
 from pyscf.nao.qchem_inter_rf import qchem_inter_rf
 from pyscf.nao import tddft_iter
 
 mol = gto.M( verbose = 1,
     atom = '''
         H    0.0   2.0      0.0
-        H   -1.0   0.0      0.0
-        H     0    0        0
-        H     0    0.757    0.587''', basis = 'cc-pVDZ',)
+        N   -1.0   0.0      0.0''', basis = 'cc-pVDZ',)
 
 class KnowValues(unittest.TestCase):
     
   def test_qchem_irf(self):
-    """ Test computation of overlaps between NAOs against overlaps computed between GTOs"""
+    """ Test """
     gto_hf = scf.RHF(mol)
     gto_hf.kernel()
     print(gto_hf.mo_energy)
+
+    nao_td  = tddft_iter(mf=gto_hf, gto=mol, xc_code='RPA')
+    eps = 0.01
+    omegas = np.arange(0.0,1.0,eps/2.0)+1j*eps
+    p_iter = -nao_td.comp_polariz_inter_ave(omegas).imag
+    data = np.array([omegas.real*27.2114, p_iter])
+    np.savetxt('NH.tddft_iter_rpa.omega.inter.pav.txt', data.T, fmt=['%f','%f'])
     
+    np.set_printoptions(linewidth=180)
     qrf = qchem_inter_rf(mf=gto_hf, gto=mol, pb_algorithm='fp', verbosity=1)
-    td = tddft_iter(mf=gto_hf, gto=mol, pb_algorithm='fp', verbosity=1, xc_code='RPA', tddft_iter_tol=1.1e-6)
     
-    ww = np.arange(0.0, 1.0, 0.1)+1j*0.2
-    rf1_qc = qrf.inter_rf(ww)
 
-    rf1_mv  = np.zeros_like(rf1_qc)
-    vec = np.zeros((td.nprod), dtype=td.dtypeComplex)
-    for iw,w in enumerate(ww):
-      for mu in range(td.nprod):
-        vec[:] = 0.0; vec[mu] = 1.0
-        veff = td.comp_veff(vec, w)
-        rf1_mv[iw, mu,:] = td.apply_rf0(veff, w)
-
-    print('abs(rf1_qc-rf1_mv).sum()', abs(rf1_qc-rf1_mv).sum())
-    for w,rf1,rf2 in zip(ww,rf1_qc,rf1_mv):
-      print(w, rf1[1,0:1], rf2[1,0:1])
-    
-    
 if __name__ == "__main__": unittest.main()
