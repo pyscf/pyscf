0e92903d894f09dda03bb144071797d6ec6a5091
Peter Koval <koval.peter@gmail.com>
2018-07-17 03:12:23 +0200

    qc rf

--- pyscf/nao/m_polariz_inter_ave.py
+++ pyscf/nao/m_polariz_inter_ave.py
@@ -1,6 +1,26 @@
 from __future__ import print_function, division
 import numpy as np
-from pyscf import lib
+
+def polariz_inter_xx(mf, gto, tddft, comega):
+  gto.set_common_orig((0.0,0.0,0.0))
+  ao_dip = gto.intor_symmetric('int1e_r', comp=3)[0]
+  occidx = np.where(mf.mo_occ==2)[0]
+  viridx = np.where(mf.mo_occ==0)[0]
+  mo_coeff = mf.mo_coeff
+  mo_energy = mf.mo_energy
+  orbv,orbo = mo_coeff[:,viridx], mo_coeff[:,occidx]
+  vo_dip = np.einsum('am,ab,bn->mn', orbv, ao_dip, orbo).reshape([len(viridx)*len(occidx)])
+
+  p = np.zeros((len(comega)), dtype=np.complex128)
+  #print(vo_dip.shape)
+  for (x,y),e in zip(tddft.xy, tddft.e):
+    #print(x.shape, y.shape)
+    dip = np.dot(vo_dip, np.sqrt(2.0)*(x+y)[0]) # Normalization ?
+    osc_strength = 2.0*(dip*dip).sum()
+    for iw,w in enumerate(comega):
+      p[iw] += osc_strength*((1.0/(w-e))-(1.0/(w+e)))
+  return p
+
 
 def polariz_inter_ave(mf, gto, tddft, comega):
   gto.set_common_orig((0.0,0.0,0.0))
@@ -23,6 +43,8 @@ def polariz_inter_ave(mf, gto, tddft, comega):
   return p
 
 def polariz_nonin_ave(mf, gto, comega):
+  from pyscf import lib
+  
   gto.set_common_orig((0.0,0.0,0.0))
   ao_dip = gto.intor_symmetric('int1e_r', comp=3)
   occidx = np.where(mf.mo_occ==2)[0]
--- pyscf/nao/m_rf_den_pyscf.py
+++ pyscf/nao/m_rf_den_pyscf.py
@@ -7,10 +7,11 @@ def rf_ov_subr(self, ww):
   """ Full matrix interacting response from tdscf class in occupied-virtual indices, for a given frequency"""
   nov = self.tdscf.xy[0][0].size
   rf_ov = np.zeros([len(ww), nov], dtype=self.dtypeComplex)
-  for e,xy in zip(self.tdscf.e, self.tdscf.xy):
+  for e,(x,y) in zip(self.tdscf.e, self.tdscf.xy):
     for iw,w in enumerate(ww):
-      xpy = xy[0]+xy[1]
-      rf_ov[iw] = rf_ov[iw] + xpy/(w-e)-xpy/(w+e)
+      xpy = -(x+y)
+      xmy =  (x-y)
+      rf_ov[iw] = rf_ov[iw] + 2*xpy/(w-e)-2*xpy/(w+e)
   return rf_ov
 
 def rf_den_pyscf(self, ww):
@@ -18,10 +19,10 @@ def rf_den_pyscf(self, ww):
   assert hasattr(self, 'tdscf')
   pov = self.get_vertex_pov()[0]
   nprd = pov.shape[0]
-  rf_ov = rf_ov_subr(self, ww).reshape([len(ww),pov.shape[1],pov.shape[2]])
+  rf_ov = rf_ov_subr(self, ww).reshape([len(ww),pov.shape[2],pov.shape[1]])
   rf = np.zeros([len(ww),nprd,nprd], dtype=self.dtypeComplex)
   for iw, ov in enumerate(rf_ov):
-    rf[iw] = np.einsum('pov,ov,qov->pq', pov, ov, pov)
+    rf[iw] = np.einsum('pov,vo,qov->pq', pov, ov, pov)
   return rf
 
 
