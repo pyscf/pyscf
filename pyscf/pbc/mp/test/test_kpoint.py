84cf550848de24530068e491b8fb520b3ec6a2e9
jdmcclain47@gmail.com <jdmcclain47@gmail.com>
2018-08-21 20:18:46 -0700

    Added extra MP2 gamma test

--- pyscf/pbc/mp/test/test_kpoint.py
+++ pyscf/pbc/mp/test/test_kpoint.py
@@ -44,11 +44,22 @@ def run_kcell(cell, nk):
     mp = pyscf.pbc.mp.kmp2.KMP2(kmf).run()
     return ekpt, mp.e_corr
 
+def run_kcell_complex(cell, nk):
+    abs_kpts = cell.make_kpts(nk, wrap_around=True)
+    kmf = pbchf.KRHF(cell, abs_kpts)
+    kmf.conv_tol = 1e-12
+    ekpt = kmf.scf()
+    kmf.mo_coeff = [kmf.mo_coeff[i].astype(np.complex128) for i in range(np.prod(nk))]
+    mp = pyscf.pbc.mp.kmp2.KMP2(kmf).run()
+    return ekpt, mp.e_corr
+
 class KnownValues(unittest.TestCase):
     def test_111(self):
         nk = (1, 1, 1)
         escf, emp = run_kcell(cell,nk)
         self.assertAlmostEqual(escf, -1.2061049658473704, 9)
+        self.assertAlmostEqual(emp, -5.44597932944397e-06, 9)
+        escf, emp = run_kcell_complex(cell,nk)
         self.assertAlmostEqual(emp, -5.44597932944397e-06, 9)
 
     def test_311_high_cost(self):
