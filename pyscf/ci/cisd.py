70582f8b63ea0efa0487f19336eb1fa5a0e5973e
Qiming Sun <osirpt.sun@gmail.com>
2018-08-28 01:31:29 -0700

    Optimize cisd overlap function

--- pyscf/ci/cisd.py
+++ pyscf/ci/cisd.py
@@ -348,11 +348,25 @@ def overlap(cibra, ciket, nmo, nocc, s=None):
     if s is None:
         return dot(cibra, ciket, nmo, nocc)
 
+    DEBUG = True
+
     nvir = nmo - nocc
     nov = nocc * nvir
-
     bra0, bra1, bra2 = cisdvec_to_amplitudes(cibra, nmo, nocc)
     ket0, ket1, ket2 = cisdvec_to_amplitudes(ciket, nmo, nocc)
+
+# Sort the ket orbitals to make the orbitals in bra one-one mapt to orbitals
+# in ket.
+    if ((not DEBUG) and
+        abs(numpy.linalg.det(s[:nocc,:nocc]) - 1) < 1e-2 and
+        abs(numpy.linalg.det(s[nocc:,nocc:]) - 1) < 1e-2):
+        ket_orb_idx = numpy.where(abs(s) > 0.9)[1]
+        s = s[:,ket_orb_idx]
+        oidx = ket_orb_idx[:nocc]
+        vidx = ket_orb_idx[nocc:] - nocc
+        ket1 = ket1[oidx[:,None],vidx]
+        ket2 = ket2[oidx[:,None,None,None],oidx[:,None,None],vidx[:,None],vidx]
+
     ooidx = numpy.tril_indices(nocc, -1)
     vvidx = numpy.tril_indices(nvir, -1)
     bra2aa = bra2 - bra2.transpose(1,0,2,3)
@@ -384,31 +398,102 @@ def overlap(cibra, ciket, nmo, nocc, s=None):
                     ia += 1
 
     na = len(occlists)
-    trans = numpy.empty((na,na))
-    for i, idx in enumerate(occlists):
-        s_sub = s[idx].T.copy()
-        minors = s_sub[occlists]
-        trans[i,:] = numpy.linalg.det(minors)
+    if DEBUG:
+        trans = numpy.empty((na,na))
+        for i, idx in enumerate(occlists):
+            s_sub = s[idx].T.copy()
+            minors = s_sub[occlists]
+            trans[i,:] = numpy.linalg.det(minors)
 
-    # Mimic the transformation einsum('ab,ap->pb', FCI, trans).
-    # The wavefunction FCI has the [excitation_alpha,excitation_beta]
-    # representation.  The zero blocks like FCI[S_alpha,D_beta],
-    # FCI[D_alpha,D_beta], are explicitly excluded.
-    bra_mat = numpy.zeros((na,na))
-    bra_mat[0,0] = bra0
-    bra_mat[0,1:1+nov] = bra_mat[1:1+nov,0] = bra1.ravel()
-    bra_mat[0,1+nov:] = bra_mat[1+nov:,0] = bra2aa.ravel()
-    bra_mat[1:1+nov,1:1+nov] = bra2.transpose(0,2,1,3).reshape(nov,nov)
-    c_s = lib.einsum('ab,ap,bq->pq', bra_mat, trans, trans)
-    ovlp  =  c_s[0,0] * ket0
-    ovlp += numpy.dot(c_s[0,1:1+nov], ket1.ravel())
-    ovlp += numpy.dot(c_s[1:1+nov,0], ket1.ravel())
-    ovlp += numpy.dot(c_s[0,1+nov:] , ket2aa.ravel())
-    ovlp += numpy.dot(c_s[1+nov:,0] , ket2aa.ravel())
-    ovlp += numpy.einsum('ijab,iajb->', ket2,
-                         c_s[1:1+nov,1:1+nov].reshape(nocc,nvir,nocc,nvir))
+        # Mimic the transformation einsum('ab,ap->pb', FCI, trans).
+        # The wavefunction FCI has the [excitation_alpha,excitation_beta]
+        # representation.  The zero blocks like FCI[S_alpha,D_beta],
+        # FCI[D_alpha,D_beta], are explicitly excluded.
+        bra_mat = numpy.zeros((na,na))
+        bra_mat[0,0] = bra0
+        bra_mat[0,1:1+nov] = bra_mat[1:1+nov,0] = bra1.ravel()
+        bra_mat[0,1+nov:] = bra_mat[1+nov:,0] = bra2aa.ravel()
+        bra_mat[1:1+nov,1:1+nov] = bra2.transpose(0,2,1,3).reshape(nov,nov)
+        ket_mat = numpy.zeros((na,na))
+        ket_mat[0,0] = ket0
+        ket_mat[0,1:1+nov] = ket_mat[1:1+nov,0] = ket1.ravel()
+        ket_mat[0,1+nov:] = ket_mat[1+nov:,0] = ket2aa.ravel()
+        ket_mat[1:1+nov,1:1+nov] = ket2.transpose(0,2,1,3).reshape(nov,nov)
+        ovlp = lib.einsum('ab,ap,bq,pq->', bra_mat, trans, trans, ket_mat)
+
+    else:
+        nov1 = 1 + nov
+        noovv = bra2aa.size
+        bra_SS = numpy.zeros((nov1,nov1))
+        bra_SS[0,0] = bra0
+        bra_SS[0,1:] = bra_SS[1:,0] = bra1.ravel()
+        bra_SS[1:,1:] = bra2.transpose(0,2,1,3).reshape(nov,nov)
+        ket_SS = numpy.zeros((nov1,nov1))
+        ket_SS[0,0] = ket0
+        ket_SS[0,1:] = ket_SS[1:,0] = ket1.ravel()
+        ket_SS[1:,1:] = ket2.transpose(0,2,1,3).reshape(nov,nov)
+
+        trans_SS = numpy.empty((nov1,nov1))
+        trans_SD = numpy.empty((nov1,noovv))
+        trans_DS = numpy.empty((noovv,nov1))
+        occlist01 = occlists[:nov1]
+        for i, idx in enumerate(occlist01):
+            s_sub = s[idx].T.copy()
+            minors = s_sub[occlist01]
+            trans_SS[i,:] = numpy.linalg.det(minors)
+
+            minors = s_sub[occlist2]
+            trans_SD[i,:] = numpy.linalg.det(minors)
+
+            s_sub = s[:,idx].copy()
+            minors = s_sub[occlist2]
+            trans_DS[:,i] = numpy.linalg.det(minors)
+
+        ovlp = lib.einsum('ab,ap,bq,pq->', bra_SS, trans_SS, trans_SS, ket_SS)
+        ovlp+= lib.einsum('ab,a ,bq, q->', bra_SS, trans_SS[:,0], trans_SD, ket2aa.ravel())
+        ovlp+= lib.einsum('ab,ap,b ,p ->', bra_SS, trans_SD, trans_SS[:,0], ket2aa.ravel())
+
+        ovlp+= lib.einsum(' b, p,bq,pq->', bra2aa.ravel(), trans_SS[0,:], trans_DS, ket_SS)
+        ovlp+= lib.einsum(' b, p,b ,p ->', bra2aa.ravel(), trans_SD[0,:], trans_DS[:,0], ket2aa.ravel())
+
+        ovlp+= lib.einsum('a ,ap, q,pq->', bra2aa.ravel(), trans_DS, trans_SS[0,:], ket_SS)
+        ovlp+= lib.einsum('a ,a , q, q->', bra2aa.ravel(), trans_DS[:,0], trans_SD[0,:], ket2aa.ravel())
+
+        # FIXME: whether to approximate the overlap between double excitation coefficients
+        if numpy.linalg.norm(bra2aa)*numpy.linalg.norm(ket2aa) < 1e-4:
+            # Skip the overlap if coefficients of double excitation are small enough
+            pass
+        if (abs(numpy.linalg.det(s[:nocc,:nocc]) - 1) < 1e-2 and
+            abs(numpy.linalg.det(s[nocc:,nocc:]) - 1) < 1e-2):
+            # If the overlap matrix close to identity enough, use the <D|D'> overlap
+            # for orthogonal single-particle basis to approximate the overlap
+            # for non-orthogonal basis.
+            ovlp+= numpy.dot(bra2aa.ravel(), ket2aa.ravel()) * trans_SS[0,0] * 2
+        else:
+            from multiprocessing import sharedctypes, Process
+            buf_ctypes = sharedctypes.RawArray('d', noovv)
+            trans_ket = numpy.ndarray(noovv, buffer=buf_ctypes)
+            def trans_dot_ket(i0, i1):
+                for i in range(i0, i1):
+                    s_sub = s[occlist2[i]].T.copy()
+                    minors = s_sub[occlist2]
+                    trans_ket[i] = numpy.linalg.det(minors).dot(ket2aa.ravel())
+
+            nproc = lib.num_threads()
+            if nproc > 1:
+                seg = (noovv+nproc-1) // nproc
+                ps = []
+                for i0,i1 in lib.prange(0, noovv, seg):
+                    p = Process(target=trans_dot_ket, args=(i0,i1))
+                    ps.append(p)
+                    p.start()
+                [p.join() for p in ps]
+            else:
+                trans_dot_ket(0, noovv)
+
+            ovlp+= numpy.dot(bra2aa.ravel(), trans_ket) * trans_SS[0,0] * 2
+
     return ovlp
-
 
 def make_rdm1(myci, civec=None, nmo=None, nocc=None):
     '''
