cc432fb5fbb448f09936639c4414a702a5203534
Qiming Sun <osirpt.sun@gmail.com>
2018-12-02 14:19:03 -0800

    Update pbc.df test

--- pyscf/pbc/df/test/test_aft_jk.py
+++ pyscf/pbc/df/test/test_aft_jk.py
@@ -48,7 +48,7 @@ class KnowValues(unittest.TestCase):
         nao = cell.nao_nr()
         dm = numpy.random.random((nao,nao))
         dm = dm + dm.T
-        vj1, vk1 = mydf.get_jk(dm, hermi=0)
+        vj1, vk1 = mydf.get_jk(dm, hermi=0, exxdiv='ewald')
         ej1 = numpy.einsum('ij,ji->', vj1, dm)
         ek1 = numpy.einsum('ij,ji->', vk1, dm)
         self.assertAlmostEqual(ej1, 12.234106555081793, 9)
