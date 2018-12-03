e284968b7e234b30a8a593b298a96b78bb151c03
Artem <gpulkin@gmail.com>
2018-12-03 15:54:19 -0800

    Extend a docstring in PBC-Gamma TDHF

--- pyscf/pbc/tdscf/rhf_slow.py
+++ pyscf/pbc/tdscf/rhf_slow.py
@@ -1,4 +1,18 @@
 """
-This module is an alias for `pyscf.tdscf.rhf_slow`. It works with single k-point HF objects.
+This and other `_slow` modules implement the time-dependent Hartree-Fock procedure. The primary performance drawback is
+that, unlike other 'fast' routines with an implicit construction of the eigenvalue problem, these modules construct
+TDHF matrices explicitly via an AO-MO transformation, i.e. with a O(N^5) complexity scaling. As a result, regular
+`numpy.linalg.eig` can be used to retrieve TDHF roots in a reliable fashion without any issues related to the Davidson
+procedure. Several variants of TDHF are available:
+
+ * `pyscf.tdscf.rhf.slow`: the molecular implementation;
+ * (this module) `pyscf.pbc.tdscf.rhf_slow`: PBC (periodic boundary condition) implementation for RHF objects of
+   `pyscf.pbc.scf` modules;
+ * `pyscf.pbc.tdscf.krhf_slow_supercell`: PBC implementation for KRHF objects of `pyscf.pbc.scf` modules. Works with
+   an arbitrary number of k-points but has a overhead due to an effective construction of a supercell.
+ * `pyscf.pbc.tdscf.krhf_slow`: PBC implementation for KRHF objects of `pyscf.pbc.scf` modules. Works with an arbitrary
+   number of k-points and employs k-point conservation (diagonalizes matrix blocks separately).
 """
+
+# This module is simply an alias of the molecular code
 from pyscf.tdscf.rhf_slow import *
