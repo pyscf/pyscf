8c1ab4929837786dc86ebb9cdbb3926fc436dd87
James McClain <jdmcclain47@gmail.com>
2019-01-28 16:52:54 -0800

    slight changes to kuccsd eom test

--- pyscf/pbc/cc/test/test_eom_uccsd.py
+++ pyscf/pbc/cc/test/test_eom_uccsd.py
@@ -1,77 +1,69 @@
+import make_test_cell
 from pyscf.pbc.tools.pbc import super_cell
-from pyscf.pbc import gto, scf, cc
+from pyscf.pbc import gto
+from pyscf.pbc import cc as pbcc
+from pyscf.pbc import scf as pbcscf
 from pyscf.pbc.cc.eom_kccsd_uhf import EOMIP, EOMEA
-from pyscf.pbc.cc.eom_kccsd_rhf import EOMIP as rEOMIP
-from pyscf.pbc.cc.eom_kccsd_rhf import EOMEA as rEOMEA
 from pyscf.cc import eom_uccsd
 import unittest
 
-class Test(unittest.TestCase):
-    def test_he(self):
-        cell = gto.Cell()
-        cell.atom='''
-        C 0.000000000000   0.000000000000   0.000000000000
-        C 1.685068664391   1.685068664391   1.685068664391
-        '''
-        cell.basis = { 'C': [[0, (0.8, 1.0)],
-                            [1, (1.0, 1.0)]]}
-        cell.pseudo = 'gth-pade'
-        cell.a = '''
-        0.000000000, 3.370137329, 3.370137329
-        3.370137329, 0.000000000, 3.370137329
-        3.370137329, 3.370137329, 0.000000000'''
-        cell.unit = 'B'
-        cell.verbose = 5
-        cell.build()
+cell_n3d = make_test_cell.test_cell_n3_diffuse()
+kmf = pbcscf.KRHF(cell_n3d, cell_n3d.make_kpts((1,1,2), with_gamma_point=True), exxdiv=None)
+kmf.conv_tol = 1e-10
+kmf.scf()
 
+def tearDownModule():
+    global cell_n3d, kmf
+    del cell_n3d, kmf
+
+class KnownValues(unittest.TestCase):
+    def test_n3_diffuse(self):
+        self.assertAlmostEqual(kmf.e_tot, -6.1870676561721227, 6)
+        cell = make_test_cell.test_cell_n3_diffuse()
         nmp = [1,1,2]
-        ##nmp = [1,1,1]
-        ##'''
-        ## treating 1*1*2 supercell at gamma point
-        #supcell = super_cell(cell,nmp)
-        #gmf  = scf.UHF(supcell,exxdiv=None)
-        #ehf  = gmf.kernel()
-        #gcc  = cc.UCCSD(gmf)
-        #gcc.conv_tol=1e-12
-        #gcc.conv_tol_normt=1e-10
-        #gcc.max_cycle=250
-        #ecc, t1, t2 = gcc.kernel()
-        #print('UHF energy (supercell) %.7f \n' % (float(ehf)/2.))
-        #print('UCCSD correlation energy (supercell) %.7f \n' % (float(ecc)/2.))
-
+        '''
+        # treating 1*1*2 supercell at gamma point
+        supcell = super_cell(cell,nmp)
+        gmf  = scf.UHF(supcell,exxdiv=None)
+        ehf  = gmf.kernel()
+        gcc  = cc.UCCSD(gmf)
+        gcc.conv_tol=1e-12
+        gcc.conv_tol_normt=1e-10
+        gcc.max_cycle=250
+        ecc, t1, t2 = gcc.kernel()
+        print('UHF energy (supercell) %.7f \n' % (float(ehf)/2.))
+        print('UCCSD correlation energy (supercell) %.7f \n' % (float(ecc)/2.))
         #eom = eom_uccsd.EOMIP(gcc)
         #e1, v = eom.ipccsd(nroots=2)
         #eom = eom_uccsd.EOMEA(gcc)
         #e2, v = eom.eaccsd(nroots=2, koopmans=True)
-        ##'''
-        #exit()
-        # Running HF and CCSD with 1x1x2 Monkhorst-Pack k-point mesh
-        #kmf = scf.KUHF(cell, kpts=cell.make_kpts(nmp), exxdiv=None)
-        kmf = scf.KRHF(cell, kpts=cell.make_kpts(nmp), exxdiv=None)
-        ehf2 = kmf.kernel()
-
-        mycc = cc.KUCCSD(kmf)
-        #mycc = cc.KRCCSD(kmf)
+        '''
+        mycc = pbcc.KUCCSD(kmf)
         mycc.conv_tol = 1e-7
         mycc.conv_tol_normt = 1e-7
-        mycc.max_cycle=250
         ecc2, t1, t2 = mycc.kernel()
-        print('UHF energy %.7f \n' % (float(ehf2)))
-        print('UCCSD correlation energy  %.7f \n' % (float(ecc2)))
+        self.assertAlmostEqual(ecc2, -0.0676483711263548, 6)
 
-        #EOM-CCSD root 0 E = -1.23768083043397  qpwt = 0.967562
-        #EOM-CCSD root 1 E = -1.215047888039312  qpwt = 0.964023
-        #EOM-CCSD root 2 E = -1.183328480541004  qpwt = 0.976061
-        #EOM-CCSD root 3 E = -1.183328552049644  qpwt = 0.976061
         eom = EOMIP(mycc)
         e1_obt, v = eom.ipccsd(nroots=3, kptlist=[0])
-        ##eom = rEOMIP(mycc)
-        ##e1_obt, v = eom.ipccsd(nroots=4, koopmans=True, kptlist=[0])
-        ##eom = EOMEA(mycc)
-        ##e2_obt, v = eom.eaccsd(nroots=2, koopmans=True, kptlist=[0])
-        ##print(e1_obt)
-        ##print(e2_obt)
-        ##assert ehf/2 - ehf2 < 1e-10
-        ##assert ecc/2 - ecc2 < 1e-10
-        ##assert e1[0]-(e1_obt[0][0]) < 1e-7
-        ##assert e2[0]-(e2_obt[0][0]) < 1e-7
+        self.assertAlmostEqual(e1_obt[0][0], -1.148946994550331, 6)
+        self.assertAlmostEqual(e1_obt[0][1], -1.148946994544336, 6)
+        self.assertAlmostEqual(e1_obt[0][2], -1.10881945217663, 6)
+
+        eom = EOMEA(mycc)
+        e2_obt, v = eom.eaccsd(nroots=3, koopmans=True, kptlist=[0])
+        self.assertAlmostEqual(e2_obt[0][0], 1.26697897719539, 6)
+        self.assertAlmostEqual(e2_obt[0][1], 1.26697897720534, 6)
+        self.assertAlmostEqual(e2_obt[0][2], 1.278883205460933, 6)
+
+        eom = EOMIP(mycc)
+        e1_obt, v = eom.ipccsd(nroots=3, koopmans=True, kptlist=[1])
+        self.assertAlmostEqual(e1_obt[0][0], -0.9074337295664157, 6)
+        self.assertAlmostEqual(e1_obt[0][1], -0.9074337295664148, 6)
+        self.assertAlmostEqual(e1_obt[0][2], -0.9074331791699104, 6)
+
+        eom = EOMEA(mycc)
+        e2_obt, v = eom.eaccsd(nroots=3, koopmans=True, kptlist=[1])
+        self.assertAlmostEqual(e2_obt[0][0], 1.227583017460536, 6)
+        self.assertAlmostEqual(e2_obt[0][1], 1.227583017460617, 6)
+        self.assertAlmostEqual(e2_obt[0][2], 1.383037918699404, 6)
