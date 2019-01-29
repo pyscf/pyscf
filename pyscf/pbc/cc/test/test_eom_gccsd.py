d036e587ff589d852d48d8d42ae9f021d45179be
James McClain <jdmcclain47@gmail.com>
2019-01-28 16:43:15 -0800

    slight changes to test_eom_gccsd.py

--- pyscf/pbc/cc/test/test_eom_gccsd.py
+++ pyscf/pbc/cc/test/test_eom_gccsd.py
@@ -1,29 +1,18 @@
+import make_test_cell
 from pyscf.pbc.tools.pbc import super_cell
 from pyscf.pbc import gto, scf, cc
 from pyscf.pbc.cc.eom_kccsd_ghf import EOMIP, EOMEA
 from pyscf.cc import eom_gccsd
 import unittest
 
-class Test(unittest.TestCase):
-    def test_dmd_high_cost(self):
-        cell = gto.Cell()
-        cell.atom='''
-        C 0.000000000000   0.000000000000   0.000000000000
-        C 1.685068664391   1.685068664391   1.685068664391
-        '''
-        cell.basis = { 'C': [[0, (0.8, 1.0)],
-                            [1, (1.0, 1.0)]]}
-        cell.pseudo = 'gth-pade'
-        cell.a = '''
-        0.000000000, 3.370137329, 3.370137329
-        3.370137329, 0.000000000, 3.370137329
-        3.370137329, 3.370137329, 0.000000000'''
-        cell.unit = 'B'
-        cell.verbose = 5
-        cell.build()
+cell = make_test_cell.test_cell_n3_diffuse()
+
+class KnownValues(unittest.TestCase):
+    def test_n3_diffuse(self):
+        cell = make_test_cell.test_cell_n3_diffuse()
 
         nmp = [1,1,2]
-        
+        '''
         # treating 1*1*2 supercell at gamma point
         supcell = super_cell(cell,nmp)
         gmf  = scf.GHF(supcell,exxdiv=None)
@@ -35,31 +24,43 @@ class Test(unittest.TestCase):
         ecc, t1, t2 = gcc.kernel()
         print('GHF energy (supercell) %.7f \n' % (float(ehf)/2.))
         print('GCCSD correlation energy (supercell) %.7f \n' % (float(ecc)/2.))
-    
+
         eom = eom_gccsd.EOMIP(gcc)
         e1, v = eom.ipccsd(nroots=2)
         eom = eom_gccsd.EOMEA(gcc)
         e2, v = eom.eaccsd(nroots=2, koopmans=True)
-
+        '''
         # Running HF and CCSD with 1x1x2 Monkhorst-Pack k-point mesh
-        kmf = scf.KGHF(cell, kpts=cell.make_kpts(nmp), exxdiv=None)
+        kmf = scf.KGHF(cell, kpts=cell.make_kpts(nmp, with_gamma_point=True), exxdiv=None)
         ehf2 = kmf.kernel()
+        self.assertAlmostEqual(ehf2, -6.1870676561725695, 6)
 
         mycc = cc.KGCCSD(kmf)
-        mycc.conv_tol = 1e-12
-        mycc.conv_tol_normt = 1e-10
-        mycc.max_cycle=250
+        mycc.conv_tol = 1e-7
+        mycc.conv_tol_normt = 1e-7
         ecc2, t1, t2 = mycc.kernel()
-        print('GHF energy %.7f \n' % (float(ehf2)))
-        print('GCCSD correlation energy  %.7f \n' % (float(ecc2)))
+        self.assertAlmostEqual(ecc2, -0.0676483716898783, 6)
 
-        kptlist = cell.make_kpts(nmp)
         eom = EOMIP(mycc)
-        e1_obt, v = eom.ipccsd(nroots=2, kptlist=[0])
-        eom = EOMEA(mycc)
-        e2_obt, v = eom.eaccsd(nroots=2, koopmans=True, kptlist=[0])
+        e1_obt, v = eom.ipccsd(nroots=3, kptlist=[0])
+        self.assertAlmostEqual(e1_obt[0][0], -1.1489469962099519, 6)
+        self.assertAlmostEqual(e1_obt[0][1], -1.1489469961858796, 6)
+        self.assertAlmostEqual(e1_obt[0][2], -1.1088194518036925, 6)
 
-        assert(ehf/2 - ehf2 < 1e-10)
-        assert(ecc/2 - ecc2 < 1e-10)
-        assert(e1[0]-(e1_obt[0][0]) < 1e-7)
-        assert(e2[0]-(e2_obt[0][0]) < 1e-7)
+        eom = EOMEA(mycc)
+        e2_obt, v = eom.eaccsd(nroots=3, kptlist=[0])
+        self.assertAlmostEqual(e2_obt[0][0], 1.2669788613362731, 6)
+        self.assertAlmostEqual(e2_obt[0][1], 1.2669788614703625, 6)
+        self.assertAlmostEqual(e2_obt[0][2], 1.278883205515518, 6)
+
+        eom = EOMIP(mycc)
+        e1_obt, v = eom.ipccsd(nroots=2, koopmans=True, kptlist=[1])
+        self.assertAlmostEqual(e1_obt[0][0], -0.9074337292436309, 6)
+        self.assertAlmostEqual(e1_obt[0][1], -0.9074337292161299, 6)
+        self.assertAlmostEqual(e1_obt[0][2], -0.9074331788469051, 6)
+
+        eom = EOMEA(mycc)
+        e2_obt, v = eom.eaccsd(nroots=3, koopmans=True, kptlist=[1])
+        self.assertAlmostEqual(e2_obt[0][0], 1.227583017804503, 6)
+        self.assertAlmostEqual(e2_obt[0][1], 1.2275830178298166, 6)
+        self.assertAlmostEqual(e2_obt[0][2], 1.3830379190440196, 6)
