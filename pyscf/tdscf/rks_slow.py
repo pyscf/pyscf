309f0b760f5b4a24a2ce05175249faf4753dede1
Artem <gpulkin@gmail.com>
2019-02-01 15:35:19 +0100

    Proxy implementation of molecular TD RKS

---
+++ pyscf/tdscf/rks_slow.py
@@ -0,0 +1,145 @@
+#  Author: Artem Pulkin
+"""
+This and other `_slow` modules implement the time-dependent Kohn-Sham procedure. The primary performance drawback is
+that, unlike other 'fast' routines with an implicit construction of the eigenvalue problem, these modules construct
+TDHF matrices explicitly by proxying to pyscf density response routines with a O(N^4) complexity scaling. As a result,
+regular `numpy.linalg.eig` can be used to retrieve TDKS roots in a reliable fashion without any issues related to the
+Davidson procedure. Several variants of TDKS are available:
+
+ * (this module) `pyscf.tdscf.rks_slow`: the molecular implementation;
+ * `pyscf.pbc.tdscf.rks_slow`: PBC (periodic boundary condition) implementation for RKS objects of `pyscf.pbc.scf`
+ modules;
+ * `pyscf.pbc.tdscf.krks_slow_supercell`: PBC implementation for KRKS objects of `pyscf.pbc.scf` modules. Works with
+   an arbitrary number of k-points but has a overhead due to an effective construction of a supercell.
+ * `pyscf.pbc.tdscf.krks_slow_gamma`: A Gamma-point calculation resembling the original `pyscf.pbc.tdscf.krks`
+   module. Despite its name, it accepts KRKS objects with an arbitrary number of k-points but finds only few TDKS roots
+   corresponding to collective oscillations without momentum transfer;
+ * `pyscf.pbc.tdscf.krks_slow`: PBC implementation for KRKS objects of `pyscf.pbc.scf` modules. Works with
+   an arbitrary number of k-points and employs k-point conservation (diagonalizes matrix blocks separately).
+"""
+
+# Convention for these modules:
+# * PhysERI, PhysERI4, PhysERI8 are proxy classes for computing the full TDDFT matrix
+# * vector_to_amplitudes reshapes and normalizes the solution
+# * TDRKS provides a container
+
+from pyscf.tdscf import rhf_slow as tdhf
+import pyscf.tdscf as proxy
+
+import numpy
+
+
+def rotate_proxy(m, o, v, return_ov=False):
+    """
+    Rotates the output of pyscf TDDFT matrix to convention.
+    Args:
+        m (ndarray): the TDDFT matrix to diagonalize;
+        o (ndarray): occupied orbital energies;
+        v (ndarray): virtual orbital energies;
+        return_ov (bool): if True, returns the K-matrix as well;
+
+    Returns:
+        The rotated matrix as well as .
+    """
+    e_ov = (v[numpy.newaxis, :] - o[:, numpy.newaxis]).reshape(-1)
+    e_ov_sqr = e_ov ** .5
+    result = m * (e_ov_sqr[numpy.newaxis, :] / e_ov_sqr[:, numpy.newaxis])
+    if return_ov:
+        return result, numpy.diag(e_ov)
+    else:
+        return result
+
+
+class PhysERI(tdhf.PhysERI):
+
+    def __init__(self, model, frozen=None):
+        """
+        A proxy class for calculating the TDKS matrix blocks.
+
+        Args:
+            model (RKS): the base model;
+            frozen (int, Iterable): the number of frozen valence orbitals or the list of frozen orbitals;
+        """
+        tdhf.TDDFTMatrixBlocks.__init__(self)
+        self.model = model
+        self.proxy_model = proxy.TDDFT(model)
+        self.space = tdhf.format_frozen(frozen, len(model.mo_energy))
+
+    def ao2mo(self, coeff):
+        """
+        Phys ERI in MO basis.
+        Args:
+            coeff (Iterable): MO orbitals;
+
+        Returns:
+            ERI in MO basis.
+        """
+        raise RuntimeError("No calls to ao2mo expected in the time-dependent KS workflow")
+
+    def tdhf_a(self, *args, **kwargs):
+        """
+        The TDHF A-matrix.
+        Returns:
+            The matrix.
+        """
+        raise RuntimeError("This is a proxy class: no calls to A and B matrices expected: only 'fast_tdhf_matrix_set'"
+                           " can be used")
+
+    def tdhf_b(self, *args, **kwargs):
+        """
+        The TDHF B-matrix.
+        Returns:
+            The matrix.
+        """
+        raise RuntimeError("This is a proxy class: no calls to A and B matrices expected: only 'fast_tdhf_matrix_set'"
+                           " can be used")
+
+    def tdhf_matrix(self, *args, **kwargs):
+        """
+        Full matrix of the TDKS problem.
+        Returns:
+            The matrix.
+        """
+        raise RuntimeError("This is a proxy class: no calls to the full TDHF matrix expected: only"
+                           " 'fast_tdhf_matrix_set' can be used")
+
+    def fast_tdhf_matrix_set(self):
+        """
+        A set of real tdks matrixes to perform an optimized diagonlaization.
+        Returns:
+            The matrix to diagonalize as well as the matrix to determine the second half of the TD KS solution.
+        """
+        # Retrieve matrix in the active space specified
+        vind, hdiag = self.proxy_model.gen_vind(self.model)
+
+        nmo_full = len(self.space)
+        nocc_full = int(self.model.mo_occ.sum() // 2)
+        nvirt_full = nmo_full - nocc_full
+        size_full = nocc_full * nvirt_full
+
+        nmo = self.nmo
+        nocc = self.nocc
+        nvirt = nmo - nocc
+        size = nocc * nvirt
+
+        probe = numpy.zeros((size, size_full))
+
+        o = self.space[:nocc_full]
+        v = self.space[nocc_full:]
+        ov = (o[:, numpy.newaxis] * v[numpy.newaxis, :]).reshape(-1)
+
+        probe[numpy.arange(size), numpy.argwhere(ov)[:, 0]] = 1
+        result = vind(probe).T[ov, :]
+
+        # The TD matrix is not exactly the output of gen_vind: some rotation is needed
+        e_occ = self.mo_energy[:nocc]
+        e_virt = self.mo_energy[nocc:]
+        return rotate_proxy(result, e_occ, e_virt, return_ov=True)
+
+
+vector_to_amplitudes = tdhf.vector_to_amplitudes
+
+
+class TDRKS(tdhf.TDRHF):
+    eri1 = PhysERI
+    eri4 = eri8 = None
---
+++ pyscf/tdscf/test/test_rks_slow.py
@@ -0,0 +1,85 @@
+from pyscf.gto import Mole
+from pyscf.scf import RKS
+from pyscf.tdscf import TDDFT
+from pyscf.tdscf.rks_slow import PhysERI, TDRKS, rotate_proxy
+
+import numpy
+from numpy import testing
+import unittest
+
+from test_common import retrieve_m, assert_vectors_close, tdhf_frozen_mask
+
+
+class H20Test(unittest.TestCase):
+    """Compare this (rks_slow) vs reference."""
+    @classmethod
+    def setUpClass(cls):
+        cls.mol = mol = Mole()
+        mol.atom = [
+            [8, (0., 0., 0.)],
+            [1, (0., -0.757, 0.587)],
+            [1, (0., 0.757, 0.587)]]
+
+        mol.basis = 'cc-pvdz'
+        mol.verbose = 5
+        mol.build()
+
+        cls.model_rks = model_rks = RKS(mol)
+        model_rks.kernel()
+
+        cls.td_model_rks = td_model_rks = TDDFT(model_rks)
+        td_model_rks.nroots = 4
+        td_model_rks.kernel()
+
+        e = model_rks.mo_energy
+        nocc = int(sum(model_rks.mo_occ) // 2)
+        cls.ref_m = rotate_proxy(retrieve_m(td_model_rks), e[:nocc], e[nocc:])
+
+    @classmethod
+    def tearDownClass(cls):
+        # These are here to remove temporary files
+        del cls.td_model_rks
+        del cls.model_rks
+        del cls.mol
+
+    def test_eri(self):
+        """Tests all ERI implementations: with and without symmetries."""
+        e = PhysERI(self.model_rks)
+        mk, _ = e.fast_tdhf_matrix_set()
+        testing.assert_allclose(self.ref_m, mk, atol=1e-13)
+
+        # Test frozen
+        for frozen in (1, [0, -1]):
+            try:
+                e = PhysERI(self.model_rks, frozen=frozen)
+                mk, _ = e.fast_tdhf_matrix_set()
+                ov_mask = tdhf_frozen_mask(e, kind="1ov")
+                ref_m = self.ref_m[numpy.ix_(ov_mask, ov_mask)]
+                testing.assert_allclose(ref_m, mk, atol=1e-13)
+
+            except Exception:
+                print("When testing with frozen={} the following exception occurred:".format(repr(frozen)))
+                raise
+
+    def test_class(self):
+        """Tests container behavior."""
+        model = TDRKS(self.model_rks)
+        model.nroots = self.td_model_rks.nroots
+        model.kernel()
+        testing.assert_allclose(model.e, self.td_model_rks.e, atol=1e-5)
+        assert_vectors_close(model.xy, self.td_model_rks.xy, atol=1e-2)
+
+    def test_class_frozen(self):
+        """Tests container behavior."""
+        for frozen in (1, [0, -1]):
+            try:
+                model = TDRKS(self.model_rks, frozen=frozen)
+                model.nroots = self.td_model_rks.nroots
+                model.kernel()
+                mask_o, mask_v = tdhf_frozen_mask(model.eri, kind="o,v")
+                testing.assert_allclose(model.e, self.td_model_rks.e, atol=1e-3)
+                assert_vectors_close(model.xy, numpy.array(self.td_model_rks.xy)[..., mask_o, :][..., mask_v], atol=1e-3)
+
+            except Exception:
+                print("When testing class with frozen={} the following exception occurred:".format(repr(frozen)))
+                raise
