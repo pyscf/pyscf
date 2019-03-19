262d976c85d753f4e03c0188aef0e2fb03d99e98
Xiao Wang <xiaowang862@gmail.com>
2019-03-18 22:37:09 -0400

    Added supercell EOM-EE-GCCSD for Gamma-point calculations. Added diamond.

--- examples/pbc/44-k_point_eom_ccsd_ghf.py
+++ examples/pbc/44-k_point_eom_ccsd_ghf.py
@@ -9,51 +9,93 @@ Showing use of general EOM-CCSD with K-point sampling.
 import numpy as np
 from pyscf.pbc import gto, scf, cc
 from pyscf.pbc.cc import eom_kccsd_ghf as eom_kgccsd
+from pyscf import cc as molcc
+from pyscf.cc.gccsd import GCCSD
+from pyscf.cc import eom_gccsd as mol_eom_gccsd
+from pyscf.pbc.tools.pbc import super_cell
 
 
 cell = gto.Cell()
 cell.verbose = 4
 cell.unit = 'B'
-cell.mesh = [5, 5, 5]
 
 #
 # Hydrogen crystal
 #
-cell.a = np.eye(3) * 4.2
-cell.basis = 'sto-3g'
-cell.atom = '''
-    H 0.000000000000   0.000000000000   0.000000000000
-    H 0.000000000000   0.000000000000   1.400000000000
-    '''
+# cell.a = np.eye(3) * 7.0
+# cell.basis = '3-21g'
+# cell.atom = '''
+#     H 0.000000000000   0.000000000000   0.000000000000
+#     H 0.000000000000   0.000000000000   1.400000000000
+#     '''
 
 #
 # Helium crystal
 #
+cell.atom = '''
+He 0.000000000000   0.000000000000   0.000000000000
+He 1.685068664391   1.685068664391   1.685068664391
+'''
+cell.basis = [[0, (1., 1.)], [0, (.5, 1.)]]
+cell.a = '''
+0.000000000, 3.370137329, 3.370137329
+3.370137329, 0.000000000, 3.370137329
+3.370137329, 3.370137329, 0.000000000
+'''
+
+#
+# Diamond
+#
 # cell.atom = '''
-# He 0.000000000000   0.000000000000   0.000000000000
-# He 1.685068664391   1.685068664391   1.685068664391
+# C 0.000000000000   0.000000000000   0.000000000000
+# C 1.685068664391   1.685068664391   1.685068664391
 # '''
-# cell.basis = [[0, (1., 1.)], [0, (.5, 1.)]]
+# cell.basis = 'gth-szv'
+# cell.pseudo = 'gth-pade'
 # cell.a = '''
 # 0.000000000, 3.370137329, 3.370137329
 # 3.370137329, 0.000000000, 3.370137329
-# 3.370137329, 3.370137329, 0.000000000
-# '''
+# 3.370137329, 3.370137329, 0.000000000'''
+
 
 cell.build()
 
+nmp = [2,2,2]
+
 # KRHF
-kpts = cell.make_kpts([1,1,3])
-kmf = scf.KRHF(cell, kpts=kpts, exxdiv=None)
-euhf = kmf.kernel()
+kpts = cell.make_kpts(nmp)
+kmf = scf.KRHF(cell, kpts=kpts, exxdiv=None).density_fit()
+ekrhf = kmf.kernel()
 
 # KGCCSD
 mycc = cc.KGCCSD(kmf)
-egccsd = mycc.kernel()
+ekgcc, t1, t2 = mycc.kernel()
 
-# EOM-KGCCSD
-myeomip = eom_kgccsd.EOMIP(mycc)
-eip, vip = myeomip.kernel(nroots=2)
+nroots_test = 4
+myeomee = eom_kgccsd.EOMEE(mycc)
+eee, vee = myeomee.kernel(nroots=nroots_test)
 
-myeomea = eom_kgccsd.EOMEA(mycc)
-eea, vea = myeomea.kernel(nroots=2)
+# Supercell
+scell = super_cell(cell, nmp)
+
+# PBC Gamma-point RHF based on supercell
+mf = scf.RHF(scell, exxdiv=None).density_fit()
+erhf = mf.kernel()
+
+# Molecular GCCSD
+mf = scf.addons.convert_to_ghf(mf)
+mycc = GCCSD(mf)
+egcc, t1, t2 = mycc.kernel()
+
+# Molecular EOM-GCCSD
+myeomee = mol_eom_gccsd.EOMEE(mycc)
+eee_mol, vee_mol = myeomee.kernel(nroots=nroots_test*2)
+
+print("PBC KRHF Energy:", ekrhf)
+print("PBC RHF Energy :", erhf)
+print("PBC KGCCSD Energy        :", ekgcc)
+print("Mol GCCSD Energy per cell:", egcc / np.product(nmp))
+print("PBC EOMEE roots:", eee)
+print("Mol EOMEE roots:", eee_mol)
+
+
