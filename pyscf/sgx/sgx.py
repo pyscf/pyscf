df4f674ec843accd9c2eed30a0913e61673c04a9
Qiming Sun <osirpt.sun@gmail.com>
2019-07-13 02:06:50 -0700

    Fix ao2mo.semi_incore

--- pyscf/sgx/sgx.py
+++ pyscf/sgx/sgx.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright 2014-2018 The PySCF Developers. All Rights Reserved.
+# Copyright 2018-2019 The PySCF Developers. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
