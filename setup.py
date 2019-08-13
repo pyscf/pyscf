9b313edb5e7e84b8d4e2c516aac5e3464dbea519
Qiming Sun <osirpt.sun@gmail.com>
2019-08-12 17:22:50 -0700

    Add example for tddft excited state density

--- setup.py
+++ setup.py
@@ -48,6 +48,8 @@ CLASSIFIERS = [
 'Programming Language :: Python :: 3.4',
 'Programming Language :: Python :: 3.5',
 'Programming Language :: Python :: 3.6',
+'Programming Language :: Python :: 3.7',
+'Programming Language :: Python :: 3.8',
 'Topic :: Software Development',
 'Topic :: Scientific/Engineering',
 'Operating System :: POSIX',
