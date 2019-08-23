0496f1d6e9f18754b08b39663cd4a318b3c16099
Qiming Sun <osirpt.sun@gmail.com>
2019-08-22 22:24:06 -0700

    Add cubegen.read function

--- pyscf/tools/cubegen.py
+++ pyscf/tools/cubegen.py
@@ -265,7 +265,27 @@ class Cube(object):
                         f.write(fmt % tuple(field[ix,iy,iz0:iz1].tolist()))
 
     def read(self, cube_file):
-        raise NotImplementedError
+        with open(cube_file, 'r') as f:
+            f.readline()
+            f.readline()
+            data = f.readline().split()
+            natm = int(data[0])
+            self.boxorig = numpy.array([float(x) for x in data[1:]])
+            def parse_nx(data):
+                d = data.split()
+                return int(d[0]), numpy.array([float(x) for x in d[1:]])
+            self.nx, self.xs = parse_nx(f.readline())
+            self.ny, self.ys = parse_nx(f.readline())
+            self.nz, self.zs = parse_nx(f.readline())
+            atoms = []
+            for ia in range(natm):
+                d = f.readline().split()
+                atoms.append([int(d[0]), [float(x) for x in d[2:]]])
+            self.mol = gto.M(atom=atoms, unit='Bohr')
+
+            data = f.read()
+        cube_data = numpy.array([float(x) for x in data.split()])
+        return cube_data.reshape([self.nx, self.ny, self.nz])
 
 
 if __name__ == '__main__':
@@ -278,4 +298,3 @@ if __name__ == '__main__':
     cubegen.density(mol, 'h2o_den.cube', mf.make_rdm1()) #makes total density
     cubegen.mep(mol, 'h2o_pot.cube', mf.make_rdm1())
     cubegen.orbital(mol, 'h2o_mo1.cube', mf.mo_coeff[:,0])
-
