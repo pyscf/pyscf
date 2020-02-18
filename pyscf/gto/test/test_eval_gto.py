pyscf/pyscf

commit b6db4d03d85e3a6e5a035ca7e967c6566028b114

Remove negative address in basis evalulation

pyscf/lib/gto/deriv2.c
@@ -1006,9 +1006,9 @@ void GTOshell_eval_grid_cart_deriv1(double *gto, double *ri, double *exps,
                                         ypows[0] = 1;
                                         zpows[0] = 1;
                                         for (lx = 1; lx <= l; lx++) {
-                                                xpows[lx] = xpows[lx-1] *gridx[i];
-                                                ypows[lx] = ypows[lx-1] *gridy[i];
-                                                zpows[lx] = zpows[lx-1] *gridz[i];
+                                                xpows[lx+1] = xpows[lx] *gridx[i];
+                                                ypows[lx+1] = ypows[lx] *gridy[i];
+                                                zpows[lx+1] = zpows[lx] *gridz[i];
                                         }
                                         for (lx = l, n = 0; lx >= 0; lx--) {
                                         for (ly = l - lx; ly >= 0; ly--, n++) {
@@ -1018,9 +1018,9 @@ void GTOshell_eval_grid_cart_deriv1(double *gto, double *ri, double *exps,
                                                 gtox[n*ngrids+i] = exps_2a[k*BLKSIZE+i] * gridx[i] * tmp;
                                                 gtoy[n*ngrids+i] = exps_2a[k*BLKSIZE+i] * gridy[i] * tmp;
                                                 gtoz[n*ngrids+i] = exps_2a[k*BLKSIZE+i] * gridz[i] * tmp;
-                                                gtox[n*ngrids+i] += exps[k*BLKSIZE+i] * lx * xpows[lx-1] * ypows[ly] * zpows[lz];
-                                                gtoy[n*ngrids+i] += exps[k*BLKSIZE+i] * ly * xpows[lx] * ypows[ly-1] * zpows[lz];
-                                                gtoz[n*ngrids+i] += exps[k*BLKSIZE+i] * lz * xpows[lx] * ypows[ly] * zpows[lz-1];
+                                                gtox[n*ngrids+i] += exps[k*BLKSIZE+i] * lx * xpows_1less_in_power[lx] * ypows[ly] * zpows[lz];
+                                                gtoy[n*ngrids+i] += exps[k*BLKSIZE+i] * ly * xpows[lx] * ypows_1less_in_power[ly] * zpows[lz];
+                                                gtoz[n*ngrids+i] += exps[k*BLKSIZE+i] * lz * xpows[lx] * ypows[ly] * zpows_1less_in_power[lz];
                                         } }
                                 } else {
                                         for (n = 0; n < degen; n++) {

pyscf/lib/gto/deriv1.c
@@ -487,10 +487,10 @@ void GTOshell_eval_grid_ip_cart(double *gto, double *ri, double *exps,
                                         xpows[0] = 1;
                                         ypows[0] = 1;
                                         zpows[0] = 1;
-                                        for (lx = 1; lx <= l; lx++) {
-                                                xpows[lx] = xpows[lx-1] *gridx[i];
-                                                ypows[lx] = ypows[lx-1] *gridy[i];
-                                                zpows[lx] = zpows[lx-1] *gridz[i];
+                                        for (lx = 0; lx < l; lx++) {
+                                                xpows[lx+1] = xpows[lx] *gridx[i];
+                                                ypows[lx+1] = ypows[lx] *gridy[i];
+                                                zpows[lx+1] = zpows[lx] *gridz[i];
                                         }
                                         for (lx = l, n = 0; lx >= 0; lx--) {
                                         for (ly = l - lx; ly >= 0; ly--, n++) {
@@ -499,9 +499,9 @@ void GTOshell_eval_grid_ip_cart(double *gto, double *ri, double *exps,
                                                 gtox[n*ngrids+i] = exps_2a[k*BLKSIZE+i] * gridx[i] * tmp;
                                                 gtoy[n*ngrids+i] = exps_2a[k*BLKSIZE+i] * gridy[i] * tmp;
                                                 gtoz[n*ngrids+i] = exps_2a[k*BLKSIZE+i] * gridz[i] * tmp;
-                                                gtox[n*ngrids+i] += exps[k*BLKSIZE+i] * lx * xpows[lx-1] * ypows[ly] * zpows[lz];
-                                                gtoy[n*ngrids+i] += exps[k*BLKSIZE+i] * ly * xpows[lx] * ypows[ly-1] * zpows[lz];
-                                                gtoz[n*ngrids+i] += exps[k*BLKSIZE+i] * lz * xpows[lx] * ypows[ly] * zpows[lz-1];
+                                                gtox[n*ngrids+i] += exps[k*BLKSIZE+i] * lx * xpows_1less_in_power[lx] * ypows[ly] * zpows[lz];
+                                                gtoy[n*ngrids+i] += exps[k*BLKSIZE+i] * ly * xpows[lx] * ypows_1less_in_power[ly] * zpows[lz];
+                                                gtoz[n*ngrids+i] += exps[k*BLKSIZE+i] * lz * xpows[lx] * ypows[ly] * zpows_1less_in_power[lz];
                                         } }
                                 } else {
                                         for (n = 0; n < degen; n++) {

commit bb2b70717137e5e91b80311bcc05315884e57cad

Fix bug in eval_gto caused by optimization in commit b6db4d03d85e3a6e5a035ca7e967c6566028b114

pyscf/lib/gto/deriv2.c
@@ -1005,7 +1005,7 @@ void GTOshell_eval_grid_cart_deriv1(double *gto, double *ri, double *exps,
                                         xpows[0] = 1;
                                         ypows[0] = 1;
                                         zpows[0] = 1;
-                                        for (lx = 1; lx <= l; lx++) {
+                                        for (lx = 0; lx < l; lx++) {
                                                 xpows[lx+1] = xpows[lx] *gridx[i];
                                                 ypows[lx+1] = ypows[lx] *gridy[i];
                                                 zpows[lx+1] = zpows[lx] *gridz[i];

pyscf/gto/test/test_eval_gto.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright 2014-2018 The PySCF Developers. All Rights Reserved.
+# Copyright 2014-2020 The PySCF Developers. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -27,47 +27,115 @@ H 8. 0. 0.
 numpy.random.seed(1)
 r = numpy.random.random((100,3)) * 2
 
+mol1 = gto.M(atom='''
+H 0. 0. 0.
+H 8. 0. 0.
+''', basis=[[0, (1, 1)],
+            [1, (.9, 1)],
+            [2, (.8, 1)],
+            [3, (.6, 1)],
+            [4, (.5, 1)],
+            [5, (.4, 1)]])
+r1 = r[:70]
+
 def tearDownModule():
-    global mol, r
-    del mol, r
+    global mol, mol1, r, r1
+    del mol, mol1, r, r1
 
 class KnownValues(unittest.TestCase):
     def test_cart(self):
         val = mol.eval_gto('GTOval', r)
         self.assertEqual(val.shape, (100,60))
-        self.assertAlmostEqual(lib.finger(val), -3.0283379087553808, 9)
+        self.assertAlmostEqual(lib.fp(val), -3.0283379087553808, 9)
 
         val = mol.eval_gto('GTOval_spinor', r)
         self.assertEqual(val.shape, (2,100,120))
         self.assertTrue(val.dtype == numpy.complex)
-        self.assertAlmostEqual(lib.finger(val), -2.029604328041736+0.80571570114202995j, 9)
+        self.assertAlmostEqual(lib.fp(val), -2.029604328041736+0.80571570114202995j, 9)
 
         val = mol.eval_gto('GTOval_ip', r)
         self.assertEqual(val.shape, (3,100,60))
-        self.assertAlmostEqual(lib.finger(val), -14.526634330008513, 9)
+        self.assertAlmostEqual(lib.fp(val), -14.526634330008513, 9)
 
         val = mol.eval_gto('GTOval_ig', r)
         self.assertEqual(val.shape, (3,100,60))
-        self.assertAlmostEqual(lib.finger(val), -9.8708097471236808e-08, 9)
+        self.assertAlmostEqual(lib.fp(val), -9.8708097471236808e-08, 9)
 
         val = mol.eval_gto('GTOval_sph_deriv3', r)
         self.assertEqual(val.shape, (20,100,60))
-        self.assertAlmostEqual(lib.finger(val), 207.32371064250577, 9)
+        self.assertAlmostEqual(lib.fp(val), 207.32371064250577, 9)
 
         val = mol.eval_gto('GTOval_cart_deriv3', r)
         self.assertEqual(val.shape, (20,100,70))
-        self.assertAlmostEqual(lib.finger(val), -93.310347533061787, 9)
+        self.assertAlmostEqual(lib.fp(val), -93.310347533061787, 9)
 
         val = mol.eval_gto('GTOval_spinor_deriv3', r)
         self.assertEqual(val.shape, (2,20,100,120))
         self.assertTrue(val.dtype == numpy.complex)
-        self.assertAlmostEqual(lib.finger(val), -2470.5090409822333+64.324145522087917j, 8)
+        self.assertAlmostEqual(lib.fp(val), -2470.5090409822333+64.324145522087917j, 8)
 
         mol1 = mol.copy()
         mol1.cart = True
         val = mol1.eval_gto('GTOval_ip', r)
         self.assertEqual(val.shape, (3,100,70))
-        self.assertAlmostEqual(lib.finger(val), -15.411590075004403, 9)
+        self.assertAlmostEqual(lib.fp(val), -15.411590075004403, 9)
+
+    def test_deriv(self):
+        val = mol1.eval_gto('GTOval_sph_deriv4', r1)
+        self.assertEqual(val.shape, (35,70,72))
+        self.assertAlmostEqual(lib.fp(val), 9.0043160890191309, 9)
+
+        ref = val
+        val = mol1.eval_gto('GTOval_sph_deriv3', r1)
+        self.assertEqual(val.shape, (20,70,72))
+        self.assertAlmostEqual(abs(val - ref[:20]).max(), 0, 12)
+
+        val = mol1.eval_gto('GTOval_sph_deriv2', r1)
+        self.assertEqual(val.shape, (10,70,72))
+        self.assertAlmostEqual(abs(val - ref[:10]).max(), 0, 12)
+
+        val = mol1.eval_gto('GTOval_sph_deriv1', r1)
+        self.assertEqual(val.shape, (4,70,72))
+        self.assertAlmostEqual(abs(val - ref[:4]).max(), 0, 12)
+
+        val = mol1.eval_gto('GTOval_ip_sph', r1)
+        self.assertAlmostEqual(abs(val - ref[1:4]).max(), 0, 12)
+
+        val = mol1.eval_gto('GTOval_cart_deriv4', r1)
+        self.assertEqual(val.shape, (35,70,112))
+        self.assertAlmostEqual(lib.fp(val), -210.58019667772351, 9)
+
+        ref = val
+        val = mol1.eval_gto('GTOval_cart_deriv3', r1)
+        self.assertAlmostEqual(abs(val - ref[:20]).max(), 0, 12)
+
+        val = mol1.eval_gto('GTOval_cart_deriv2', r1)
+        self.assertAlmostEqual(abs(val - ref[:10]).max(), 0, 12)
+
+        val = mol1.eval_gto('GTOval_cart_deriv1', r1)
+        self.assertAlmostEqual(abs(val - ref[:4]).max(), 0, 12)
+
+        val = mol1.eval_gto('GTOval_ip_cart', r1)
+        self.assertAlmostEqual(abs(val - ref[1:4]).max(), 0, 12)
+
+        val = mol1.eval_gto('GTOval_spinor_deriv4', r1)
+        self.assertEqual(val.shape, (2,35,70,144))
+        self.assertTrue(val.dtype == numpy.complex)
+        self.assertAlmostEqual(lib.fp(val), 91.706259060820059+147.16582428195531j, 9)
+
+        ref = val
+        val = mol1.eval_gto('GTOval_spinor_deriv3', r1)
+        self.assertAlmostEqual(abs(val - ref[:,:20]).max(), 0, 12)
+
+        val = mol1.eval_gto('GTOval_spinor_deriv2', r1)
+        self.assertAlmostEqual(abs(val - ref[:,:10]).max(), 0, 12)
+
+        val = mol1.eval_gto('GTOval_spinor_deriv1', r1)
+        self.assertAlmostEqual(abs(val - ref[:,:4]).max(), 0, 12)
+
+        val = mol1.eval_gto('GTOval_ip_spinor', r1)
+        self.assertAlmostEqual(abs(val - ref[:,1:4]).max(), 0, 12)
+
 
 if __name__ == '__main__':
     print("Full Tests for eval_gto")