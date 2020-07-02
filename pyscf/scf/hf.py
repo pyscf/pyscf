f80f819b89f1d5f102dd74e640af09b2dab8ac92
JezsMartinez <jezsmartinez@gmail.com>
2020-07-02 14:42:48 -0400

    Vcstatic

--- pyscf/scf/hf.py
+++ pyscf/scf/hf.py
@@ -547,7 +547,7 @@ def static_dft(mf, s, f, mo_energy, mo_occ, option=2):
         if option == 1:
             dEdp = beta * numpy.where((p > 0.0) & (p < 0.5), -1.0 / (1.0 - p), numpy.where((p > 0.5) & (p < 1.0), 1.0 / p, 0))
         else:
-            dEdp = beta * numpy.where((p > 0.0) & (p < 1.0), (numpy.log(p) - numpy.log(1-p)))
+            dEdp = beta * numpy.where((p > 0.0) & (p < 1.0), numpy.log(p) - numpy.log(1-p), 0)
 
         v_c_static = dEdp * dpde * mo_energy
 
