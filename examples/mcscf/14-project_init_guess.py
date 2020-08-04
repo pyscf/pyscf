1ae7addec5859aa15d3e4886da5fc537c8a8702f
Matthew R Hermes <herme068@herme068.umn.edu>
2020-08-04 14:23:06 -0500

    Additional mcscf project_init_guess example

--- examples/mcscf/14-project_init_guess.py
+++ examples/mcscf/14-project_init_guess.py
@@ -14,10 +14,10 @@ you get the orbital coeffcients.  It can even be the orbitals of different
 molecule.
 '''
 
-mol1 = gto.M(
+mol0 = gto.M(
     atom = 'C 0 0 0; C 0 0 1.2',
     basis = '6-31g')
-mf = scf.RHF(mol1)
+mf = scf.RHF(mol0)
 mf.kernel()
 
 mo_init_guess = mf.mo_coeff
@@ -31,16 +31,16 @@ mo_init_guess = mf.mo_coeff
 # 1. If only the geometry is different, we only need to give
 #    the function the guess orbitals
 
-mol2 = gto.M(
+mol1 = gto.M(
 atom = 'C 0 0 0; C 0 0 1.3',
 basis = '6-31g',
 verbose = 4)
-mf = scf.RHF(mol2)
+mf = scf.RHF(mol1)
 mf.kernel()
-mc = mcscf.CASSCF(mf, 4, 4)
-mo = mcscf.project_init_guess(mc, mo_init_guess)
-mc.kernel(mo)
-print('E(CAS) = %.12f, ref = -75.465214455907' % mc.e_tot)
+mc1 = mcscf.CASSCF(mf, 4, 4)
+mo = mcscf.project_init_guess(mc1, mo_init_guess)
+mc1.kernel(mo)
+print('E(CAS) = %.12f, ref = -75.465214455907' % mc1.e_tot)
 
 # 2. When we change the basis set, we also need to give it
 #    the original molecule corresponding to our guess
@@ -51,9 +51,29 @@ mol2 = gto.M(
     verbose = 4)
 mf = scf.RHF(mol2)
 mf.kernel()
-mc = mcscf.CASSCF(mf, 4, 4)
-mo = mcscf.project_init_guess(mc, mo_init_guess, prev_mol=mol1)
-mc.kernel(mo)
-print('E(CAS) = %.12f, ref = -75.482856483217' % mc.e_tot)
+mc2 = mcscf.CASSCF(mf, 4, 4)
+mo = mcscf.project_init_guess (mc2, mo_init_guess,
+    prev_mol=mol0)
+mc2.kernel(mo)
+print('E(CAS) = %.12f, ref = -75.482856483217' % mc2.e_tot)
+
+# 3. Changing the basis set and geometry at the same time is
+#    not supported. However, you can always just call the
+#    function multiple times with different arguments. 
+mol3 = gto.M(
+atom = 'C 0 0 0; C 0 0 1.3',
+basis = 'cc-pvdz',
+verbose = 4)
+mf = scf.RHF(mol3)
+mf.kernel()
+mc3 = mcscf.CASSCF(mf, 4, 4)
+mo = mcscf.project_init_guess (mc2, mo_init_guess,
+    prev_mol=mol0)
+mo = mcscf.project_init_guess (mc3, mo)
+e3a = mc3.kernel(mo)[0] # basis, then geom
+mo = mcscf.project_init_guess (mc1, mo_init_guess) 
+mo = mcscf.project_init_guess (mc3, mo, prev_mol=mol1)
+e3b = mc3.kernel(mo)[0] # geom, then basis
+print ('E(CAS) = %.12f, %.12f, ref = -75.503011798165' % (e3a, e3b))
 
 
