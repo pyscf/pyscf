554905caa5b27be51319de3b549d5e8432ec8774
Qiming Sun <osirpt.sun@gmail.com>
2020-08-08 17:10:54 -0700

    Add warning msg for the misleading mcscf attribute natorb. (#issue 581)

--- pyscf/mcscf/mc1step.py
+++ pyscf/mcscf/mc1step.py
@@ -466,6 +466,14 @@ def kernel(casscf, mo_coeff, tol=1e-7, conv_tol_grad=None,
             occ, ucas = casscf._eig(-casdm1, ncore, nocc)
             casdm1 = numpy.diag(-occ)
     else:
+        if casscf.natorb:
+            # FIXME (pyscf-2.0): Whether to transform natural orbitals in
+            # active space when this flag is enabled?
+            log.warn('The attribute natorb of mcscf object affects only the '
+                     'orbital canonicalization.\n'
+                     'If you would like to get natural orbitals in active space '
+                     'without touching core and external orbitals, an explicit '
+                     'call to mc.cas_natorb_() is required')
         mo_energy = None
 
     if dump_chk:
