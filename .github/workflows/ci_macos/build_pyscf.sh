0bd495d03aac69b14c87a564a48522e3bb3b3989
Xing Zhang <fishjojo@users.noreply.github.com>
2020-11-09 12:44:47 -0800

    Update actions workflow (#756)
    * clean up CI
    * add CI for macOS

---
+++ .github/workflows/ci_macos/build_pyscf.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#XXX default clang compiler does not support openmp, shall we use gcc?
+cd ./pyscf/lib
+wget https://github.com/fishjojo/pyscf-deps/raw/master/pyscf-1.7.5-deps-macos-10.14.tar.gz
+tar xzf pyscf-1.7.5-deps-macos-10.14.tar.gz
+mkdir build; cd build
+cmake -DBUILD_LIBXC=OFF -DBUILD_XCFUN=OFF ..
+make -j4
+cd ../../..
