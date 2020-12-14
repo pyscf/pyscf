b40cd5e957ea72d40891a3ab4d37a69343b38497
Xing Zhang <zhangxing.nju@gmail.com>
2020-12-13 21:11:31 -0800

    test

--- .github/workflows/ci_macos/test_libxc.sh
+++ .github/workflows/ci_macos/test_libxc.sh
@@ -3,4 +3,8 @@ export OMP_NUM_THREADS=1
 export PYTHONPATH=$(pwd):$PYTHONPATH 
 
 echo $PYTHONPATH
+lldb ./pyscf/lib/libxc_itrf.dylib
+ls ./pyscf/lib/deps/lib
+lldb ./pyscf/lib/deps/lib/libxc.5.dylib
+python -c "from pyscf import lib; lib.load_library('libxc_itrf')"
 python -c "from pyscf import dft; print(dft.libxc)"
