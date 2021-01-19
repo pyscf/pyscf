ca075e28008759f0f78bb276fd92947b9e1acdac
Mark Williamson <mjw@mjw.name>
2021-01-19 12:18:31 +0000

    Build lower level libs first

--- conda/build.sh
+++ conda/build.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env bash
-python -m pip install --upgrade pip
-pip install "numpy!=1.16,!=1.17" "scipy<1.5" h5py nose nose-exclude nose-timer nose-cov codecov
-pip install pyberny geometric
+# You can run build.sh with bash -x -e. The -x makes it echo each command that is run
+# and the -e makes it exit whenever a command in the script returns nonzero exit status.
+set -x -e
 
-#cppe
-version=$(python -c 'import sys; version=sys.version_info[:2]; print("{0}.{1}".format(*version))')
-if [ $version != '2.7' ] && [ $version != '3.5' ]; then
-    pip install git+https://github.com/maxscheurer/cppe.git
-fi
+cd ../pyscf/lib
+mkdir build
+cd build
+cmake ..
+make
+cd ../../conda
 
 $PYTHON setup.py install --single-version-externally-managed --record record.txt
