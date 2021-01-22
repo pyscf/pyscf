c07127b28446284c04bc4306c78309f19153db52
Mark Williamson <mjw@mjw.name>
2021-01-22 15:54:07 +0000

    Clean up after build

--- .github/workflows/ci_linux/build_pyscf.sh
+++ .github/workflows/ci_linux/build_pyscf.sh
@@ -4,4 +4,7 @@ curl http://www.sunqm.net/pyscf/files/bin/pyscf-1.7.5-deps.tar.gz | tar xzf -
 mkdir build; cd build
 cmake -DBUILD_LIBXC=OFF -DBUILD_XCFUN=OFF ..
 make -j4
-cd ../../..
+cd ..
+rm -Rf build
+rm -f pyscf-1.7.5-deps.tar.gz
+cd ../..
