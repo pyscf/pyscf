b8914bda9cb14d34b71743169a0958ad32f695b2
Samragni Banerjee <banerjee.156@buckeyemail.osu.edu>
2021-01-29 14:53:22 -0500

    Changes in variable names in dfadc

--- pyscf/adc/dfadc.py
+++ pyscf/adc/dfadc.py
@@ -40,22 +40,22 @@ def  get_ovvv_spin_df(myadc, Lov, Lvv, p, chnk_size):
 
     ''' Returns approximate ovvv integrals (different spin cases) used in unrestricted implementation '''
 
-    norb_1 = Lov.shape[1]
-    norb_2 = Lov.shape[2]
-    norb_3 = Lvv.shape[1]
+    nocc = Lov.shape[1]
+    nvir_1 = Lov.shape[2]
+    nvir_2 = Lvv.shape[1]
     naux = myadc.with_df.get_naoaux()
 
-    Lvv = Lvv.reshape(naux,norb_3*norb_3)
-    Lov = Lov.reshape(naux,norb_1,norb_2)
+    Lvv = Lvv.reshape(naux,nvir_2*nvir_2)
+    Lov = Lov.reshape(naux,nocc,nvir_1)
 
-    if chnk_size < norb_1:
+    if chnk_size < nocc:
         #Lov_temp = np.ascontiguousarray(Lov.T[:,p:p+chnk_size,:].reshape(-1,naux))
         Lov_temp = np.ascontiguousarray(Lov.transpose(1,2,0)[p:p+chnk_size].reshape(-1,naux))
     else :
         Lov_temp = np.ascontiguousarray(Lov.transpose(1,2,0).reshape(-1,naux))
 
     ovvv = np.dot(Lov_temp, Lvv)
-    ovvv = ovvv.reshape(-1, norb_2, norb_3, norb_3)
+    ovvv = ovvv.reshape(-1, nvir_1, nvir_2, nvir_2)
     #vvvv = np.ascontiguousarray(vvvv.transpose(0,2,1,3)).reshape(-1, nvir, nvir * nvir)
     return ovvv    
 
