3f4a95591c4839745fbb364bb6a4f49b194eb447
Qiming Sun <osirpt.sun@gmail.com>
2021-03-28 15:43:52 -0700

    A few updates for libxc tests

--- .github/workflows/ci_linux/build_pyscf.sh
+++ .github/workflows/ci_linux/build_pyscf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd ./pyscf/lib
-curl http://www.sunqm.net/pyscf/files/bin/pyscf-2.0-deps.tar.gz | tar xzf -
+curl http://www.sunqm.net/pyscf/files/bin/pyscf-2.0a-deps.tar.gz | tar xzf -
 mkdir build; cd build
 cmake -DBUILD_LIBXC=OFF -DBUILD_XCFUN=OFF -DBUILD_LIBCINT=OFF ..
 make -j4
