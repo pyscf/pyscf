7d98190e8b57792198f4eee9b6ac1d2948aefaa7
Peter Pinski <peter.pinski@quantumsimulations.de>
2021-04-02 01:13:40 +0200

    Add option to use cholesky orbitals as the initial guess for iterative orbital localization.

--- pyscf/lo/__init__.py
+++ pyscf/lo/__init__.py
@@ -15,6 +15,7 @@
 from pyscf.lo import nao
 from pyscf.lo import orth
 from pyscf.lo.orth import lowdin, schmidt, vec_lowdin, vec_schmidt, orth_ao
+from pyscf.lo.cholesky import cholesky_mos
 from pyscf.lo import iao
 from pyscf.lo import ibo
 from pyscf.lo import vvo
@@ -22,4 +23,3 @@ from pyscf.lo.nao import set_atom_conf
 from pyscf.lo.boys import Boys, BF
 from pyscf.lo.edmiston import EdmistonRuedenberg, ER
 from pyscf.lo.pipek import PipekMezey, PM
-from pyscf.lo.cholesky import cholesky_mos
