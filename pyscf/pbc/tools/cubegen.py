ffb12ccf2e9d757dc4076667c281c33b0adb4f7e
Max Nusspickel <max.nusspickel@kcl.ac.uk>
2021-06-06 15:27:04 +0100

    Changes to pbc/cubegen.py

--- pyscf/pbc/tools/cubegen.py
+++ pyscf/pbc/tools/cubegen.py
@@ -8,8 +8,8 @@ from pyscf.lib import logger
 from pyscf import gto
 from pyscf.dft import numint
 
-DEFAULT_NPOINTS = getattr(__config__, "cubegen_npoints", None)
-DEFAULT_RESOLUTION = getattr(__config__, "cubegen_resolution", 15)
+DEFAULT_NPOINTS = getattr(__config__, 'cubegen_npoints', None)
+DEFAULT_RESOLUTION = getattr(__config__, 'cubegen_resolution', 15)
 
 
 class CubeFile:
@@ -26,14 +26,14 @@ class CubeFile:
         >>>     f.add_orbital(hf.mo_coeff[:,5:10])
         >>>     f.add_density(hf.make_rdm1())
 
-        Arguments
-        ---------
+        Parameters
+        ----------
             cell : pbc.gto.Cell object
             filename : str
-                Filename for cube file. Should include ".cube" extension
+                Filename for cube file. Should include ".cube" extension.
             nx, ny, nz : int, optional
-                Number of grid points in X, Y, and Z direction. If specified,
-                they take precedence over `resolution`. Default: None.
+                Number of grid points in x, y, and z direction. If specified,
+                they take precedence over `resolution` parameter. Default: None.
             resolution : float, optional
                 Resolution in units of 1/Bohr for automatic choice of `nx`,`ny`, and `nz`. Default: 15.0.
             title : str, optional
@@ -41,10 +41,10 @@ class CubeFile:
             comment : str, optional
                 Second comment line in cube-file.
             origin : array(3), optional
-                Origin in X, Y, Z coordinates
+                Origin in x, y, z coordinates.
             fmt : str, optional
-                Float formatter for voxel data. According to the cube-file standard, this is required to
-                be "%13.5E", but some applications may work with a different format. Default: "%13.5E"
+                Float formatter for voxel data. According to the cube-file standard this is required to
+                be "%13.5E", but some applications may support a different format. Default: "%13.5E"
             crop : dict, optional
                 By default, the coordinate grid will span the entire unit cell. `crop` can be set
                 to crop the unit cell. `crop` should be a dictionary with possible keys
@@ -57,6 +57,7 @@ class CubeFile:
 
         self.cell = cell
         self.filename = filename
+        # Make user aware of different behavior of resolution, compared to pyscf.tools.cubegen
         if resolution < 1:
             logger.warn(cell, "Warning: resolution is below 1/Bohr. Recommended values are 5/Bohr or higher.")
         self.a = self.cell.lattice_vectors().copy()
@@ -64,12 +65,12 @@ class CubeFile:
         if crop is not None:
             a = self.a.copy()
             norm = np.linalg.norm(self.a, axis=1)
-            a[0] -= (crop.get("a0", 0) + crop.get("a1", 0)) * self.a[0]/norm[0]
-            a[1] -= (crop.get("b0", 0) + crop.get("b1", 0)) * self.a[1]/norm[1]
-            a[2] -= (crop.get("c0", 0) + crop.get("c1", 0)) * self.a[2]/norm[2]
-            self.origin += crop.get("a0", 0)*self.a[0]/norm[0]
-            self.origin += crop.get("b0", 0)*self.a[1]/norm[1]
-            self.origin += crop.get("c0", 0)*self.a[2]/norm[2]
+            a[0] -= (crop.get('a0', 0) + crop.get('a1', 0)) * self.a[0]/norm[0]
+            a[1] -= (crop.get('b0', 0) + crop.get('b1', 0)) * self.a[1]/norm[1]
+            a[2] -= (crop.get('c0', 0) + crop.get('c1', 0)) * self.a[2]/norm[2]
+            self.origin += crop.get('a0', 0)*self.a[0]/norm[0]
+            self.origin += crop.get('b0', 0)*self.a[1]/norm[1]
+            self.origin += crop.get('c0', 0)*self.a[2]/norm[2]
             self.a = a
 
         # Use nx, ny, or nz if provided, else determine based on resolution, but not more than 128
@@ -89,25 +90,27 @@ class CubeFile:
         zs = np.arange(self.nz) / (self.nz-1)
         coords = lib.cartesian_prod([xs, ys, zs])
         coords = np.dot(coords, self.a)
-        coords = np.asarray(coords, order="C") + self.origin
+        coords = np.asarray(coords, order='C') + self.origin
         return coords
 
     @property
     def ncoords(self):
+        """Number of grod points."""
         return self.nx*self.ny*self.nz
 
     @property
     def nfields(self):
+        """Number of datasets (orbitals + density matrices)."""
         return len(self.fields)
 
     def save_state(self, filename):
         cell, self.cell = self.cell, None       # Do not pickle cell
-        pickle.dump(self, open(filename, "wb"))
+        pickle.dump(self, open(filename, 'wb'))
         self.cell = cell                        # Restore self.cell
 
     @classmethod
     def load_state(cls, filename, cell=None):
-        self = pickle.load(open(filename, "rb"))
+        self = pickle.load(open(filename, 'rb'))
         self.cell = cell
         return self
 
@@ -119,29 +122,49 @@ class CubeFile:
         self.write()
 
     def add_orbital(self, coeff, dset_idx=None):
-        """Add one or more orbitals to the cube files data set.
+        """Add one or more orbitals to the cube file.
 
         Arguments
         ---------
-        coeff : (N,...) array
-            AO coefficients of orbitals.
+        coeff : (N) or (N,M) array
+            AO coefficients of orbitals. Supports adding a single orbitals,
+            where `coeff` is a one-dimensional array, or multiple orbitals,
+            in which case the second dimension of `coeff` labels the orbitals.
+        dset_idx : int, optional
+            Dataset index of orbital(s). In the application, the orbitals will
+            be labelled as 'Orbital <dset_idx>' or similar. If set to `None`,
+            the smallest unused, positive integer will be used. Default: None.
         """
         coeff = np.asarray(coeff)
         if coeff.ndim == 1: coeff = coeff[:,np.newaxis]
-        assert coeff.ndim == 2
+        assert (coeff.ndim == 2)
         for i, c in enumerate(coeff.T):
             idx = dset_idx+i if dset_idx is not None else None
-            self.fields.append((c, "orbital", idx))
+            self.fields.append((c, 'orbital', idx))
 
     def add_density(self, dm, dset_idx=None):
+        """Add one or more densities to the cube file.
+
+        Arguments
+        ---------
+        dm : (N,N) or (M,N,N) array
+            Density-matrix in AO-representation. Supports adding a single density-matrix,
+            where `dm` is a two-dimensional array, or multiple density-matrices,
+            in which case the first dimension of `dm` labels the matrices.
+        dset_idx : int, optional
+            Dataset index of density-matrix. In the application, the density-matrix will
+            be labelled as 'Orbital <dset_idx>' or similar. If set to `None`,
+            the smallest unused, positive integer will be used. Default: None.
+        """
         dm = np.asarray(dm)
         if dm.ndim == 2: dm = dm[np.newaxis]
-        assert dm.ndim == 3
+        assert (dm.ndim == 3)
         for i, d in enumerate(dm):
             idx = dset_idx+i if dset_idx is not None else None
-            self.fields.append((d, "density", idx))
+            self.fields.append((d, 'density', idx))
 
     def add_mep(self, dm, dset_idx=None):
+        # TODO
         raise NotImplementedError()
 
     def write(self, filename=None):
@@ -160,54 +183,57 @@ class CubeFile:
         self.write_fields(filename)
 
     def write_header(self, filename, dset_ids=None):
+        """Write header of cube-file."""
         if self.nfields > 1 and dset_ids is None:
             dset_ids = range(1, self.nfields+1)
-        with open(filename, "w") as f:
-            f.write("%s\n" % self.title)
-            f.write("%s\n" % self.comment)
+        with open(filename, 'w') as f:
+            f.write('%s\n' % self.title)
+            f.write('%s\n' % self.comment)
             if self.nfields > 1:
-                f.write("%5d" % -self.cell.natm)
+                f.write('%5d' % -self.cell.natm)
             else:
-                f.write("%5d" % self.cell.natm)
-            f.write("%12.6f%12.6f%12.6f" % tuple(self.origin))
+                f.write('%5d' % self.cell.natm)
+            f.write('%12.6f%12.6f%12.6f' % tuple(self.origin))
             if self.nfields > 1:
-                f.write("%5d" % self.nfields)
-            f.write("\n")
+                f.write('%5d' % self.nfields)
+            f.write('\n')
             # Lattice vectors
-            f.write("%5d%12.6f%12.6f%12.6f\n" % (self.nx, *(self.a[0]/(self.nx-1))))
-            f.write("%5d%12.6f%12.6f%12.6f\n" % (self.ny, *(self.a[1]/(self.ny-1))))
-            f.write("%5d%12.6f%12.6f%12.6f\n" % (self.nz, *(self.a[2]/(self.nz-1))))
+            f.write('%5d%12.6f%12.6f%12.6f\n' % (self.nx, *(self.a[0]/(self.nx-1))))
+            f.write('%5d%12.6f%12.6f%12.6f\n' % (self.ny, *(self.a[1]/(self.ny-1))))
+            f.write('%5d%12.6f%12.6f%12.6f\n' % (self.nz, *(self.a[2]/(self.nz-1))))
             # Atoms
             for atm in range(self.cell.natm):
                 sym = self.cell.atom_symbol(atm)
-                f.write("%5d%12.6f" % (gto.charge(sym), 0.0))
-                f.write("%12.6f%12.6f%12.6f\n" % tuple(self.cell.atom_coords()[atm]))
+                f.write('%5d%12.6f' % (gto.charge(sym), 0.0))
+                f.write('%12.6f%12.6f%12.6f\n' % tuple(self.cell.atom_coords()[atm]))
             # Data set indices
             if self.nfields > 1:
-                f.write("%5d" % self.nfields)
+                f.write('%5d' % self.nfields)
                 for i in range(self.nfields):
-                    f.write("%5d" % dset_ids[i])
-                f.write("\n")
+                    f.write('%5d' % dset_ids[i])
+                f.write('\n')
 
     def write_fields(self, filename):
-        # Loop over coordinates first, then fields!
+        """Write voxel data of registered fields in `self.fields` to cube-file."""
         blksize = min(self.ncoords, 8000)
-        with open(filename, "a") as f:
+        with open(filename, 'a') as f:
+            # Loop over x,y,z coordinates first, then fields!
             for blk0, blk1 in lib.prange(0, self.ncoords, blksize):
-                blksize = blk1-blk0 # Actual blocksize
-                data = np.zeros((blksize, self.nfields))
+                data = np.zeros((blk1-blk0, self.nfields))
                 blk = np.s_[blk0:blk1]
-                ao = self.cell.eval_gto("PBCGTOval", self.coords[blk])
+                ao = self.cell.eval_gto('PBCGTOval', self.coords[blk])
                 for i, (field, ftype, _) in enumerate(self.fields):
-                    if ftype == "orbital":
+                    if ftype == 'orbital':
                         data[:,i] = np.dot(ao, field)
-                    elif ftype == "density":
+                    elif ftype == 'density':
                         data[:,i] = numint.eval_rho(self.cell, ao, field)
+                    else:
+                        raise ValueError('Unknown field type: %s' % ftype)
                 data = data.flatten()
                 for d0, d1 in lib.prange(0, len(data), 6):
-                    f.write(((d1-d0)*self.fmt + "\n") % tuple(data[d0:d1]))
+                    f.write(((d1-d0)*self.fmt + '\n') % tuple(data[d0:d1]))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     def make_graphene(a, c, atoms=["C", "C"], supercell=None):
         amat = np.asarray([
@@ -232,7 +258,7 @@ if __name__ == "__main__":
                         atom.append((atoms[0]+str(nr), coords[0]+shift))
                         atom.append((atoms[1]+str(nr), coords[1]+shift))
                         nr += 1
-            amat = np.einsum("i,ij->ij", ncopy, amat)
+            amat = np.einsum('i,ij->ij', ncopy, amat)
         return amat, atom
 
 
