ac735aa1355f1ca5a65c191ccfc32de7574e90af
Kyle Bystrom <kylebystrom@gmail.com>
2021-07-27 04:59:41 -0700

    move pjs dfj check from pre_kernel to build. Settings should be corrected and warnings issued in the build step in case the object is used outside SCF

--- pyscf/sgx/sgx.py
+++ pyscf/sgx/sgx.py
@@ -115,16 +115,6 @@ def sgx_fit(mf, auxbasis=None, with_df=None, pjs=False):
                 self.with_df.build(level=self.with_df.grids_level_f)
             else:
                 self.with_df.build(level=self.with_df.grids_level_i)
-            return mf_class.build(self, mol, **kwargs)
-
-        def reset(self, mol=None):
-            self.with_df.reset(mol)
-            return mf_class.reset(self, mol)
-
-        def pre_kernel(self, envs):
-            self.direct_scf = False # should always be False
-            if self.with_df.grids_level_i != self.with_df.grids_level_f:
-                self._in_scf = True
             if self.with_df.pjs:
                 if not self.with_df.dfj:
                     import warnings
@@ -135,6 +125,16 @@ def sgx_fit(mf, auxbasis=None, with_df=None, pjs=False):
                     '''
                     warnings.warn(msg)
                 self.with_df.dfj = True # no SGX-J allowed if P-junction screening on
+            return mf_class.build(self, mol, **kwargs)
+
+        def reset(self, mol=None):
+            self.with_df.reset(mol)
+            return mf_class.reset(self, mol)
+
+        def pre_kernel(self, envs):
+            self.direct_scf = False # should always be False
+            if self.with_df.grids_level_i != self.with_df.grids_level_f:
+                self._in_scf = True
 
         def get_jk(self, mol=None, dm=None, hermi=1, with_j=True, with_k=True,
                    omega=None):
