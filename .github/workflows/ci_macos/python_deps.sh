03a8499844549aca3a499dea93e19b439f3b3422
Xing Zhang <zhangxing.nju@gmail.com>
2021-11-27 16:16:17 -0800

    test scipy version for ci workflow

--- .github/workflows/ci_macos/python_deps.sh
+++ .github/workflows/ci_macos/python_deps.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
 python -m pip install --upgrade pip
-pip install numpy scipy h5py nose nose-exclude nose-timer nose-cov codecov
+pip install wheel
+pip install nose nose-exclude nose-timer nose-cov codecov
+pip install numpy h5py
 pip install pyberny geometric
 
-#cppe
 version=$(python -c 'import sys; version=sys.version_info[:2]; print("{0}.{1}".format(*version))')
+
+#scipy
+if [ $version == '3.6' ]; then
+    pip install 'scipy<1.2'
+elif [ $version == '3.7' ]; then
+    pip install 'scipy<1.5'
+elif [ $version == '3.8' ]; then
+    pip install 'scipy<1.6'
+else
+    pip install scipy
+fi
+
+#cppe
 if [ $version != '2.7' ] && [ $version != '3.5' ]; then
     pip install cppe
 fi
