8d262452c667e47271aa0546c0250af1e63f0e4f
Max Nusspickel <max.nusspickel@googlemail.com>
2022-01-11 18:39:03 +0000

    Fixes uccsd.make_rdm2 without frozen + typos

--- pyscf/lib/linalg_helper.py
+++ pyscf/lib/linalg_helper.py
@@ -104,8 +104,7 @@ def eigh_by_blocks(h, s=None, labels=None):
         labels : list
 
     Returns:
-        w, v.  w is the eigenvalue vector; v is the eigenfunction array;
-        seig is the eigenvalue vector of the metric s.
+        w, v.  w is the eigenvalue vector; v is the eigenfunction array.
 
     Examples:
 
@@ -1471,7 +1470,7 @@ def dsolve(aop, b, precond, tol=1e-12, max_cycle=30, dot=numpy.dot,
 
 
 def cho_solve(a, b, strict_sym_pos=True):
-    '''Solve ax = b, where a is a postive definite hermitian matrix
+    '''Solve ax = b, where a is a positive definite hermitian matrix
 
     Kwargs:
         strict_sym_pos (bool) : Whether to impose the strict positive definition
