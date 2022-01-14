47cae6e94508b93dcb1a2096f7ecf45c4af6dc3b
Susi Lehtola <susi.lehtola@gmail.com>
2022-01-14 13:18:42 -0500

    Include examples on how to print out version data and basis set references

--- pyscf/gto/basis/bse.py
+++ pyscf/gto/basis/bse.py
@@ -18,6 +18,9 @@ def _orbital_basis(basis):
     # Elements for which we have electron basis
     electron_elements = [k for k, v in basis['elements'].items() if 'electron_shells' in v]
 
+    # List of references in the used basis
+    reference_list = []
+
     # Electron Basis
     if electron_elements:
         for z in electron_elements:
@@ -43,7 +46,13 @@ def _orbital_basis(basis):
                 atom_shells.append(shell_data)
             r[sym] = atom_shells
 
-    return r
+            # Collect the literature references
+            for ref in data['references']:
+                for key in ref['reference_keys']:
+                    if key not in reference_list:
+                        reference_list.append(key)
+
+    return r, reference_list
 
 
 def _ecp_basis(basis):
@@ -90,17 +99,40 @@ def _ecp_basis(basis):
 
     return r
 
+def _print_basis_information(basis):
+    name = basis['name']
+    version = basis['version']
+    revision_description = basis['revision_description']
+    revision_date = basis['revision_date']
+    print('{} basis set, version {}'.format(name, version))
+    print('Last revised on {}'.format(revision_date))
+    print('Revision description: {}'.format(revision_description))
+
 if __name__ == '__main__':
-    from basis_set_exchange import api
+    from basis_set_exchange import api, references
+
+    # Get reference data
+    reference_data = api.get_reference_data()
+    #print(references)
+
     o631gbas = api.get_basis('6-31g', elements='O')
-    print('O 6-31G basis, BSE format\n{}'.format(o631gbas))
-    o631gorb = _orbital_basis(o631gbas)
+    #print('O 6-31G basis, BSE format\n{}'.format(o631gbas))
+    _print_basis_information(o631gbas)
+    o631gorb, o631gref = _orbital_basis(o631gbas)
     print('O 6-31G orbital basis, PySCF format\n{}'.format(o631gorb))
+    print('Literature references')
+    for ref in o631gref:
+        print(references.reference_text(ref, reference_data[ref]))
     print('')
 
     nalanl2dzbas = api.get_basis('lanl2dz', elements='Na')
-    print('Na LANL2DZ basis, BSE format\n{}'.format(nalanl2dzbas))
-    nalanl2dzorb = _orbital_basis(nalanl2dzbas)
+    #print('Na LANL2DZ basis, BSE format\n{}'.format(nalanl2dzbas))
+    _print_basis_information(nalanl2dzbas)
+    nalanl2dzorb, nalanl2dzref = _orbital_basis(nalanl2dzbas)
     print('Na LANL2DZ orbital basis, PySCF format\n{}'.format(nalanl2dzorb))
     nalanl2dzecp = _ecp_basis(nalanl2dzbas)
     print('Na LANL2DZ ECP basis, PySCF format\n{}'.format(nalanl2dzecp))
+    print('Literature references')
+    for ref in nalanl2dzref:
+        print(references.reference_text(ref, reference_data[ref]))
+    print('')
