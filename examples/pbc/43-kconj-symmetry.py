ed7ff6cc7017f8db88a78e826f3a6c3ed4fb7fde
Max Nusspickel <max.nusspickel@googlemail.com>
2022-02-21 14:31:31 +0000

    Adds file pbc/scf/kconjsym.py

--- examples/pbc/43-kconj-symmetry.py
+++ examples/pbc/43-kconj-symmetry.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+
+'''
+The addon kconj_symmetry_ can be used to perform k-point sampled SCF calculations
+which retain the phi(k) = phi(-k)* symmetry of the MOs phi.
+'''
+
 import numpy as np
 
 from pyscf.pbc import scf
@@ -5,28 +12,23 @@ from pyscf.pbc.gto import Cell
 from pyscf.pbc.lib import kpts_helper
 from pyscf.pbc.scf.addons import kconj_symmetry_
 
-def get_symmetry_error(mf):
-    dm1 = mf.make_rdm1()
-    conj_indices = kpts_helper.conj_mapping(mf.mol, mf.kpts)
-    ddm1 = (dm1 - dm1[...,conj_indices,:,:].conj())
-    err = np.linalg.norm(ddm1)
-    return err
+print("1D oxygen chain")
+print("---------------")
 
-# --- Example 1: 1D Oxygen chain with 4 k-points
-# In this case, enforcing the (k,-k)-symmetry helps the SCF to converge
-# to a lower energy solution
+# --- Example 1: 1D Oxygen chain with 3 k-points
+# The (k,-k)-symmetry is only broken slightly. With the kconj_symmetry_ decorator,
+# it can be restored without a significant change of the Hartree-Fock energy:
 
 cell = Cell()
-cell.atom = 'O 0 0 0 ; O 2 0 0'
+cell.atom = 'O 0 0 0'
 cell.dimension = 1
 cell.basis = '6-31G'
-cell.a = np.eye(3)*[4,10,10]
+cell.a = np.eye(3)*[2,10,10]
 cell.unit = 'A'
 cell.output = '43-example-1.out'
 cell.verbose = 4
 cell.build()
-
-kpts = cell.make_kpts([4,1,1])
+kpts = cell.make_kpts([3,1,1])
 
 mf = scf.KRHF(cell, kpts).density_fit()
 mf.kernel()
@@ -36,16 +38,24 @@ mf_sym.with_df = mf.with_df
 mf_sym = kconj_symmetry_(mf_sym)
 mf_sym.kernel()
 
-print("1D oxygen chain, 4 k-points:")
-print("Regular RHF:           E= % 16.8f  symmetry error= %.1e" % (mf.e_tot, get_symmetry_error(mf)))
-print("(k,-k)-symmetric RHF:  E= % 16.8f  symmetry error= %.1e" % (mf_sym.e_tot, get_symmetry_error(mf_sym)))
+def get_symmetry_error(mf):
+    dm1 = mf.make_rdm1()
+    conj_indices = kpts_helper.conj_mapping(mf.mol, mf.kpts)
+    ddm1 = (dm1 - dm1[...,conj_indices,:,:].conj())
+    err = np.linalg.norm(ddm1)
+    return err
+
+print("Example 1: 1 atom, 3 k-points:")
+print("  Regular RHF:           E= % 14.8f  converged= %5r  symmetry error= %.1e" % (
+    mf.e_tot, mf.converged, get_symmetry_error(mf)))
+print("  (k,-k)-symmetric RHF:  E= % 14.8f  converged= %5r  symmetry error= %.1e" % (
+    mf_sym.e_tot, mf_sym.converged, get_symmetry_error(mf_sym)))
 
 del cell, kpts, mf, mf_sym
 
-# --- Example 2: 1D Oxygen chain with 3 k-points
-# In this case, no (k,-k)-symmetric solution can be converged, as the RHF solution has a
-# degeneracy at the Fermi level, which can only be lifted by breaking (k,-k)-symmetry.
-# Note that a warning is logged in this case and mf.converged equals False.
+# --- Example 2: 1D Oxygen chain with 2 atoms and 4 k-points
+# The (k,-k)-symmetry is significantly broken. With the kconj_symmetry_ decorator,
+# a lower energy solution can be converged:
 
 cell = Cell()
 cell.atom = 'O 0 0 0 ; O 2 0 0'
@@ -56,8 +66,7 @@ cell.unit = 'A'
 cell.output = '43-example-2.out'
 cell.verbose = 4
 cell.build()
-
-kpts = cell.make_kpts([3,1,1])
+kpts = cell.make_kpts([4,1,1])
 
 mf = scf.KRHF(cell, kpts).density_fit()
 mf.kernel()
@@ -67,67 +76,31 @@ mf_sym.with_df = mf.with_df
 mf_sym = kconj_symmetry_(mf_sym)
 mf_sym.kernel()
 
-print("1D oxygen chain, 3 k-points:")
-print("Regular RHF:           E= % 16.8f  symmetry error= %.1e" % (mf.e_tot, get_symmetry_error(mf)))
-print("(k,-k)-symmetric RHF:  E= % 16.8f  symmetry error= %.1e" % (mf_sym.e_tot, get_symmetry_error(mf_sym)))
+print("Example 2: 2 atoms, 4 k-points:")
+print("  Regular RHF:           E= % 14.8f  converged= %5r  symmetry error= %.1e" % (
+    mf.e_tot, mf.converged, get_symmetry_error(mf)))
+print("  (k,-k)-symmetric RHF:  E= % 14.8f  converged= %5r  symmetry error= %.1e" % (
+    mf_sym.e_tot, mf_sym.converged, get_symmetry_error(mf_sym)))
 
 del cell, kpts, mf, mf_sym
 
-# --- Example 3: UHF for spin=2 system
+# --- Example 3: 1D Oxygen chain with 4 k-points
+# In this case, no (k,-k)-symmetric solution can be converged, as the RHF solution has a
+# degeneracy at the Fermi level, which can only be lifted by breaking (k,-k)-symmetry.
+# Note that a warning is logged in this case and mf.converged equals False:
 
 cell = Cell()
-cell.atom = 'O 0 0 0 ; O 2 0 0'
+cell.atom = 'O 0 0 0'
 cell.dimension = 1
 cell.basis = '6-31G'
-cell.a = np.eye(3)*[4,10,10]
+cell.a = np.eye(3)*[2,10,10]
 cell.unit = 'A'
 cell.output = '43-example-3.out'
 cell.verbose = 4
-cell.spin = 2
 cell.build()
+kpts = cell.make_kpts([4,1,1])
 
-kpts = cell.make_kpts([3,1,1])
-
-mf = scf.KUHF(cell, kpts).density_fit()
-mf.conv_tol = 1e-12
-mf.conv_tol_grad = 1e-8
-mf.kernel()
-
-mf_sym = scf.KUHF(cell, kpts)
-mf_sym.conv_tol = 1e-12
-mf_sym.conv_tol_grad = 1e-8
-mf_sym.with_df = mf.with_df
-mf_sym = kconj_symmetry_(mf_sym)
-mf_sym.kernel()
-
-print("1D oxygen chain, 3 k-points, spin=2:")
-print("Regular UHF:           E= % 16.8f  symmetry error= %.1e" % (mf.e_tot, get_symmetry_error(mf)))
-print("(k,-k)-symmetric UHF:  E= % 16.8f  symmetry error= %.1e" % (mf_sym.e_tot, get_symmetry_error(mf_sym)))
-
-del cell, kpts, mf, mf_sym
-
-# --- Example 4: FeO in rocksalt structure with 3 x 3 x 3 k-points
-# In this case, the (k,-k)-symmetry can be restored without a change in the Hartree-Fock energy
-
-cell = Cell()
-cell.a = 3.6 * np.asarray([
-            [0.5, 0.5, 0.0],
-            [0.0, 0.5, 0.5],
-            [0.5, 0.0, 0.5]])
-internal = np.asarray([
-            [0.0, 0.0, 0.0],
-            [0.5, 0.5, 0.5]])
-external = np.dot(internal, cell.a)
-cell.atom = 'Fe %f %f %f ; O %f %f %f' % (*external[0], *external[1])
-cell.basis = 'def2-svp'
-cell.output = '43-example-4.out'
-cell.verbose = 4
-cell.exp_to_discard = 0.1
-cell.build()
-
-kpts = cell.make_kpts([3,3,3])
-
-mf = scf.KRHF(cell, kpts).rs_density_fit(auxbasis='def2-svp-ri')
+mf = scf.KRHF(cell, kpts).density_fit()
 mf.kernel()
 
 mf_sym = scf.KRHF(cell, kpts)
@@ -135,6 +108,56 @@ mf_sym.with_df = mf.with_df
 mf_sym = kconj_symmetry_(mf_sym)
 mf_sym.kernel()
 
-print("FeO, 3x3x3 k-points:")
-print("Regular RHF:           E= % 16.8f  symmetry error= %.1e" % (mf.e_tot, get_symmetry_error(mf)))
-print("(k,-k)-symmetric RHF:  E= % 16.8f  symmetry error= %.1e" % (mf_sym.e_tot, get_symmetry_error(mf_sym)))
+print("Example 3: 1 atom, 4 k-points:")
+print("  Regular RHF:           E= % 14.8f  converged= %5r  symmetry error= %.1e" % (
+    mf.e_tot, mf.converged, get_symmetry_error(mf)))
+print("  (k,-k)-symmetric RHF:  E= % 14.8f  converged= %5r  symmetry error= %.1e" % (
+    mf_sym.e_tot, mf_sym.converged, get_symmetry_error(mf_sym)))
+
+del cell, kpts, mf, mf_sym
+
+
+# --- Example 4: 1D Oxygen chain with 2 atoms and 4 k-points, antiferromagnetic order
+# Same as example 3, but using UHF the degeneracy can be lifted and a (k,-k)-symmetric
+# solution with antiferromagnetic order converged:
+
+cell = Cell()
+cell.atom = 'O 0 0 0 ; O 2 0 0'
+cell.dimension = 1
+cell.basis = '6-31G'
+cell.a = np.eye(3)*[4,10,10]
+cell.unit = 'A'
+cell.output = '43-example-4.out'
+cell.verbose = 4
+cell.build()
+
+kpts = cell.make_kpts([4,1,1])
+
+def break_spinsym(cell, dm1, delta=0.05):
+    """Break spin symmetry of density-matrix, to converge AFM order"""
+    start, stop = cell.aoslice_by_atom()[:,[2,3]]
+    atm1 = np.s_[start[0]:stop[0]]
+    atm2 = np.s_[start[1]:stop[1]]
+    dm1a, dm1b = dm1.copy()
+    # Atom 1: Majority spin = alpha
+    dm1a[:,atm1,atm1] += delta*dm1b[:,atm1,atm1]
+    dm1b[:,atm1,atm1] -= delta*dm1b[:,atm1,atm1]
+    # Atom 2: Majority spin = beta
+    dm1a[:,atm2,atm2] -= delta*dm1a[:,atm2,atm2]
+    dm1b[:,atm2,atm2] += delta*dm1a[:,atm2,atm2]
+    return np.stack((dm1a, dm1b), axis=0)
+
+mf = scf.KUHF(cell, kpts).density_fit()
+dm0 = break_spinsym(cell, mf.get_init_guess())
+mf.kernel(dm0=dm0)
+
+mf_sym = scf.KUHF(cell, kpts)
+mf_sym.with_df = mf.with_df
+mf_sym = kconj_symmetry_(mf_sym)
+mf_sym.kernel(dm0=dm0)
+
+print("Example 4: 2 atoms, 3 k-points, AFM order:")
+print("  Regular UHF:           E= % 14.8f  converged= %5r  symmetry error= %.1e" % (
+    mf.e_tot, mf.converged, get_symmetry_error(mf)))
+print("  (k,-k)-symmetric UHF:  E= % 14.8f  converged= %5r  symmetry error= %.1e" % (
+    mf_sym.e_tot, mf_sym.converged, get_symmetry_error(mf_sym)))
---
+++ pyscf/pbc/scf/kconjsym.py
@@ -0,0 +1,193 @@
+'''
+Modifications to k-point sampled SCF classes,
+to enforce \phi(k) = \phi(-k)* symmetry
+'''
+
+import numpy as np
+
+from pyscf.scf.uhf import UHF
+from pyscf.scf.ghf import GHF
+from pyscf.lib import logger
+from pyscf.pbc.lib import kpts_helper
+
+# --- Helper functions:
+
+def _get_kconj_indices(cell, kpts):
+    '''Get list of indices of unique k-point k, and indices of conjugated k-points -k.'''
+    conj_index = kpts_helper.conj_mapping(cell, kpts)
+    skip = np.zeros(len(kpts), dtype=bool)
+    unique = []
+    conjugated = []
+    for k in range(len(kpts)):
+        if skip[k]:
+            continue
+        kconj = conj_index[k]
+        skip[k] = skip[kconj] = True
+        unique.append(k)
+        conjugated.append(kconj)
+    assert set(unique + conjugated) == set(range(len(kpts)))
+    return unique, conjugated
+
+def _kconj_symmetrize_rdm1(cell, kpts, dm1):
+    '''Get (k,-k)-symmetric 1-DM, by arithmetric averaging.'''
+    conj_index = kpts_helper.conj_mapping(cell, kpts)
+    dm1 = (dm1 + dm1[...,conj_index,:,:].conj()) / 2
+    return dm1
+
+def _rdm1_kconj_symmetry_error(cell, kpts, dm1):
+    '''Determine (k,-k)-symmetry error from the 1-DM.'''
+    kuniq, kconj = _get_kconj_indices(cell, kpts)
+    err = abs(dm1[...,kuniq,:,:] - dm1[...,kconj,:,:].conj()).max()
+    return err
+
+
+def kscf_with_kconjsym(cls):
+    '''Create SCF class with k-point sampling and phi(k) = phi(-k)* symmetry.
+
+    Args:
+        cls: SCF class with k-point sampling.
+
+    Returns:
+        cls_sym: SCF class with k-point sampling and \phi(k) = \phi(-k)* symmetry.
+    '''
+
+    is_uhf = issubclass(cls, UHF)
+    is_ghf = issubclass(cls, GHF)
+    if is_ghf:
+        raise NotImplementedError("k-conjugation symmetry not implemented for generalized-spin SCF.")
+
+    # --- Replacement methods:
+
+    def get_jk(self, cell=None, dm_kpts=None, hermi=1, kpts=None, kpts_band=None,
+            with_j=True, with_k=True, omega=None, **kwargs):
+        # If kpts_band is provided, do not use (k,-k)-symmetry:
+        if kpts_band is not None:
+            return super(type(self), self).get_jk(cell=cell, dm_kpts=dm_kpts, hermi=hermi, kpts=kpts,
+                kpts_band=kpts_band, with_j=with_j, with_k=with_k, omega=omega, **kwargs)
+
+        if cell is None: cell = self.cell
+        if kpts is None: kpts = self.kpts
+        if dm_kpts is None: dm_kpts = self.make_rdm1()
+        nkpts = len(kpts)
+        nao = dm_kpts.shape[-1]
+        # Use kpts_band to evaluate J and K matrix at reduced set of unique k-points:
+        kuniq, kconj = _get_kconj_indices(cell, kpts)
+        kpts_red = kpts[kuniq]
+        vj_red, vk_red = super(type(self), self).get_jk(cell=cell, dm_kpts=dm_kpts, hermi=hermi, kpts=kpts,
+                kpts_band=kpts_red, with_j=with_j, with_k=with_k, omega=omega, **kwargs)
+        selfconj = np.equal(kuniq, kconj)
+        vj = vk = None
+        vshape = (2, nkpts, nao, nao) if is_uhf else (nkpts, nao, nao)
+        # Use vj_red and vk_red evaluated at the unique k-points k to fill the values
+        # for vj and vk at both k and -k:
+        if with_j:
+            vj_red[...,selfconj,:,:] = vj_red[...,selfconj,:,:].real
+            vj = np.zeros(vshape, dtype=complex)
+            vj[...,kconj,:,:] = vj_red.conj()
+            vj[...,kuniq,:,:] = vj_red
+        if with_k:
+            vk_red[...,selfconj,:,:] = vk_red[...,selfconj,:,:].real
+            vk = np.zeros(vshape, dtype=complex)
+            vk[...,kconj,:,:] = vk_red.conj()
+            vk[...,kuniq,:,:] = vk_red
+        return vj, vk
+
+    if is_uhf:
+        def eig(self, h_kpts, s_kpts):
+            nkpts = len(s_kpts)
+            kuniq, kconj = _get_kconj_indices(self.cell, self.kpts)
+            # Only diagonalize unique k-points:
+            h_red = h_kpts[:,kuniq]
+            s_red = s_kpts[kuniq]
+            # Self-conjugated k-points (e.g. Gamma) should have a real Fock matrix:
+            selfconj = np.equal(kuniq, kconj)
+            h_red[:,selfconj] = h_red[:,selfconj].real
+            # Diagonalize reduced set of k-points:
+            e_red, c_red = super(type(self), self).eig(h_red, s_red)
+            # Use lists and loops instead of NumPy arrays, since the number of MOs can be k-point dependent:
+            mo_energy_k = (nkpts*[None], nkpts*[None])
+            mo_coeff_k = (nkpts*[None], nkpts*[None])
+            for idx, (k, kc) in enumerate(zip(kuniq, kconj)):
+                mo_energy_k[0][k] = e_red[0][idx]
+                mo_energy_k[1][k] = e_red[1][idx]
+                mo_coeff_k[0][k] = c_red[0][idx]
+                mo_coeff_k[1][k] = c_red[1][idx]
+                if (kc == k):
+                    continue
+                # Shift conjugated energies slightly, to avoid a possible exact degeneracy
+                # between HOMO and LUMO. The exact degeneracy is avoided at the price of
+                # breaking the (k,-k)-symmetry via the MO occupations in this case,
+                # but it is possible that a real gap opens during the SCF loop,
+                # such that the symmetry is restored in the end.
+                mo_energy_k[0][kc] = e_red[0][idx] + 1e-15
+                mo_energy_k[1][kc] = e_red[1][idx] + 1e-15
+                mo_coeff_k[0][kc] = c_red[0][idx].conj()
+                mo_coeff_k[1][kc] = c_red[1][idx].conj()
+
+            # Write out (k,-k)-symmetry-error
+            mo_occ_k = self.get_occ(mo_energy_kpts=mo_energy_k)
+            dm1 = self.make_rdm1(mo_coeff_kpts=mo_coeff_k, mo_occ_kpts=mo_occ_k)
+            err = _rdm1_kconj_symmetry_error(self.cell, self.kpts, dm1)
+            logger.info(self, "Max (k,-k)-symmetry error in 1-DM: %.3e", err)
+
+            return mo_energy_k, mo_coeff_k
+
+    else:   # RHF
+        def eig(self, h_kpts, s_kpts):
+            nkpts = len(s_kpts)
+            kuniq, kconj = _get_kconj_indices(self.cell, self.kpts)
+            # Only diagonalize unique k-points:
+            h_red = h_kpts[kuniq]
+            s_red = s_kpts[kuniq]
+            # Self-conjugated k-points (e.g. Gamma) should have a real Fock matrix:
+            selfconj = np.equal(kuniq, kconj)
+            h_red[selfconj] = h_red[selfconj].real
+            # Diagonalize reduced set of k-points:
+            e_red, c_red = super(type(self), self).eig(h_red, s_red)
+            # Use lists and loops instead of NumPy arrays, since the number of MOs can be k-point dependent:
+            mo_energy_k = nkpts*[None]
+            mo_coeff_k = nkpts*[None]
+            for idx, (k, kc) in enumerate(zip(kuniq, kconj)):
+                mo_energy_k[k] = e_red[idx]
+                mo_coeff_k[k] = c_red[idx]
+                if (kc == k):
+                    continue
+                # Shift conjugated energies slightly, to avoid a possible exact degeneracy
+                # between HOMO and LUMO. The exact degeneracy is avoided at the price of
+                # breaking the (k,-k)-symmetry via the MO occupations in this case,
+                # but it is possible that a real gap opens during the SCF loop,
+                # such that the symmetry is restored in the end.
+                mo_energy_k[kc] = e_red[idx] + 1e-15
+                mo_coeff_k[kc] = c_red[idx].conj()
+
+            # Write out (k,-k)-symmetry-error
+            mo_occ_k = self.get_occ(mo_energy_kpts=mo_energy_k)
+            dm1 = self.make_rdm1(mo_coeff_kpts=mo_coeff_k, mo_occ_kpts=mo_occ_k)
+            err = _rdm1_kconj_symmetry_error(self.cell, self.kpts, dm1)
+            logger.info(self, "Max (k,-k)-symmetry error in 1-DM: %.3e", err)
+
+            return mo_energy_k, mo_coeff_k
+
+    def get_init_guess(self, *args, **kwargs):
+        dm0 = super(type(self), self).get_init_guess(*args, **kwargs)
+        dm0 = _kconj_symmetrize_rdm1(self.cell, self.kpts, dm0)
+        return dm0
+
+    def kernel(self, dm0=None, *args, **kwargs):
+        if dm0 is not None:
+            dm0 = _kconj_symmetrize_rdm1(self.cell, self.kpts, dm0)
+        res = super(type(self), self).kernel(dm0=dm0, *args, **kwargs)
+        # Print a warning to the log, if (k,-k)-symmetry is broken in the final 1-DM:
+        dm1 = self.make_rdm1()
+        kuniq, kconj = _get_kconj_indices(self.cell, self.kpts)
+        err = _rdm1_kconj_symmetry_error(self.cell, self.kpts, dm1)
+        if (err > 1e-8):
+            logger.warn(self, "(k,-k)-symmetry broken in 1-DM! Max error= %.3e", err)
+            self.converged = False
+        return res
+
+    # Dynamically create new class `cls_sym`, inheriting from `cls`, but
+    # overwriting `get_jk`, `eig`, `get_init_guess`, and `kernel`:
+    cls_sym = type('%s_kConjSym' % cls.__name__, (cls,), {'get_jk': get_jk, 'eig': eig,
+        'get_init_guess' : get_init_guess, 'kernel': kernel})
+    return cls_sym
