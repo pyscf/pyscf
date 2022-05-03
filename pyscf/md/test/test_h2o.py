5f38447d27798c8f9ca961122306aeacac836c78
Matthew Hennefarth <matthew.hennefarth@gmail.com>
2022-05-03 14:26:39 -0500

    checking with initial velocities

--- pyscf/md/test/test_h2o.py
+++ pyscf/md/test/test_h2o.py
@@ -19,14 +19,13 @@ import numpy as np
 from pyscf import gto, scf
 import pyscf.md.integrator as integrator
 
-
 h2o = gto.M(
     verbose=5,
     output='/dev/null',
     atom=[
-    ['O', 0,0,0],
-    ['H', 0, -0.757, 0.587],
-    ['H', 0, 0.757, 0.587]
+        ['O', 0, 0, 0],
+        ['H', 0, -0.757, 0.587],
+        ['H', 0, 0.757, 0.587]
     ],
     basis='def2-svp')
 
@@ -42,43 +41,58 @@ def tearDownModule():
     hf_scanner.stdout.close()
     del h2o, hf_scanner
 
+
 class KnownValues(unittest.TestCase):
     def test_zero_init_veloc(self):
         driver = integrator.VelocityVerlot(hf_scanner, dt=10, max_iterations=10)
 
         driver.kernel()
-        self.assertAlmostEqual(driver.ekin, 0.0003490353178906977,8)
-        self.assertAlmostEqual(driver.epot, -75.96132726509461, 8)
+        self.assertAlmostEqual(driver.ekin, 0.0003490353178906977, 12)
+        self.assertAlmostEqual(driver.epot, -75.96132726509461, 12)
 
-        final_coord = np.array([[-7.28115148e-19,  1.49705405e-16,  2.05638219e-03],
-                       [-9.60088675e-17, -1.41129779e+00,  1.09281818e+00],
-                       [ 1.07658710e-16,  1.41129779e+00,  1.09281818e+00]])
+        final_coord = np.array([[-7.28115148e-19, 1.49705405e-16, 2.05638219e-03],
+                                [-9.60088675e-17, -1.41129779e+00, 1.09281818e+00],
+                                [1.07658710e-16, 1.41129779e+00, 1.09281818e+00]])
 
         self.assertTrue(np.allclose(driver.mol.atom_coords(), final_coord))
         beginning_energy = driver.ekin + driver.epot
-    #     #driver.max_iterations=990
-    #     #driver.kernel()
-    #     #self.assertAlmostEqual(driver.ekin+driver.epot, beginning_energy, 6)
+
+        driver.max_iterations=990
+        driver.kernel()
+        self.assertAlmostEqual(driver.ekin+driver.epot, beginning_energy, 6)
 
     def test_init_veloc(self):
         init_veloc = np.array([
-            [0.000335,   0.000044,   0.000432],
-            [-0.000363,  -0.000178,   0.001175],
-            [-0.001129,  -0.000181,   0.000047]
-        ])
+            [0.000613, 0.000080, 0.000792],
+            [-0.000664, -0.000326, 0.002152],
+            [-0.002068, -0.000332, 0.000086]])
 
         driver = integrator.VelocityVerlot(hf_scanner,
                                            mol=h2o,
-                                           veloc = init_veloc,
-                                           dt=10, max_iterations=10,
+                                           veloc=init_veloc,
+                                           dt=10, max_iterations=1000,
                                            energy_output="BOMD.md.energies",
                                            trajectory_output="BOMD.md.xyz")
 
         driver.kernel()
-        print(driver.ekin)
+        # self.assertAlmostEqual(driver.ekin, 2.233200713192E-02, 12)
+        # self.assertAlmostEqual(driver.epot, -7.595967859449E+01, 12)
+        #
+        # final_coord = np.array([[0.05509466, 0.00421155, 0.07404655],
+        #                         [-0.05805231, -1.41284605, 1.2617895],
+        #                         [-0.18662225, 1.40144122, 1.11390423]])
+        #
+        # self.assertTrue(np.allclose(driver.mol.atom_coords(), final_coord))
+        # beginning_energy = driver.ekin + driver.epot
+        #
+        # driver.max_iterations=990
+        # driver.kernel()
+        # self.assertAlmostEqual(driver.ekin+driver.epot, beginning_energy, 3)
+
         driver.energy_output.close()
         driver.trajectory_output.close()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     print("Full Tests for H2O")
     unittest.main()
