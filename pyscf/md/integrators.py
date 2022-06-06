2775fe45c52cdb0d0b5740e7696947e7606d29c8
Matthew Hennefarth <matthew.hennefarth@gmail.com>
2022-06-06 15:19:58 -0500

    updated copywrite and remove the double closing of output file in test

--- pyscf/md/integrators.py
+++ pyscf/md/integrators.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright 2014-2018 The PySCF Developers. All Rights Reserved.
+# Copyright 2014-2022 The PySCF Developers. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Author: Matthew Hennefarth <matthew.hennefarth@gmail.com>
 
 import os
 import numpy as np
