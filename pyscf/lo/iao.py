71d030a9fd05df362b5eeb8d9f70964c0b6f2d8d
Max Nusspickel <max.nusspickel@googlemail.com>
2022-06-09 22:13:16 +0100

    Refactors IAO code

--- pyscf/lo/iao.py
+++ pyscf/lo/iao.py
@@ -25,7 +25,7 @@ ref. JCTC, 9, 4834
 from functools import reduce
 import numpy
 import scipy.linalg
-from pyscf.lib import logger, c_null_ptr
+from pyscf.lib import logger
 from pyscf import gto
 from pyscf import scf
 from pyscf import __config__
@@ -37,99 +37,90 @@ from pyscf.data.elements import is_ghost_atom
 #     vec_lowdin(iao_coeff, mol.intor('int1e_ovlp'))
 MINAO = getattr(__config__, 'lo_iao_minao', 'minao')
 
-def iao(mol, orbocc, minao=MINAO, kpts=None, lindep_threshold=1e-10):
-    '''Intrinsic Atomic Orbitals (IAO). [Ref. JCTC, 9, 4834]
+def iao(mol, orbocc, minao=MINAO, kpts=None, lindep_threshold=1e-8):
+    '''Intrinsic Atomic Orbitals. [Ref. JCTC, 9, 4834]
+
+    For large basis sets which are close to being linearly dependent,
+    the Cholesky decomposition can fail. In this case a canonical orthogonalization
+    with threshold `lindep_threshold` is used.
 
     Args:
-        mol : pyscf.gto.Mole or pyscf.pbc.gto.Cell
-            The molecule or cell object.
-        orbocc : (N, M) or (K, N, M) array
-            Occupied molecular orbitals. N= number of AOs, M= number of occupied MOs,
-            K= number of k-points (only if mol is Cell object).
+        mol : molecule or cell object
+        orbocc : 2D array
+            occupied orbitals
         minao : str, optional
-            Minimal basis set used for IAOs.
-        kpts : ndarray(K), optional
-            k-points, only used for Cell objects.
+            reference basis set for IAOs
+        kpts : 2D ndarray, optional
+            k-points, for cell objects only
         lindep_threshold : float, optional
-            For almost linearly dependent basis sets, the Cholesky decomposition can fail.
-            In this case the eigendecomposition of the overlap matrix is used
-            with eigenvectors corresponding to eigenvalues below `lindep_threshold`
-            removed.
+            threshold for canonical orthogonalization
 
     Returns:
-        a : (N, L) or (K, N, L) array
-            Non-orthogonal IAOs. Orthogonalize them as C (C^T S C)^{-1/2}, eg. using :func:`orth.lowdin`
+        non-orthogonal IAO orbitals.  Orthogonalize them as C (C^T S C)^{-1/2},
+        eg using :func:`orth.lowdin`
 
         >>> orbocc = mf.mo_coeff[:,mf.mo_occ>0]
         >>> c = iao(mol, orbocc)
         >>> numpy.dot(c, orth.lowdin(reduce(numpy.dot, (c.T,s,c))))
     '''
-    if mol.has_ecp() and minao == "minao":
-        logger.warn(mol, 'ECP/PP is used. MINAO is not a good reference AO basis for IAOs.')
-
-    def kernel(s1, s2, s12, orbocc):
-        """Make IAOs for a molecule or single k-point."""
-        s21 = s12.conj().T
-        # Minimal basis CD is not expected to fail
-        s2cd = scipy.linalg.cho_factor(s2)
-        ctild = scipy.linalg.cho_solve(s2cd, numpy.dot(s21, orbocc))
-        # Try Cholesky of computational basis first
-        try:
-            s1cd = scipy.linalg.cho_factor(s1)
-            p12 = scipy.linalg.cho_solve(s1cd, s12)
-            ctild = scipy.linalg.cho_solve(s1cd, numpy.dot(s12, ctild))
-        # For overcomplete basis sets, use eigendecomposition + canonical orthogonalization instead
-        except numpy.linalg.LinAlgError:
-            se, sv = numpy.linalg.eigh(s1)
-            logger.debug(mol, "Cholesky decomp. of overlap S failed; removing %d eigenvectors of S with eigenvalues below %.2e",
-                    numpy.count_nonzero(se < lindep_threshold), lindep_threshold)
-            keep = (se >= lindep_threshold)
-            invS = numpy.einsum("ai,i,bi->ab", sv[:,keep], 1/se[keep], sv[:,keep])
-            p12 = numpy.dot(invS, s12)
-            ctild = numpy.dot(p12, ctild)
-        # If there are no IAOs, there is nothing to orthogonalize here (this happens in one-electron systems,
-        # in the minority spin channel)
-        if ctild.shape[-1] > 0:
-            ctild = vec_lowdin(ctild, s1)
-        ccs1 = reduce(numpy.dot, (orbocc, orbocc.conj().T, s1))
-        ccs2 = reduce(numpy.dot, (ctild, ctild.conj().T, s1))
-        #a is the set of IAOs in the original basis
-        a = (p12 + reduce(numpy.dot, (ccs1, ccs2, p12)) * 2
-             - numpy.dot(ccs1, p12) - numpy.dot(ccs2, p12))
-        return a
+    if mol.has_ecp() and minao == 'minao':
+        logger.warn(mol, 'ECP/PP is used. MINAO is not a good reference AO basis in IAO.')
 
     pmol = reference_mol(mol, minao)
     # For PBC, we must use the pbc code for evaluating the integrals lest the
     # pbc conditions be ignored.
-    # DO NOT import pbcgto early and check whether mol is a cell object.
-    # "from pyscf.pbc import gto as pbcgto and isinstance(mol, pbcgto.Cell)"
-    # The code should work even pbc module is not availabe.
-    #
-    # s1 is the one electron overlap integrals (coulomb integrals)
-    # s2 is the same as s1 except in minao
-    # s12 are the overlap integrals of the two molecules
-    #if getattr(mol, 'pbc_intor', None):  # cell object has pbc_intor method
-    if hasattr(mol, 'a') and mol.a is not None:
+    has_pbc = getattr(mol, 'dimension', 0) > 1
+    if has_pbc:
         from pyscf.pbc import gto as pbcgto
-        s1 = numpy.asarray(mol.pbc_intor('int1e_ovlp', hermi=1, kpts=kpts, pbcopt=c_null_ptr()))
-        s2 = numpy.asarray(pmol.pbc_intor('int1e_ovlp', hermi=1, kpts=kpts, pbcopt=c_null_ptr()))
-        s12 = numpy.asarray(pbcgto.cell.intor_cross('int1e_ovlp', mol, pmol, kpts=kpts, pbcopt=c_null_ptr()))
+        s1 = mol.pbc_intor('int1e_ovlp', hermi=1, kpts=kpts)
+        s2 = pmol.pbc_intor('int1e_ovlp', hermi=1, kpts=kpts)
+        s12 = pbcgto.cell.intor_cross('int1e_ovlp', mol, pmol, kpts=kpts)
     else:
+        #s1 is the one electron overlap integrals (coulomb integrals)
         s1 = mol.intor_symmetric('int1e_ovlp')
+        #s2 is the same as s1 except in minao
         s2 = pmol.intor_symmetric('int1e_ovlp')
+        #overlap integrals of the two molecules
         s12 = gto.mole.intor_cross('int1e_ovlp', mol, pmol)
 
-    # Molecule
-    if len(s1.shape) == 2:
-        a = kernel(s1, s2, s12, orbocc)
-    # k-point sampling
+    def make_iaos(s1, s2, s12, mo):
+        """Make IAOs for a molecule or single k-point"""
+        s21 = s12.conj().T
+        # s2 is overlap in minimal reference basis and should never be singular:
+        s2cd = scipy.linalg.cho_factor(s2)
+        ctild = scipy.linalg.cho_solve(s2cd, numpy.dot(s21, mo))
+        try:
+            s1cd = scipy.linalg.cho_factor(s1)
+            p12 = scipy.linalg.cho_solve(s1cd, s12)
+            ctild = scipy.linalg.cho_solve(s1cd, numpy.dot(s12, ctild))
+        # s1 can be singular in large basis sets: Use canonical orthogonalization in this case:
+        except numpy.linalg.LinAlgError:
+            se, sv = numpy.linalg.eigh(s1)
+            keep = (se >= lindep_threshold)
+            sinv = numpy.einsum("ai,i,bi->ab", sv[:,keep], 1/se[keep], sv[:,keep])
+            p12 = numpy.dot(sinv, s12)
+            ctild = numpy.dot(p12, ctild)
+        # If there are no occupied orbitals at this k-point, all but the first term will vanish:
+        if mo.shape[-1] == 0:
+            return p12
+        ctild = vec_lowdin(ctild, s1)
+        ccs1 = numpy.linalg.multi_dot((mo, mo.conj().T, s1))
+        ccs2 = numpy.linalg.multi_dot((ctild, ctild.conj().T, s1))
+        #a is the set of IAOs in the original basis
+        a = (p12 + 2*numpy.linalg.multi_dot((ccs1, ccs2, p12))
+             - numpy.dot(ccs1, p12) - numpy.dot(ccs2, p12))
+        return a
+
+    # Molecules and Gamma-point only solids
+    if s1[0].ndim == 1:
+        iaos = make_iaos(s1, s2, s12, orbocc)
+    # Solid with multiple k-points
     else:
-        a = []
+        iaos = []
         for k in range(len(kpts)):
-            ak = kernel(s1[k], s2[k], s12[k], orbocc[k])
-            a.append(ak)
-        a = numpy.asarray(a)
-    return a
+            iaos.append(make_iaos(s1[k], s2[k], s12[k], orbocc[k]))
+        iaos = numpy.asarray(iaos)
+    return iaos
 
 def reference_mol(mol, minao=MINAO):
     '''Create a molecule which uses reference minimal basis'''
