2502db101a8227f45118867b4da1e5248b328527
CanonYeh <cnyeh@umich.edu>
2022-07-01 18:22:10 -0400

    clean up

--- pyscf/pbc/dft/test/test_kgks.py
+++ pyscf/pbc/dft/test/test_kgks.py
@@ -37,16 +37,6 @@ cell.build()
 kmesh = [2, 1, 1]
 kpts = cell.make_kpts(kmesh, wrap_around=True)
 
-
-H_cell = gto.Cell()
-H_cell.build(unit = 'B',
-           a = np.eye(3)*4,
-           mesh = [11]*3,
-           atom = 'H 0 0 0; H 0 0 1.8',
-           verbose = 0,
-           basis='sto3g')
-H_kpts = H_cell.make_kpts([2,1,1], wrap_around=True)
-
 alle_cell = gto.Cell()
 alle_cell.unit = 'A'
 alle_cell.atom = 'C 0.,  0.,  0.; C 0.8917,  0.8917,  0.8917'
@@ -63,7 +53,6 @@ alle_kpts = alle_cell.make_kpts(kmesh, wrap_around=True)
 class KnownValues(unittest.TestCase):
     def test_KGKS(self):
         # In the absence of off diagonal blcoks in the spin space, dft.KGKS should reprocduce the dft.KRKS results
-
         # Reference from dft.KRKS
         mf = dft.KRKS(cell, kpts)
         mf.xc = 'lda'
@@ -76,11 +65,10 @@ class KnownValues(unittest.TestCase):
         e_kgks = mf.kernel()
         print("e_kgks: {}".format(e_kgks))
         self.assertAlmostEqual(e_kgks, e_ref, 8)
-    # FIXME Tests below take too much time. A better example is needed.
+
     def test_KGKS_sfx2c1e(self):
         with lib.light_speed(10) as c:
           mf = dft.KGKS(alle_cell, alle_kpts).density_fit().sfx2c1e()
-          mf.with_x2c.approx = 'NONE'
           mf.xc = 'lda'
           mf.conv_tol = 1e-10
           e_kgks = mf.kernel()
@@ -89,7 +77,6 @@ class KnownValues(unittest.TestCase):
     def test_KGKS_x2c1e(self):
         with lib.light_speed(10) as c:
           mf = dft.KGKS(alle_cell, alle_kpts).density_fit().x2c1e()
-          mf.with_x2c.approx = 'NONE'
           mf.xc = 'lda'
           mf.conv_tol = 1e-10
           e_kgks = mf.kernel()
