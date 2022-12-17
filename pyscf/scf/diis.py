1134fa908854bb3926498ba38d0a2109d4eb47a4
Qiming Sun <osirpt.sun@gmail.com>
2022-12-16 17:09:53 -0800

    Fix lint

--- pyscf/scf/diis.py
+++ pyscf/scf/diis.py
@@ -64,20 +64,19 @@ def get_err_vec_orig(s, d, f):
     '''error vector = SDF - FDS'''
     if isinstance(f, numpy.ndarray) and f.ndim == 2:
         sdf = reduce(numpy.dot, (s,d,f))
-        errvec = sdf.T.conj() - sdf
+        errvec = (sdf.conj().T - sdf).ravel()
 
     elif isinstance(f, numpy.ndarray) and f.ndim == 3 and s.ndim == 3:
         errvec = []
         for i in range(f.shape[0]):
             sdf = reduce(numpy.dot, (s[i], d[i], f[i]))
-            errvec.append(sdf.T.conj() - sdf)
-        errvec = numpy.vstack(errvec)
+            errvec.append((sdf.conj().T - sdf).ravel())
+        errvec = numpy.hstack(errvec)
 
     elif f.ndim == s.ndim+1 and f.shape[0] == 2:  # for UHF
-        nao = s.shape[-1]
-        s = lib.asarray((s,s)).reshape(-1,nao,nao)
-        return get_err_vec(s, d.reshape(s.shape), f.reshape(s.shape))
-
+        errvec = numpy.hstack([
+            get_err_vec_orig(s, d[0], f[0]).ravel(),
+            get_err_vec_orig(s, d[1], f[1]).ravel()])
     else:
         raise RuntimeError('Unknown SCF DIIS type')
     return errvec
@@ -86,27 +85,20 @@ def get_err_vec_orth(s, d, f, Corth):
     '''error vector in orthonormal basis = C.T.conj() (SDF - FDS) C'''
     if isinstance(f, numpy.ndarray) and f.ndim == 2:
         sdf = reduce(numpy.dot, (s,d,f))
-        errvec = numpy.dot(Corth.T.conj(), numpy.dot(sdf.T.conj() - sdf, Corth)).flatten()
+        errvec = Corth.conj().T.dot(sdf.conj().T - sdf).dot(Corth).ravel()
 
     elif isinstance(f, numpy.ndarray) and f.ndim == 3 and s.ndim == 3:
-        # KUHF gives Corth as a double list; concatenate the two lists into one
-        if isinstance(Corth, tuple) and len(Corth)>0 and isinstance(Corth[0], list):
-            my_Corth = [];
-            for c in Corth:
-                for e in c:
-                    my_Corth.append(e)
-        else:
-            my_Corth = Corth
-
-        errvec = numpy.asarray([])
+        errvec = []
         for i in range(f.shape[0]):
             sdf = reduce(numpy.dot, (s[i], d[i], f[i]))
-            errvec = numpy.append(errvec, numpy.dot(my_Corth[i].T.conj(), numpy.dot((sdf.T.conj() - sdf), my_Corth[i])).flatten())
+            errvec.append(
+                Corth[i].conj().T.dot(sdf.conj().T - sdf).dot(Corth[i]).ravel())
+        errvec = numpy.vstack(errvec).ravel()
 
     elif f.ndim == s.ndim+1 and f.shape[0] == 2:  # for UHF
-        nao = s.shape[-1]
-        s = lib.asarray((s,s)).reshape(-1,nao,nao)
-        return get_err_vec(s, d.reshape(s.shape), f.reshape(s.shape), Corth)
+        errvec = numpy.hstack([
+            get_err_vec_orth(s, d[0], f[0], Corth[0]).ravel(),
+            get_err_vec_orth(s, d[1], f[1], Corth[1]).ravel()])
     else:
         raise RuntimeError('Unknown SCF DIIS type')
     return errvec
