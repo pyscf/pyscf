c5ea66b59fdb21cdaaf6cc93dbd5cee0f1be8efe
Wanja Schulze <wangenau@protonmail.com>
2023-03-17 15:37:54 +0100

    lo/boys: Remove factor 2 from <r>^2 in cost_function

--- pyscf/lo/boys.py
+++ pyscf/lo/boys.py
@@ -299,7 +299,7 @@ class Boys(ciah.CIAHOptimizer):
         with self.mol.with_common_origin(charge_center):
             r2 = self.mol.intor_symmetric('int1e_r2')
         r2 = numpy.einsum('pi,pi->', mo_coeff, lib.dot(r2, mo_coeff))
-        val = r2 - numpy.einsum('xii,xii->', dip, dip) * 2
+        val = r2 - numpy.einsum('xii,xii->', dip, dip)
         return val
 
     def get_init_guess(self, key='atomic'):
