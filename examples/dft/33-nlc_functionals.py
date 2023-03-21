1258957b0886c253224d43bee987e30a75e55e31
Qiming Sun <osirpt.sun@gmail.com>
2023-03-21 00:37:43 -0700

    Update NLC functional example (fix issue #1367)

--- examples/dft/33-nlc_functionals.py
+++ examples/dft/33-nlc_functionals.py
@@ -5,13 +5,12 @@
 
 '''
 A simple example to run density functional with non-local correlation calculation.
-
-Available NLC functionals: wB97M_V, wB97X_V, B97M_V
 '''
 
 from pyscf import gto, dft
 
-mol = gto.M(atom='H    0.000000000  -0.120407870  -0.490828400; F    0.000000000   0.009769450  -1.404249780', basis='6-31G', symmetry=False, verbose=10, unit='Angstrom', spin=0)
+mol = gto.M(atom='H    0.000000000  -0.120407870  -0.490828400; F    0.000000000   0.009769450  -1.404249780',
+            basis='6-31G')
 mf = dft.RKS(mol)
 mf.xc='wB97M_V'
 mf.nlc='VV10'
