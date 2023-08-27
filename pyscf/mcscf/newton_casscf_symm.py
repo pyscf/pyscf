f437b4562eeeab49566ae6684be914e18930a0fc
Junjie, Yang <yangjunjie0320@gmail.com>
2023-08-27 09:24:23 -0700

    Fix bug in symm cas methods (#1817)
    * fix bug in symm cas methods
    
    * Fix flake8 W293

--- pyscf/mcscf/newton_casscf_symm.py
+++ pyscf/mcscf/newton_casscf_symm.py
@@ -37,6 +37,8 @@ class CASSCF(newton_casscf.CASSCF):
     def kernel(self, mo_coeff=None, ci0=None, callback=None, _kern=None):
         if mo_coeff is None:
             mo_coeff = self.mo_coeff
+        else: # overwrite self.mo_coeff because it is needed in many methods of this class
+            self.mo_coeff = mo_coeff
         if callback is None: callback = self.callback
         if _kern is None: _kern = newton_casscf.kernel
 
