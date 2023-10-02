7089fd1a3fbff3fe557f5e0fc528fe27dc074854
Xiaojie Wu <wxj6000@gmail.com>
2023-10-02 09:50:24 -0700

    cleanup variables

--- pyscf/solvent/pcm.py
+++ pyscf/solvent/pcm.py
@@ -1,22 +1,24 @@
-# Copyright 2023 The GPU4PySCF Authors. All Rights Reserved.
+#!/usr/bin/env python
+# Copyright 2014-2023 The PySCF Developers. All Rights Reserved.
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Xiaojie Wu <wxj6000@gmail.com>
+#
 
 '''
-PCM family solvent model
+PCM family solvent models
 '''
-# pylint: disable=C0103
 
 import numpy
 import scipy
@@ -38,7 +40,7 @@ def pcm_for_scf(mf, solvent_obj=None, dm=None):
     return _attach_solvent._for_scf(mf, solvent_obj, dm)
 
 
-# Inject ddPCM to other methods
+# Inject PCM to other methods
 from pyscf import scf
 from pyscf import mcscf
 from pyscf import mp, ci, cc
@@ -325,12 +327,10 @@ def grad_kernel(pcmobj, dm):
     gridslice    = pcmobj.surface['gslice_by_atom']
     grid_coords  = pcmobj.surface['grid_coords']
     exponents    = pcmobj.surface['charge_exp']
-    switch_fun   = pcmobj.surface['switch_fun']
     v_grids      = pcmobj._intermediates['v_grids']
     A            = pcmobj._intermediates['A']
     D            = pcmobj._intermediates['D']
     S            = pcmobj._intermediates['S']
-    R            = pcmobj._intermediates['R']
     K            = pcmobj._intermediates['K']
     q            = pcmobj._intermediates['q']
     q_sym        = pcmobj._intermediates['q_sym']
@@ -550,10 +550,8 @@ class PCM(ddcosmo.DDCOSMO):
         if not self._intermediates or self.grids.coords is None:
             self.build()
 
-        mol = self.mol
         nao = dms.shape[-1]
         dms = dms.reshape(-1,nao,nao)
-        n_dm = dms.shape[0]
 
         K = self._intermediates['K']
         R = self._intermediates['R']
@@ -609,8 +607,6 @@ class PCM(ddcosmo.DDCOSMO):
     def _get_vmat(self, q):
         mol = self.mol
         nao = mol.nao
-        atom_coords = mol.atom_coords(unit='B')
-        atom_charges = mol.atom_charges()
         grid_coords = self.surface['grid_coords']
         exponents   = self.surface['charge_exp']
 
