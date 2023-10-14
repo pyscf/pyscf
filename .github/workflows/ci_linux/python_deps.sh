10f0883a9f05014254474e6937e27d0dcd440dfd
Qiming Sun <osirpt.sun@gmail.com>
2023-10-14 10:13:57 -0700

    Adjust CI pipeline for python 3.12

--- .github/workflows/ci_linux/python_deps.sh
+++ .github/workflows/ci_linux/python_deps.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 python -m pip install --upgrade pip
 pip install "numpy!=1.16,!=1.17" "scipy!=1.5" h5py pytest pytest-cov pytest-timer
-pip install pyberny geometric
+pip install pyberny
 pip install spglib
 
-#cppe
 version=$(python -c 'import sys; version=sys.version_info[:2]; print("{0}.{1}".format(*version))')
-if [ $version != '2.7' ] && [ $version != '3.5' ]; then
+if [ $version != '3.12' ]; then
+    pip install geometric
+fi
+
+#cppe
+if [ $version == '3.12' ]; then
     pip install cppe
 fi
