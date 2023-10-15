785aff23944c68edba73e59fe2baef8ddb1b020e
Qiming Sun <osirpt.sun@gmail.com>
2023-10-14 23:12:04 -0700

    Pytest-cov uses a large amount of memory in CI jobs

--- .github/workflows/ci_macos/python_deps.sh
+++ .github/workflows/ci_macos/python_deps.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 python -m pip install --upgrade pip
 pip install "numpy!=1.16,!=1.17" scipy h5py pytest pytest-cov pytest-timer
-pip install pyberny geometric
+pip install pyberny
 
 #cppe
 version=$(python -c 'import sys; version=sys.version_info[:2]; print("{0}.{1}".format(*version))')
-if [ $version != '2.7' ] && [ $version != '3.5' ]; then
+if [ $version != '3.12' ]; then
+    pip install geometric
+fi
+
+if [ $version != '3.5' ]; then
     pip install cppe
 fi
