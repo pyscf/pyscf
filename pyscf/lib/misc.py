1dbf0449ba3f50a8666379a716e5db70f473b252
Qiming Sun <osirpt.sun@gmail.com>
2023-11-20 12:34:06 -0800

    Update cistring module for issue #1886

--- pyscf/lib/misc.py
+++ pyscf/lib/misc.py
@@ -348,6 +348,8 @@ if sys.version_info > (3, 8):
 else:
     import math
     def comb(n, k):
+        if k < 0 or k > n:
+            return 0
         return math.factorial(n) // math.factorial(n-k) // math.factorial(k)
 
 def map_with_prefetch(func, *iterables):
