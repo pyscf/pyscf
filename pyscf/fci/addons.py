36be33cf96fa5aa9005cf7f5af772c4a97ef61d2
Victor Yu <victor.wz.yu.1012@gmail.com>
2024-02-26 19:50:55 -0600

    Separate occ_masks into a function

--- pyscf/fci/addons.py
+++ pyscf/fci/addons.py
@@ -658,36 +658,15 @@ def transform_ci(ci, nelec, u):
     nb_new = cistring.num_strings(norb_new, nelecb)
     ci = ci.reshape(na_old, nb_old)
 
-    one_particle_strs_old = numpy.asarray(cistring.make_strings(range(norb_old), 1))
-    one_particle_strs_new = numpy.asarray(cistring.make_strings(range(norb_new), 1))
-
     if neleca == 0:
         trans_ci_a = numpy.ones((1, 1))
     else:
         trans_ci_a = numpy.zeros((na_old, na_new), dtype=ua.dtype)
-
-        # Unitary transformation array trans_ci is the overlap between two sets of CI basis.
-        strs_old = numpy.asarray(cistring.make_strings(range(norb_old), neleca))
-        if norb_old < 64:
-            occ_masks_old = (strs_old[:,None] & one_particle_strs_old) != 0
-        else:
-            occ_masks_old = numpy.zeros((na_old, norb_old), dtype=bool)
-            for i in range(na_old):
-                for j in range(norb_old):
-                    if one_particle_strs_old[j][0] in strs_old[i]:
-                        occ_masks_old[i,j] = True
+        occ_masks_old = _init_occ_masks(norb_old, neleca, na_old)
         if norb_old == norb_new:
             occ_masks_new = occ_masks_old
         else:
-            strs_new = numpy.asarray(cistring.make_strings(range(norb_new), neleca))
-            if norb_new < 64:
-                occ_masks_new = (strs_new[:,None] & one_particle_strs_new) != 0
-            else:
-                occ_masks_new = numpy.zeros((na_new, norb_new), dtype=bool)
-                for i in range(na_new):
-                    for j in range(norb_new):
-                        if one_particle_strs_new[j][0] in strs_new[i]:
-                            occ_masks_new[i,j] = True
+            occ_masks_new = _init_occ_masks(norb_new, neleca, na_new)
 
         # Perform
         #for i in range(na_old): # old basis
@@ -706,28 +685,11 @@ def transform_ci(ci, nelec, u):
         trans_ci_b = numpy.ones((1, 1))
     else:
         trans_ci_b = numpy.zeros((nb_old, nb_new), dtype=ub.dtype)
-
-        strs_old = numpy.asarray(cistring.make_strings(range(norb_old), nelecb))
-        if norb_old < 64:
-            occ_masks_old = (strs_old[:,None] & one_particle_strs_old) != 0
-        else:
-            occ_masks_old = numpy.zeros((nb_old, norb_old), dtype=bool)
-            for i in range(nb_old):
-                for j in range(norb_old):
-                    if one_particle_strs_old[j][0] in strs_old[i]:
-                        occ_masks_old[i,j] = True
+        occ_masks_old = _init_occ_masks(norb_old, nelecb, nb_old)
         if norb_old == norb_new:
             occ_masks_new = occ_masks_old
         else:
-            strs_new = numpy.asarray(cistring.make_strings(range(norb_new), nelecb))
-            if norb_new < 64:
-                occ_masks_new = (strs_new[:,None] & one_particle_strs_new) != 0
-            else:
-                occ_masks_new = numpy.zeros((nb_new, norb_new), dtype=bool)
-                for i in range(nb_new):
-                    for j in range(norb_new):
-                        if one_particle_strs_new[j][0] in strs_new[i]:
-                            occ_masks_new[i,j] = True
+            occ_masks_new = _init_occ_masks(norb_new, nelecb, nb_new)
 
         occ_idx_all_strs = numpy.where(occ_masks_new)[1].reshape(nb_new,nelecb)
         for i in range(nb_old):
@@ -752,5 +714,18 @@ def _unpack_nelec(nelec, spin=None):
         neleca = nelec - nelecb
         nelec = neleca, nelecb
     return nelec
+
+def _init_occ_masks(norb, nelec, nci):
+    one_particle_strs = numpy.asarray(cistring.make_strings(range(norb), 1))
+    strs = numpy.asarray(cistring.make_strings(range(norb), nelec))
+    if norb < 64:
+        occ_masks = (strs[:,None] & one_particle_strs) != 0
+    else:
+        occ_masks = numpy.zeros((nci, norb), dtype=bool)
+        for i in range(nci):
+            for j in range(norb):
+                if one_particle_strs[j][0] in strs[i]:
+                    occ_masks[i,j] = True
+    return occ_masks
 
 del (LARGE_CI_TOL, RETURN_STRS, PENALTY)
