2b64786286069c6010f8e7967285e7e537681de6
Christopher Hillenbrand <chillenbrand15@gmail.com>
2024-03-19 19:42:11 -0400

    libcint, xcfun, libxc search

--- .github/workflows/ci_linux/build_pyscf.sh
+++ .github/workflows/ci_linux/build_pyscf.sh
@@ -6,7 +6,7 @@ cd ./pyscf/lib
 curl -L "https://github.com/pyscf/pyscf-build-deps/blob/master/pyscf-2.4a-deps.tar.gz?raw=true" | tar xzf -
 mkdir build; cd build
 cmake -DBUILD_LIBXC=OFF -DBUILD_XCFUN=ON -DBUILD_LIBCINT=OFF -DXCFUN_MAX_ORDER=4 ..
-make -j4
+make -j
 cd ..
 rm -Rf build
 cd ../..
