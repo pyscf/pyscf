77e13d0de49dde3bf3ee11a9a3ea5f9a86705a24
Hong-Zhou Ye <hzyechem@gmail.com>
2024-04-13 01:16:56 -0400

    Bug fix for single k-point JK-build in PBC DF (#2165)
    * bug fix for j-build in get_jk
    
    * bug fix for k-build in get_jk
    
    ---------
    
    Co-authored-by: hongzhouye <>

--- pyscf/pbc/df/df_jk.py
+++ pyscf/pbc/df/df_jk.py
@@ -1261,12 +1261,12 @@ def get_jk(mydf, dm, hermi=1, kpt=numpy.zeros(3),
         if with_j:
             #:rho_coeff = numpy.einsum('Lpq,xqp->xL', Lpq, dms)
             #:vj += numpy.dot(rho_coeff, Lpq.reshape(-1,nao**2))
-            rhoR  = numpy.einsum('Lpq,xpq->xL', LpqR, dmsR)
+            rhoR  = numpy.einsum('Lpq,xqp->xL', LpqR, dmsR)
             if not j_real:
                 LpqI = LpqI.reshape(-1,nao,nao)
-                rhoR -= numpy.einsum('Lpq,xpq->xL', LpqI, dmsI)
-                rhoI  = numpy.einsum('Lpq,xpq->xL', LpqR, dmsI)
-                rhoI += numpy.einsum('Lpq,xpq->xL', LpqI, dmsR)
+                rhoR -= numpy.einsum('Lpq,xqp->xL', LpqI, dmsI)
+                rhoI  = numpy.einsum('Lpq,xqp->xL', LpqR, dmsI)
+                rhoI += numpy.einsum('Lpq,xqp->xL', LpqI, dmsR)
             vjR += sign * numpy.einsum('xL,Lpq->xpq', rhoR, LpqR)
             if not j_real:
                 vjR -= sign * numpy.einsum('xL,Lpq->xpq', rhoI, LpqI)
