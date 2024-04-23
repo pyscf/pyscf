14f9116d3d499c98d66f59089a374d00e4ec82ec
Xiaojie Wu <wxj6000@gmail.com>
2024-04-22 19:10:25 -0700

    reinstall gcc

--- .github/workflows/ci_macos/build_pyscf.sh
+++ .github/workflows/ci_macos/build_pyscf.sh
@@ -6,7 +6,7 @@ cd ./pyscf/lib
 #curl -L https://github.com/fishjojo/pyscf-deps/raw/master/pyscf-1.7.5-deps-macos-10.14.tar.gz | tar xzf -
 mkdir build; cd build
 #cmake -DBUILD_LIBXC=OFF -DBUILD_XCFUN=OFF ..
-cmake -DCMAKE_Fortran_COMPILER=/Applications/Xcode_14.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/gfortran ..
+cmake ..
 make -j4
 cd ..
 rm -Rf build
