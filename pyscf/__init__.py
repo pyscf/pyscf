00d892fef424cc3bee2ba0f0340cd9226cfa4afa
Xiaojie Wu <wxj6000@gmail.com>
2024-04-30 09:05:21 -0700

    import hessian

--- pyscf/__init__.py
+++ pyscf/__init__.py
@@ -98,6 +98,7 @@ from pyscf import lib
 from pyscf import gto
 from pyscf import scf
 from pyscf import ao2mo
+from pyscf import hessian
 
 # Whether to enable debug mode. When this flag is set, some modules may run
 # extra debug code.
