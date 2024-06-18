28c776aee5c5e6fe5ea57c6f2be20fb329b7342b
Qiming Sun <osirpt.sun@gmail.com>
2024-06-18 00:46:09 -0700

    Handle dynamically registered post-HF attributes

--- pyscf/scf/hf.py
+++ pyscf/scf/hf.py
@@ -1569,6 +1569,12 @@ class SCF(lib.StreamObject):
         self._opt = {None: None}
         self._eri = None # Note: self._eri requires large amount of memory
 
+    def __getattr__(self, key):
+        '''Accessing methods post-HF methods or mean-field properties'''
+        # Import all available modules, then retry accessing the attribute
+        from pyscf import __all__  # noqa
+        return object.__getattribute__(self, key)
+
     def check_sanity(self):
         s1e = self.get_ovlp()
         cond = lib.cond(s1e)
