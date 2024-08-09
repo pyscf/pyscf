318c7f35dc318a68f7704cbca811f62a46ad0ea1
Brian Zhao <zijun.zhao@emory.edu>
2024-08-09 17:01:35 -0400

    Add spin-separated rdm123

--- examples/mcscf/04-density_matrix.py
+++ examples/mcscf/04-density_matrix.py
@@ -32,3 +32,13 @@ dm1_alpha, dm1_beta = mycas.make_rdm1s()
 
 print(numpy.allclose(dm1, dm1_alpha+dm1_beta))
 
+dm1, dm2, dm3 = mycas.fcisolver.make_rdm123(mycas.ci, mycas.ncas, mycas.nelecas)
+
+(dm1a, dm1b), (dm2aa, dm2ab, dm2bb), (dm3aaa, dm3aab, dm3abb, dm3bbb) = \
+        mycas.fcisolver.make_rdm123s(mycas.ci, mycas.ncas, mycas.nelecas)
+
+print(numpy.allclose(dm1a+dm1b, dm1))
+print(numpy.allclose(dm2aa+dm2bb+dm2ab+dm2ab.transpose(2,3,0,1), dm2))
+print(numpy.allclose(dm3aaa+dm3bbb+dm3aab+dm3aab.transpose(0,1,4,5,2,3)+\
+dm3aab.transpose(4,5,0,1,2,3)+dm3abb+dm3abb.transpose(2,3,0,1,4,5)+dm3abb.transpose(2,3,4,5,0,1), dm3))
+
