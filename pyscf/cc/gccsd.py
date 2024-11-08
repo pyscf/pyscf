Fix: remove .conj() from eris_oovv at the emp2 computation step

pyscf/cc/gccsd.py
@@ -129,7 +129,7 @@ class GCCSD(ccsd.CCSDBase):
         t1 = eris.fock[:nocc,nocc:] / eia
         eris_oovv = np.array(eris.oovv)
         t2 = eris_oovv.conj() / eijab
-        self.emp2 = 0.25*einsum('ijab,ijab', t2, eris_oovv.conj()).real
+        self.emp2 = 0.25*einsum('ijab,ijab', t2, eris_oovv).real
         logger.info(self, 'Init t2, MP2 energy = %.15g', self.emp2)
         return self.emp2, t1, t2
 