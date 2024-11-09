dca85c22f612f3b284a0a27f7267d5f579293f4b
chillenb <chillenbrand15@gmail.com>
2024-11-09 12:54:52 -0500

    make requested changes to simplify code

--- pyscf/lib/np_helper/imatcopy.c
+++ pyscf/lib/np_helper/imatcopy.c
@@ -23,102 +23,104 @@
 
 const int TILESIZE = 32;
 
-#define ELEM_AT(A, i, j, lda) (A)[(i) * (lda) + (j)]
-
 /*
  * Calculate the largest integer i such that
  *  i * (i - 1) / 2 <= ijouter.
  */
-static int uncollapse_loop_index(long long ijouter)
+static inline int uncollapse_loop_index(const long long ijouter)
 {
   return (int) floor((sqrt(0.25 + 2.0 * ijouter) + 0.5));
 }
 
-
-static inline void dtranspose_scale_tile_offdiag(double *A, const int ii, const int jj,
-                                          int lda, const double alpha) {
+static inline void dtranspose_scale_tile_offdiag(double *A, const int ii,
+                                                 const int jj,
+                                                 const size_t lda_w,
+                                                 const double alpha) {
   for (int j = jj; j < jj + TILESIZE; j++) {
 #pragma omp simd
     for(int i = ii; i < ii + TILESIZE; i++) {
-      const double tmp = ELEM_AT(A, i, j, lda);
-      ELEM_AT(A, i, j, lda) = alpha * ELEM_AT(A, j, i, lda);
-      ELEM_AT(A, j, i, lda) = alpha * tmp;
+      const double tmp = A[i * lda_w + j];
+      A[i * lda_w + j] = alpha * A[j * lda_w + i];
+      A[j * lda_w + i] = alpha * tmp;
     }
   }
 }
 
-static inline void dtranspose_scale_tile_diag(double *A, const int ii, const int jj,
-                                       int lda, const double alpha) {
+static inline void dtranspose_scale_tile_diag(double *A, const int ii,
+                                              const int jj, const size_t lda_w,
+                                              const double alpha) {
   for (int j = jj; j < jj + TILESIZE; j++) {
 #pragma omp simd
     for(int i = ii; i < j; i++) {
-      const double tmp = ELEM_AT(A, i, j, lda);
-      ELEM_AT(A, i, j, lda) = alpha * ELEM_AT(A, j, i, lda);
-      ELEM_AT(A, j, i, lda) = alpha * tmp;
+      const double tmp = A[i * lda_w + j];
+      A[i * lda_w + j] = alpha * A[j * lda_w + i];
+      A[j * lda_w + i]= alpha * tmp;
     }
-    ELEM_AT(A, j, j, lda) *= alpha;
+    A[j * lda_w + j] *= alpha;
   }
 }
 
 static inline void dtranspose_tile_diag(double *A, const int ii, const int jj,
-                                       int lda) {
+                                        const size_t lda_w) {
   for (int j = jj; j < jj + TILESIZE; j++) {
 #pragma omp simd
     for(int i = ii; i < j; i++) {
-      const double tmp = ELEM_AT(A, i, j, lda);
-      ELEM_AT(A, i, j, lda) = ELEM_AT(A, j, i, lda);
-      ELEM_AT(A, j, i, lda) = tmp;
+      const double tmp = A[i * lda_w + j];
+      A[i * lda_w + j] = A[j * lda_w + i];
+      A[j * lda_w + i] = tmp;
     }
   }
 }
 
-static inline void ztranspose_scale_tile_offdiag(double complex *A, const int ii,
-                                          const int jj, int lda,
-                                          const double complex alpha) {
+static inline void ztranspose_scale_tile_offdiag(double complex *A,
+                                                 const int ii, const int jj,
+                                                 const size_t lda_w,
+                                                 const double complex alpha) {
   for (int j = jj; j < jj + TILESIZE; j++) {
 #pragma omp simd
     for(int i = ii; i < ii + TILESIZE; i++) {
-      const double complex tmp = ELEM_AT(A, i, j, lda);
-      ELEM_AT(A, i, j, lda) = alpha * ELEM_AT(A, j, i, lda);
-      ELEM_AT(A, j, i, lda) = alpha * tmp;
+      const double complex tmp = A[i * lda_w + j];
+      A[i * lda_w + j] = alpha * A[j * lda_w + i];
+      A[j * lda_w + i] = alpha * tmp;
     }
   }
 }
 
 static inline void ztranspose_scale_tile_diag(double complex *A, const int ii,
-                                       const int jj, int lda,
-                                       const double complex alpha) {
+                                              const int jj, const size_t lda_w,
+                                              const double complex alpha) {
   for (int j = jj; j < jj + TILESIZE; j++) {
 #pragma omp simd
     for(int i = ii; i < j; i++) {
-      const double complex tmp = ELEM_AT(A, i, j, lda);
-      ELEM_AT(A, i, j, lda) = alpha * ELEM_AT(A, j, i, lda);
-      ELEM_AT(A, j, i, lda) = alpha * tmp;
+      const double complex tmp = A[i * lda_w + j];
+      A[i * lda_w + j] = alpha * A[j * lda_w + i];
+      A[j * lda_w + i] = alpha * tmp;
     }
-    ELEM_AT(A, j, j, lda) *= alpha;
+    A[j * lda_w + j] *= alpha;
   }
 }
 
 static inline void ztranspose_tile_diag(double complex *A, const int ii,
-                                       const int jj, int lda) {
+                                       const int jj, const size_t lda_w) {
   for (int j = jj; j < jj + TILESIZE; j++) {
 #pragma omp simd
     for(int i = ii; i < j; i++) {
-      const double complex tmp = ELEM_AT(A, i, j, lda);
-      ELEM_AT(A, i, j, lda) = ELEM_AT(A, j, i, lda);
-      ELEM_AT(A, j, i, lda) = tmp;
+      const double complex tmp = A[i * lda_w + j];
+      A[i * lda_w + j] = A[j * lda_w + i];
+      A[j * lda_w + i] = tmp;
     }
   }
 }
 
 /*
- * In-place parallel matrix transpose
+ * In-place parallel matrix transpose, double version.
  * See https://colfaxresearch.com/multithreaded-transposition-of-square-matrices-with-common-code-for-intel-xeon-processors-and-intel-xeon-phi-coprocessors/
  */
 void NPomp_d_itranspose_scale(const int n, const double alpha, double *A, int lda)
 {
   const int nclean = n - n % TILESIZE;
   const int ntiles = nclean / TILESIZE;
+  const size_t lda_w = (size_t) lda;
 
 #pragma omp parallel
   {
@@ -150,7 +152,7 @@ void NPomp_d_itranspose_scale(const int n, const double alpha, double *A, int ld
  */
     int first_iteration = 1;
     int iouter, jouter;
-#pragma omp for nowait
+#pragma omp for schedule(static) nowait
     for(long long ijouter = 0; ijouter < (ntiles*(ntiles-1))/2; ijouter++) {
       if(first_iteration) {
         iouter = uncollapse_loop_index(ijouter);
@@ -163,7 +165,7 @@ void NPomp_d_itranspose_scale(const int n, const double alpha, double *A, int ld
           jouter = 0;
         }
       }
-      dtranspose_scale_tile_offdiag(A, iouter * TILESIZE, jouter * TILESIZE, lda, alpha);
+      dtranspose_scale_tile_offdiag(A, iouter * TILESIZE, jouter * TILESIZE, lda_w, alpha);
     }
 
 
@@ -188,12 +190,12 @@ void NPomp_d_itranspose_scale(const int n, const double alpha, double *A, int ld
     if(alpha != 1.0) {
 #pragma omp for schedule(static) nowait
       for(int ii = 0; ii < nclean; ii+=TILESIZE) {
-        dtranspose_scale_tile_diag(A, ii, ii, lda, alpha);
+        dtranspose_scale_tile_diag(A, ii, ii, lda_w, alpha);
       }
     } else {
 #pragma omp for schedule(static) nowait
       for(int ii = 0; ii < nclean; ii+=TILESIZE) {
-        dtranspose_tile_diag(A, ii, ii, lda);
+        dtranspose_tile_diag(A, ii, ii, lda_w);
       }
     }
 
@@ -218,9 +220,9 @@ void NPomp_d_itranspose_scale(const int n, const double alpha, double *A, int ld
 #pragma omp for schedule(static) nowait
     for(int j = 0; j < nclean; j++) {
       for(int i = nclean; i < n; i++) {
-        const double tmp = ELEM_AT(A, i, j, lda);
-        ELEM_AT(A, i, j, lda) = alpha * ELEM_AT(A, j, i, lda);
-        ELEM_AT(A, j, i, lda) = alpha * tmp;
+        const double tmp = A[i * lda_w + j];
+        A[i * lda_w + j] = alpha * A[j * lda_w + i];
+        A[j * lda_w + i] = alpha * tmp;
       }
     }
   } // end parallel region
@@ -244,26 +246,30 @@ void NPomp_d_itranspose_scale(const int n, const double alpha, double *A, int ld
 
   for(int j = nclean; j < n; j++) {
     for(int i = nclean; i < j; i++) {
-      const double tmp = ELEM_AT(A, i, j, lda);
-      ELEM_AT(A, i, j, lda) = alpha * ELEM_AT(A, j, i, lda);
-      ELEM_AT(A, j, i, lda) = alpha * tmp;
+      const double tmp = A[i * lda_w + j];
+      A[i * lda_w + j] = alpha * A[j * lda_w + i];
+      A[j * lda_w + i] = alpha * tmp;
     }
   }
 
   if(alpha != 1.0) {
     for(int i = nclean; i < n; i++) {
-      ELEM_AT(A, i, i, lda) *= alpha;
+      A[i * lda_w + i] *= alpha;
     }
   }
 
 }
 
-
+/*
+ * In-place parallel matrix transpose, double complex version.
+ * See https://colfaxresearch.com/multithreaded-transposition-of-square-matrices-with-common-code-for-intel-xeon-processors-and-intel-xeon-phi-coprocessors/
+ */
 void NPomp_z_itranspose_scale(const int n, const double complex *alphaptr, double complex *A, int lda)
 {
   const double complex alpha = *alphaptr;
   const int nclean = n - n % TILESIZE;
   const int ntiles = nclean / TILESIZE;
+  const size_t lda_w = (size_t) lda;
 
 #pragma omp parallel
   {
@@ -277,7 +283,7 @@ void NPomp_z_itranspose_scale(const int n, const double complex *alphaptr, doubl
  */
     int first_iteration = 1;
     int iouter, jouter;
-#pragma omp for nowait
+#pragma omp for schedule(static) nowait
     for(long long ijouter = 0; ijouter < (ntiles*(ntiles-1))/2; ijouter++) {
       if(first_iteration) {
         iouter = uncollapse_loop_index(ijouter);
@@ -290,27 +296,27 @@ void NPomp_z_itranspose_scale(const int n, const double complex *alphaptr, doubl
           jouter = 0;
         }
       }
-      ztranspose_scale_tile_offdiag(A, iouter * TILESIZE, jouter * TILESIZE, lda, alpha);
+      ztranspose_scale_tile_offdiag(A, iouter * TILESIZE, jouter * TILESIZE, lda_w, alpha);
     }
 
     if(alpha != 1.0) {
 #pragma omp for schedule(static) nowait
       for(int ii = 0; ii < nclean; ii+=TILESIZE) {
-        ztranspose_scale_tile_diag(A, ii, ii, lda, alpha);
+        ztranspose_scale_tile_diag(A, ii, ii, lda_w, alpha);
       }
     } else {
 #pragma omp for schedule(static) nowait
       for(int ii = 0; ii < nclean; ii+=TILESIZE) {
-        ztranspose_tile_diag(A, ii, ii, lda);
+        ztranspose_tile_diag(A, ii, ii, lda_w);
       }
     }
 
 #pragma omp for schedule(static) nowait
     for(int j = 0; j < nclean; j++) {
       for(int i = nclean; i < n; i++) {
-        const double complex tmp = ELEM_AT(A, i, j, lda);
-        ELEM_AT(A, i, j, lda) = alpha * ELEM_AT(A, j, i, lda);
-        ELEM_AT(A, j, i, lda) = alpha * tmp;
+        const double complex tmp = A[i * lda_w + j];
+        A[i * lda_w + j] = alpha * A[j * lda_w + i];
+        A[j * lda_w + i] = alpha * tmp;
       }
     }
 
@@ -318,15 +324,15 @@ void NPomp_z_itranspose_scale(const int n, const double complex *alphaptr, doubl
 
   for(int j = nclean; j < n; j++) {
     for(int i = nclean; i < j; i++) {
-        const double complex tmp = ELEM_AT(A, i, j, lda);
-        ELEM_AT(A, i, j, lda) = alpha * ELEM_AT(A, j, i, lda);
-        ELEM_AT(A, j, i, lda) = alpha * tmp;
+        const double complex tmp = A[i * lda_w + j];
+        A[i * lda_w + j] = alpha * A[j * lda_w + i];
+        A[j * lda_w + i] = alpha * tmp;
     }
   }
 
   if(alpha != 1.0) {
     for(int i = nclean; i < n; i++) {
-      ELEM_AT(A, i, i, lda) *= alpha;
+      A[i * lda_w + i] *= alpha;
     }
   }
 }
@@ -340,41 +346,15 @@ void NPomp_z_itranspose_scale(const int n, const double complex *alphaptr, doubl
 void NPomp_dtensor_itranspose_scale021(const long long matstride, int nmat, int n, const double alpha,
                                       double *A, int lda)
 {
-    if (omp_get_num_threads() >= nmat) {
-#pragma omp parallel
-{
-    int nlevels = omp_get_max_active_levels();
-    omp_set_max_active_levels(1);
-#pragma omp for schedule(static)
-    for (int imat = 0; imat < nmat; imat++) {
-      NPomp_d_itranspose_scale(n, alpha, A + imat * matstride, lda);
-    }
-    omp_set_max_active_levels(nlevels);
-}
-  } else {
-    for (int imat = 0; imat < nmat; imat++) {
-      NPomp_d_itranspose_scale(n, alpha, A + imat * matstride, lda);
-    }
+  for (int imat = 0; imat < nmat; imat++) {
+    NPomp_d_itranspose_scale(n, alpha, A + imat * matstride, lda);
   }
 }
 
 void NPomp_ztensor_itranspose_scale021(const long long matstride, int nmat, int n, const double complex *alpha,
                                       double complex *A, int lda)
 {
-    if (omp_get_num_threads() >= nmat) {
-#pragma omp parallel
-{
-    int nlevels = omp_get_max_active_levels();
-    omp_set_max_active_levels(1);
-#pragma omp for schedule(static)
     for (int imat = 0; imat < nmat; imat++) {
       NPomp_z_itranspose_scale(n, alpha, A + imat * matstride, lda);
     }
-    omp_set_max_active_levels(nlevels);
-}
-  } else {
-    for (int imat = 0; imat < nmat; imat++) {
-      NPomp_z_itranspose_scale(n, alpha, A + imat * matstride, lda);
-    }
-  }
 }
