8f85576c85a81e53c5f5d9a1e96c80b97511f48d
Susi Lehtola <susi.lehtola@gmail.com>
2024-12-03 23:16:34 +0200

    Update to libxc 7.0.0 (#2458)
    * Update to libxc 7.0.0
    
    * Update dependency in CI jobs
    
    ---------
    
    Co-authored-by: Qiming Sun <osirpt.sun@gmail.com>

--- .github/workflows/ci_linux/build_pyscf.sh
+++ .github/workflows/ci_linux/build_pyscf.sh
@@ -3,7 +3,7 @@
 set -e
 
 cd ./pyscf/lib
-curl -L "https://github.com/pyscf/pyscf-build-deps/blob/master/pyscf-2.4a-deps.tar.gz?raw=true" | tar xzf -
+curl -L "https://github.com/pyscf/pyscf-build-deps/blob/master/pyscf-2.8a-deps.tar.gz?raw=true" | tar xzf -
 mkdir build; cd build
 cmake -DBUILD_LIBXC=OFF -DBUILD_XCFUN=ON -DBUILD_LIBCINT=OFF -DXCFUN_MAX_ORDER=4 ..
 make -j4
