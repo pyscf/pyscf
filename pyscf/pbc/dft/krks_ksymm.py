8b41d9d71c45dae5cbfa6234a664275c528b9d36
Qiming Sun <osirpt.sun@gmail.com>
2025-06-03 06:50:06 -0700

    When building RSH K integrals, cderi is overwritten (fix #2790) (#2797)
    * When building RSH K integrals, cderi is overwritten (fix #2790)
    
    * lint
    
    * typo
    
    * Fix df tests
    
    * Fix tests

--- pyscf/pbc/dft/krks_ksymm.py
+++ pyscf/pbc/dft/krks_ksymm.py
@@ -92,12 +92,6 @@ def get_veff(ks, cell=None, dm=None, dm_last=0, vhf_last=0, hermi=1,
         vxc += vj
     else:
         omega, alpha, hyb = ni.rsh_and_hybrid_coeff(ks.xc, spin=cell.spin)
-        if getattr(ks.with_df, '_j_only', False):  # for GDF and MDF
-            logger.warn(ks, 'df.j_only cannot be used with hybrid functional')
-            ks.with_df._j_only = False
-            # Rebuild df object due to the change of parameter _j_only
-            if ks.with_df._cderi is not None:
-                ks.with_df.build()
         if omega == 0:
             vj, vk = ks.get_jk(cell, dm, hermi, kpts, kpts_band)
             vk *= hyb
