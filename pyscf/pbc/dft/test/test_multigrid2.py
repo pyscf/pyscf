fix multigrid dft gradient for no pseudo potential (#2883)
* fix multigrid gradient without pp

* fix typo

* lint

* Fix exp overflow

* update get_nuc_nuc_grad

* Handle exp overflow in more functions

---------

Co-authored-by: Qiming Sun <osirpt.sun@gmail.com>

pyscf/pbc/dft/test/test_multigrid2.py
@@ -20,12 +20,13 @@ import unittest
 import numpy as np
 from pyscf.pbc import gto, dft, df
 from pyscf.pbc.dft import multigrid
+from pyscf.pbc.dft.multigrid.pp import vpploc_part1_nuc_grad
 from pyscf.pbc.grad import rks as rks_grad
 from pyscf.pbc.grad import uks as uks_grad
 from pyscf.pbc.grad import krks as krks_grad
 
 def setUpModule():
-    global He_orth, He_nonorth, cell_orth, cell_nonorth, dm, dm1
+    global He_orth, He_nonorth, cell_orth, cell_nonorth, dm, dm1, mol
 
     He_basis = [[0, ( 1, 1, .1), (.5, .1, 1)],
                 [1, (.8, 1)]]
@@ -72,9 +73,19 @@ def setUpModule():
     dm = (dm + dm.T) * .5
     dm1 = np.asarray([dm,dm])*.5
 
+    mol = gto.M(
+        atom = 'H 0 0 0; H 1 1 1.01',
+        a = np.eye(3) * 2,
+        basis = [[0, [1.3, 1]],
+                 [1, [0.8, 1]]
+                ],
+        unit = 'bohr',
+        ke_cutoff = 20,
+    )
+
 def tearDownModule():
-    global He_orth, He_nonorth, cell_orth, cell_nonorth, dm, dm1
-    del He_orth, He_nonorth, cell_orth, cell_nonorth, dm, dm1
+    global He_orth, He_nonorth, cell_orth, cell_nonorth, dm, dm1, mol
+    del He_orth, He_nonorth, cell_orth, cell_nonorth, dm, dm1, mol
 
 def _fftdf_energy_grad(cell, xc):
     mf = dft.KRKS(cell, kpts=np.zeros((1,3)))
@@ -230,6 +241,34 @@ class KnownValues(unittest.TestCase):
         assert abs(e1-e0) < 1e-7
         assert abs(g-g0).max() < 1e-7
         assert abs(g1-g0).max() < 1e-7
+
+    # https://github.com/pyscf/pyscf/issues/2882
+    def test_nopp_vpploc_part1_nuc_grad(self):
+        mf = dft.RKS(mol, xc='lda,vwn')
+        mf.with_df = multigrid.MultiGridFFTDF2(mol)
+        mf.run()
+        g = vpploc_part1_nuc_grad(mf.with_df, mf.make_rdm1())
+        assert abs(g).max() < 1e-8
+
+    def test_mol_rks(self):
+        mf = dft.RKS(mol, xc='lda,vwn')
+        mf.with_df = multigrid.MultiGridFFTDF2(mol)
+        mf.run()
+        assert abs(mf.e_tot - -2.937753) < 1e-6
+
+        g = rks_grad.Gradients(mf).kernel()
+        g0 = np.array([[0, 0, -0.003145], [0, 0, 0.003145]])
+        assert abs(g - g0).max() < 1e-6
+
+    def test_mol_uks(self):
+        mf = dft.UKS(mol, xc='lda,vwn')
+        mf.with_df = multigrid.MultiGridFFTDF2(mol)
+        mf.run()
+        assert abs(mf.e_tot - -2.937753) < 1e-6
+
+        g = uks_grad.Gradients(mf).kernel()
+        g0 = np.array([[0, 0, -0.003145], [0, 0, 0.003145]])
+        assert abs(g - g0).max() < 1e-6
 
 if __name__ == '__main__':
     print("Full Tests for multigrid2")