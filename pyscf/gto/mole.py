74379edfcf8745e6d92e656745f3880b4eb4b935
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2025-08-12 09:59:04 -0500

    Small fix for mol sanity check

--- pyscf/gto/mole.py
+++ pyscf/gto/mole.py
@@ -2640,13 +2640,21 @@ class MoleBase(lib.StreamObject):
                 logger.warn(self, f'ECP not specified. The basis set {self.basis} '
                             f'include an ECP. Recommended ECP: {ecp}.')
         elif isinstance(self.basis, dict) and isinstance(self.ecp, dict):
-            for element, basname in self.basis.items():
+            _basis = self.basis
+            if any(key.upper() == 'DEFAULT' for key in _basis.keys()):
+                basis = {atm: _basis[next(k for k in _basis.keys() if k.upper() == 'DEFAULT')]
+                        for atm in [self.atom_symbol(atm_id)
+                            for atm_id in range(self.natm)]}
+            else:
+                basis = _basis
+            for element, basname in basis.items():
                 if isinstance(basname, str) and not self.ecp.get(element):
                     ecp, ecp_atoms = bse_predefined_ecp(basname, element)
                     if ecp_atoms:
                         logger.warn(self, f'ECP for {element} not specified. '
                                     f'The basis set {basname} include an ECP. '
                                     f'Recommended ECP: {ecp}.')
+            basis = None
         return self
 
     def _build_symmetry(self, *args, **kwargs):
