59beb0f068aa65378e5232d52192c04039073026
Qiming Sun <osirpt.sun@gmail.com>
2025-11-05 12:21:19 -0800

    Add an interface to gpu4pyscf

--- pyscf/lib/misc.py
+++ pyscf/lib/misc.py
@@ -1538,11 +1538,18 @@ def to_gpu(method, out=None):
 
         from importlib import import_module
         mod = import_module(method.__module__.replace('pyscf', 'gpu4pyscf'))
-        cls = getattr(mod, method.__class__.__name__)
+        try:
+            cls = getattr(mod, method.__class__.__name__)
+        except AttributeError:
+            if hasattr(cls, 'from_cpu'):
+                # the customized to_gpu function can be accessed at module
+                # levelin gpu4pyscf.
+                return cls.from_cpu(method)
+            raise
 
         # Allow gpu4pyscf to customize the to_gpu method for PySCF classes.
-        if hasattr(cls, 'from_cpu'):
-            return cls.from_cpu(method)
+        if hasattr(mod, 'from_cpu'):
+            return mod.from_cpu(method)
 
         # A temporary GPU instance. This ensures to initialize private
         # attributes that are only available for GPU code.
