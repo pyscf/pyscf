5cccd01c230063eb103553ecbfc0c5ec10093217
hongzhouye <>
2026-02-15 09:33:46 -0500

    Add pbc lo and kpipek

---
+++ pyscf/pbc/lo/base.py
@@ -0,0 +1,250 @@
+#!/usr/bin/env python
+# Copyright 2014-2019 The PySCF Developers. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Hong-Zhou Ye <hzyechem@gmail.com>
+#         Gengzhi Yang <genzyang17@gmail.com>
+#
+
+'''
+Base classes for k-point orbital rotations
+
+ref. [To be updated]
+'''
+
+import numpy
+
+from pyscf import lib
+from pyscf.lib import logger
+from pyscf.pbc.soscf import kciah
+from pyscf.lo import cholesky_mos
+from pyscf.lo import boys
+from pyscf.pbc.tools import k2gamma
+from pyscf.pbc.lib.kpts import KPoints
+from pyscf.pbc.lib.kpts_helper import gamma_point
+from pyscf.tools import mo_mapping
+from pyscf import __config__
+
+
+def _align_phase(mo, mo0):
+    ovlp = lib.dot(mo.conj().T, mo0)
+    l, _, r = numpy.linalg.svd(ovlp)
+    return lib.dot(l, r)
+
+def wannierization(cell, kpts, mo_coeff, kmesh=None, refcell_only=False):
+    mo_coeff = numpy.asarray(mo_coeff, order='C')
+    if kmesh is None: kmesh = get_kmesh(cell, kpts)
+    scell, phase = k2gamma.get_phase(cell, kpts, kmesh=kmesh)
+    nkpts,nao,nmo = mo_coeff.shape
+    Nao = nkpts*nao
+
+    if refcell_only:
+        W = lib.einsum('Rk,kmi,k->Rmi', phase, mo_coeff, phase[0].conj()).reshape(Nao,nmo)
+    else:
+        W = lib.einsum('Rk,kmi,Sk->RmSi', phase, mo_coeff, phase.conj()).reshape(Nao,nkpts*nmo)
+
+    return W
+
+
+class KptsOrbitalLocalizer(lib.StreamObject, kciah.SubspaceCIAHOptimizerMixin):
+
+    conv_tol = getattr(__config__, 'pbc_lo_base_conv_tol', 1e-6)
+    conv_tol_grad = getattr(__config__, 'pbc_lo_base_conv_tol_grad', None)
+    max_cycle = getattr(__config__, 'pbc_lo_base_max_cycle', 100)
+    max_iters = getattr(__config__, 'pbc_lo_base_max_iters', 20)
+    max_stepsize = getattr(__config__, 'pbc_lo_base_max_stepsize', .05)
+    ah_trust_region = getattr(__config__, 'pbc_lo_base_ah_trust_region', 3)
+    ah_start_tol = getattr(__config__, 'pbc_lo_base_ah_start_tol', 1e9)
+    ah_max_cycle = getattr(__config__, 'pbc_lo_base_ah_max_cycle', 40)
+    init_guess = getattr(__config__, 'pbc_lo_base_init_guess', 'atomic')
+    algorithm = getattr(__config__, 'pbc_lo_base_init_guess', 'ciah')
+    maximize = getattr(__config__, 'pbc_lo_base_init_guess', False)
+
+    _keys = {
+        'conv_tol', 'conv_tol_grad', 'max_cycle', 'max_iters',
+        'max_stepsize', 'ah_trust_region', 'ah_start_tol',
+        'ah_max_cycle', 'init_guess', 'algorithm', 'cell', 'mo_coeff', 'kpts'
+    }
+
+    def __init__(self, cell, mo_coeff, kpts):
+        if isinstance(kpts, KPoints):
+            if not kpts.time_reversal:
+                raise NotImplementedError('k-point symmetry not implemented')
+            mo_coeff = remove_trs_mo(mo_coeff, kpts)
+            kpts = kpts.kpts
+            logger.warn(cell, 'Time-reversal symmetry will be ignored')
+
+        self.kpts = kpts
+        rtypes = [1] + [2] * (len(kpts)-1)  # One gauge-fixed complex rotation
+                                            # + (Nk-1) general complex rotations
+        kciah.SubspaceCIAHOptimizerMixin.__init__(self, mo_coeff[0].shape[1], rtypes)
+
+        self.cell = cell
+        self.stdout = cell.stdout
+        self.verbose = cell.verbose
+        self.mo_coeff = numpy.asarray(mo_coeff)
+
+    def rotate_orb(self, u=None):
+        if u is None:
+            return self.mo_coeff
+        else:
+            return numpy.asarray([lib.dot(xk, uk) for xk,uk in zip(self.mo_coeff, u)])
+
+    def dump_flags(self, verbose=None):
+        boys.OrbitalLocalizer.dump_flags(self, verbose)
+
+    def get_init_guess(self, key='atomic'):
+        ''' Generate initial guess for localization.
+
+            The initial guess is first generated for the first k-point. Other k-points
+
+        Kwargs:
+            key : str or bool
+                If key is 'atomic', initial guess is based on the projected
+                atomic orbitals. False
+        '''
+        cell = self.cell
+        kpts = self.kpts
+        nkpts = len(kpts)
+        mo_coeff = self.mo_coeff
+        nmo = mo_coeff[0].shape[1]
+
+        mo0 = mo_coeff[0]
+        if isinstance(key, str) and key.lower().startswith('atom'):
+            u00 = boys.atomic_init_guess(cell, mo0, kpt=kpts[0])
+        elif isinstance(key, str) and key.lower().startswith('cho'):
+            mo_init = cholesky_mos(mo0)
+            S = cell.pbc_intor('int1e_ovlp', kpt=kpts[0])
+            u00 = numpy.linalg.multi_dot([mo0.T, S, mo_init])
+        else:
+            return self.identity_rotation()
+
+        # diabatization: align phase of MO[k] to MO[0]
+        mo0 = lib.dot(mo_coeff[0], u00)
+
+        return self.diabatization(mo0)
+
+    def diabatization(self, mo0):
+        mo_coeff = self.mo_coeff
+        return numpy.asarray([_align_phase(mok, mo0) for mok in mo_coeff])
+
+    def get_wannier_function(self, u=None, refcell_only=False):
+        mo_coeff = self.rotate_orb(u)
+
+        return wannierization(self.cell, self.kpts, mo_coeff, refcell_only=refcell_only)
+
+    def sort_orb(self, u):
+        u = numpy.asarray(u)
+        u00 = u.sum(axis=0) / len(self.kpts)
+        sorted_idx = mo_mapping.mo_1to1map(u00)
+        return self.rotate_orb([uk[:,sorted_idx] for uk in u])
+
+    kernel = boys.kernel
+
+
+def get_kmesh(cell, kpts, tol=1e-6, nmax=1000):
+    scaled_kpts = cell.get_scaled_kpts(kpts-kpts[0])
+    nks = numpy.arange(1,nmax+1)
+    ks = lib.einsum('kx,n->xnk', scaled_kpts, nks)
+    mask = numpy.all(abs(ks - numpy.round(ks)) < tol, axis=-1)
+    if not numpy.all(numpy.any(mask, axis=-1)):
+        raise RuntimeError('Input kmesh is either too large or not a (shifted) regular mesh.')
+
+    kmesh = [numpy.where(mask[i])[0][0]+1 for i in range(3)]
+
+    return kmesh
+
+def remove_trs_mo(mo_coeff_trs, kpts):
+    assert( len(mo_coeff_trs) == kpts.nkpts_ibz )
+
+    mo_coeff_trs = numpy.asarray(mo_coeff_trs, order='C')
+    kpairs = [numpy.where(kpts.bz2ibz==q)[0] for q in range(kpts.nkpts_ibz)]
+    mo_coeff = numpy.empty((kpts.nkpts,*mo_coeff_trs[0].shape), dtype=mo_coeff_trs.dtype)
+
+    for q,kpair in enumerate(kpairs):
+        if len(kpair) == 1:
+            k = kpair[0]
+            mo_coeff[k] = mo_coeff_trs[q]
+        else:
+            k1, k2 = kpair
+            mo_coeff[k1] = mo_coeff_trs[q].conj()
+            mo_coeff[k2] = mo_coeff_trs[q]
+
+    return mo_coeff
+
+def _pack_bz2ibz(g, kpts_symm, sign=1):
+    G = numpy.zeros((kpts_symm.nkpts_ibz, *g[0].shape), dtype=numpy.complex128)
+    for q in range(kpts_symm.nkpts_ibz):
+        idx = numpy.where(kpts_symm.bz2ibz==q)[0]
+        if idx.size == 1:
+            G[q] = g[idx[0]]
+        else:
+            G[q] = g[idx[0]] + sign * g[idx[1]].conj()
+    return G
+
+def _unpack_ibz2bz(G, kpts_symm, sign=1):
+    g = numpy.zeros((kpts_symm.nkpts, *G[0].shape), dtype=numpy.complex128)
+    for q in range(kpts_symm.nkpts_ibz):
+        idx = numpy.where(kpts_symm.bz2ibz==q)[0]
+        g[idx[0]] = G[q]
+        if idx.size == 2:
+            g[idx[1]] = sign * G[q].conj()
+    return g
+
+
+class KptsOrbitalLocalizerReal(KptsOrbitalLocalizer):
+
+    def __init__(self, cell, mo_coeff, kpts):
+        if isinstance(kpts, KPoints):
+            if not kpts.time_reversal:
+                raise NotImplementedError('k-point symmetry not implemented')
+            mo_coeff = remove_trs_mo(mo_coeff, kpts)
+            kpts_symm = kpts
+        else:
+            if not gamma_point(kpts[0]):
+                raise ValueError('Input k-mesh is not Gamma-centered.')
+            kmesh = get_kmesh(cell, kpts)
+            kpts_symm = cell.make_kpts(kmesh, time_reversal_symmetry=True)
+
+        self.kpts = kpts_symm.kpts
+        self.kpts_symm = kpts_symm
+
+        # Time-reversal invariant k-points => real rotation (type 0)
+        # Time-reversal paired    k-points => complex rotation (type 2)
+        rtypes = numpy.zeros(kpts_symm.nkpts_ibz, dtype=int)
+        for q in range(kpts_symm.nkpts_ibz):
+            idx = numpy.where(kpts_symm.bz2ibz==q)[0]
+            if idx.size == 2:
+                rtypes[q] = 2
+
+        kciah.SubspaceCIAHOptimizerMixin.__init__(self, mo_coeff[0].shape[1], rtypes)
+
+        self.cell = cell
+        self.stdout = cell.stdout
+        self.verbose = cell.verbose
+        self.mo_coeff = numpy.asarray(mo_coeff)
+
+    def rotate_orb(self, u=None):
+        if u is None:
+            return self.mo_coeff
+        else:
+            u = _unpack_ibz2bz(u, self.kpts_symm)
+            return numpy.asarray([lib.dot(xk, uk) for xk,uk in zip(self.mo_coeff, u)])
+
+    def diabatization(self, mo0):
+        u0 = []
+        for q in range(self.kpts_symm.nkpts_ibz):
+            idx = numpy.where(self.kpts_symm.bz2ibz==q)[0]
+            u0.append( _align_phase(self.mo_coeff[idx[0]], mo0) )
+        return numpy.asarray(u0)
---
+++ pyscf/pbc/lo/stability.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+# Copyright 2014-2019 The PySCF Developers. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Hong-Zhou Ye <hzyechem@gmail.com>
+#         Gengzhi Yang <genzyang17@gmail.com>
+#
+
+
+import numpy
+
+from pyscf import lib
+from pyscf.lib import logger
+from pyscf import __config__
+
+from pyscf.lo.stability import stability_newton
+from pyscf.pbc.lo.base import get_kmesh
+from pyscf.pbc.tools import k2gamma
+
+
+def pipek_stability_jacobi(mlo, verbose=None, return_status=False, Rmax=10):
+    '''Jacobi-sweep stability check for k-point PMWFs.
+
+    This routine tests whether any translationally symmetric 2×2 Jacobi
+    rotations can increase the PM objective. Specifically, it considers
+    real-valued pairwise rotations between Wannier functions
+    :math:`(w_{\mathbf{0}i}, w_{\mathbf{R}j})`, where the lattice vector
+    :math:`\mathbf{R}` is restricted to a finite real-space range.
+
+    For each orbital pair ``(i,j)`` and each allowed ``R``, the function
+    searches over a discrete set of candidate rotation angles
+    (``π/4, π/2, 3π/4``) and applies any rotation that yields an
+    increase in the objective greater than ``mlo.conv_tol``.
+
+    Kwargs:
+        Rmax : float or None
+            Real-space cutoff that controls the range of pairwise rotations
+            tested in the Jacobi sweep. Only lattice vectors satisfying
+            :math:`|\mathbf{R}| < R_{\mathrm{max}}` are included.
+            Unit: Bohr. Default is 10. If set to ``None``, all R vectors
+            in the BvK will be used.
+    '''
+    log = logger.new_logger(mlo, verbose)
+    exponent = mlo.exponent
+
+    cell = mlo.cell
+    kpts = mlo.kpts
+    norb = mlo.norb
+    Nk = len(kpts)
+
+    tril_ijdx = numpy.tril_indices(norb, k=-1)
+    tril_idx, tril_jdx = tril_ijdx
+    thetapool = numpy.asarray([1,2,3])*0.25*numpy.pi
+
+    kmesh = get_kmesh(cell, kpts)
+    Rs_bvk = k2gamma.translation_vectors_for_kmesh(cell, kmesh)
+    if Rmax is None:
+        Rs = Rs_bvk
+    else:
+        Rs = Rs_bvk[numpy.linalg.norm(Rs_bvk, axis=1) < Rmax]
+    log.debug('Checking pairwise stability for WF pairs (0i, Rj) for Rvec:\n%s', Rs)
+    phase = numpy.exp(1j * numpy.dot(Rs, kpts.T))
+    phase_bvk = numpy.exp(1j * numpy.dot(Rs_bvk, kpts.T))
+
+    latvec_bvk = lib.einsum('ix,i->ix', cell.lattice_vectors(), kmesh)
+    def get_shift_map(Rs, R0, logacc=8):
+        ''' Mapping Rs-R0 back to Rs using BvK lattice vectors
+
+            Returns:
+                shift_map : array
+                    Rs[i] - R0 = Rs[shift_map[i]]
+        '''
+        Ts = numpy.linalg.solve(latvec_bvk.T, Rs.T).T
+        T0 = numpy.linalg.solve(latvec_bvk.T, R0)
+        Ts1 = Ts - T0
+        # shift Ts back to [0,1)^3
+        Ts = numpy.round(Ts, logacc+1)%1
+        Ts1 = numpy.round(Ts1, logacc+1)%1
+        shift_map = numpy.where(abs(Ts1[:,None,:]-Ts[None,:,:]).max(axis=2)<10**-logacc)[1]
+        return shift_map
+
+    shift_map = [get_shift_map(Rs_bvk, R) for R in Rs]  # NR x Nk
+
+    def update_rotation_local_(u, theta, i, j, expikR):
+        for k,x in enumerate(u):
+            xi = x[:,i].copy()
+            xj = x[:,j].copy()
+            x[:,i] = xi*numpy.cos(theta) + xj*numpy.sin(theta)*expikR[k]
+            x[:,j] = -xi*numpy.sin(theta)*expikR[k].conj() + xj*numpy.cos(theta)
+
+    u = mlo.identity_rotation()
+    stable = True
+    while True:
+        mo_coeff = mlo.rotate_orb(u)
+        pop0 = mlo.atomic_pops(cell, mo_coeff, mode='00').real
+        pop0 = numpy.ascontiguousarray(lib.einsum('xii->ix', pop0))
+        proj0k = mlo.atomic_pops(cell, mo_coeff, mode='0k')
+        proj0R = numpy.ascontiguousarray(lib.einsum('xikj,Rk->Rijx', proj0k, phase_bvk.conj()).real)
+        proj0k = None
+
+        pop0exp = pop0**exponent
+        Lij = (pop0exp[tril_idx] + pop0exp[tril_jdx]).sum(axis=1)
+
+        dLij = numpy.zeros_like(Lij)
+        thetaij = numpy.zeros_like(Lij)
+        Ridxij = numpy.zeros(Lij.shape, dtype=int)
+
+        for iR,R in enumerate(Rs):
+            # Q_{0,TA} -> Q_{R,TA}
+            popR = pop0.reshape(norb,Nk,-1)[:,shift_map[iR]].reshape(norb,-1)
+
+            Qi_i = pop0[tril_idx]
+            Qi_j = popR[tril_jdx]
+            Pij_ij = proj0R[iR, tril_idx, tril_jdx]
+
+            for theta in thetapool:
+                c = numpy.cos(theta)
+                s = numpy.sin(theta)
+                c2 = c**2
+                s2 = s**2
+                cs2 = c * s * 2
+
+                # Population after rotations
+                Qitild = Qi_i * c2 + Qi_j * s2 + cs2 * Pij_ij
+                Qjtild = Qi_i * s2 + Qi_j * c2 - cs2 * Pij_ij
+
+                # Population change
+                dL = (Qitild**exponent + Qjtild**exponent).sum(axis=1) - Lij
+                Qitild = Qjtild = None
+
+                # Find theta that increases the PM objective
+                mask = dL > (dLij + mlo.conv_tol)
+                if numpy.any(mask):
+                    dLij[mask] = dL[mask]
+                    thetaij[mask] = theta
+                    Ridxij[mask] = iR
+
+            Qi_i = Qi_j = Pij_ij = None
+
+        idxs = numpy.where(dLij > mlo.conv_tol)[0]
+        if idxs.size == 0:
+            break
+
+        # Sort idxs in decreasing order
+        idxs = idxs[numpy.argsort(dLij[idxs])[::-1]]
+
+        # Remove overlapping pairs using a greedy algorithm
+        stable = False
+        done = numpy.zeros(mlo.norb, dtype=bool)
+
+        for idx in idxs:
+            i, j = tril_idx[idx], tril_jdx[idx]
+            if done[i] or done[j]:
+                continue
+            done[i] = done[j] = True
+
+            theta = thetaij[idx]
+            iR = Ridxij[idx]
+            R = Rs[iR]
+            log.info('Rotating orbital pair (%d,%d) by %.2f Pi for Rvec %s. delta_f= %.14g',
+                     i, j, theta/numpy.pi, R, dLij[idx])
+
+            update_rotation_local_(u, theta, i, j, phase[iR])
+
+    if stable:
+        log.info(f'{mlo.__class__.__name__} is stable in the Jacobi stability analysis')
+        mo_coeff = mlo.mo_coeff
+    else:
+        mo_coeff = mlo.rotate_orb(u)
+
+    if return_status:
+        return mo_coeff, stable
+    else:
+        return mo_coeff
