pyscf/pyscf

commit b94a7cd5d1d85808aaafe99e7951a41cb10a0c27

typo

pyscf/pbc/gto/cell.py
@@ -1185,7 +1185,7 @@ def _parse_cif(string):
         if line.startswith('_cell_'):
             # Cell parameters
             key, val = line.split(maxsplit=1)
-            cell[key] = float(val)
+            cell[key] = val
 
         elif line.startswith('loop_'):
             in_atom_loop = True

commit 3b4e915d9ea6e4a178ab3bfea98cbc25e53e837a

Fix POSCAR parser bug introduced by #3128

pyscf/pbc/gto/cell.py
@@ -1166,7 +1166,8 @@ def _parse_poscar(string):
         count = unique_atoms[atom_type]
         start, end = end, end + count
         elements.extend([atom_type] * count)
-        coords.append(np.fromstring(' '.join(lines[start:end]), sep=' '))
+        r = np.fromstring(' '.join(lines[start:end]), sep=' ')
+        coords.append(r.reshape(count, 3))
     coords = np.vstack(coords)
     assert len(coords) == len(elements)
     return lattice_vectors, elements, coords, fractional

pyscf/pbc/gto/test/test_cell.py
@@ -609,6 +609,47 @@ Direct
         assert abs(a - ref_a).max() < 1e-14
         assert abs(coords - ref_pos).max() < 1e-14
 
+        inp_str = '''\
+SrTiO3 cubic perovskite
+3.9
+1.0 0.0 0.0
+0.0 1.0 0.0
+0.0 0.0 1.0
+Sr Ti O
+1 1 3
+Cartesian
+0.00 0.00 0.00
+1.95 1.95 1.95
+1.95 1.95 0.00
+1.95 0.00 1.95
+0.00 1.95 1.95
+'''
+        a, atoms = pgto.cell.fromstring(inp_str, format='poscar')
+        ref_pos = np.array([
+            [0.00, 0.00, 0.00],
+            [1.95, 1.95, 1.95],
+            [1.95, 1.95, 0.00],
+            [1.95, 0.00, 1.95],
+            [0.00, 1.95, 1.95]])
+        coords = np.array([x[1] for x in atoms])
+        assert abs(a[0,0] - 3.9) < 1e-14
+        assert abs(coords - ref_pos).max() < 1e-14
+        cell = pgto.M(atom=atoms, a=a, basis=[[0, [1,1]]])
+
+        assert cell.tostring('poscar').split('\n', 1)[1] == '''\
+1.0
+       3.90000000        0.00000000        0.00000000
+       0.00000000        3.90000000        0.00000000
+       0.00000000        0.00000000        3.90000000
+Sr Ti O
+1 1 3
+Cartesian
+       0.00000000        0.00000000        0.00000000
+       1.95000000        1.95000000        1.95000000
+       1.95000000        1.95000000        0.00000000
+       1.95000000        0.00000000        1.95000000
+       0.00000000        1.95000000        1.95000000'''
+
     def test_parse_cif(self):
         inp_str = '''\
 # generated using pymatgen