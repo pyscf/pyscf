4d4d46fed6eebe0b5a18a2a7f2dff396d7a415fb
Shirong_Wang <srwang20@fudan.edu.cn>
2026-06-21 16:26:49 +0800

    use newer pyberny in ci

--- .github/workflows/ci_linux/python_deps.sh
+++ .github/workflows/ci_linux/python_deps.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 python -m pip install --upgrade pip
 pip install "numpy!=1.16,!=1.17" "scipy!=1.5" h5py pytest pytest-cov pytest-timer
-pip install pyberny
+pip install git+https://github.com/jhrmnn/pyberny.git@36a4be9
 pip install --no-deps pyscf-dispersion==1.3.0
+pip install geometric
 
 version=$(python -c 'import sys; version=sys.version_info[:2]; print("{0}.{1}".format(*version))')
 if [ $version != '3.12' ]; then
-    pip install geometric
     pip install spglib
 fi
 
