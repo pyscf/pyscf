181097ed9d7b3fb106a9efdf99fd1da9b37af7df
henry.wang1 <henry.wang1@bytedance.com>
2026-07-08 15:56:34 +0800

    Fix the bad atomic radii assigned to ghost atom, which causes a bad Becke partition and a negative BSSE

--- pyscf/dft/gen_grid.py
+++ pyscf/dft/gen_grid.py
@@ -34,6 +34,8 @@ from pyscf.dft import radi
 from pyscf.dft.LebedevGrid import LEBEDEV_ORDER, LEBEDEV_NGRID, MakeAngularGrid
 from pyscf import gto
 from pyscf.gto.eval_gto import BLKSIZE, NBINS, CUTOFF, make_screen_index
+from pyscf.data.elements import _std_symbol_without_ghost
+from pyscf.data.elements import charge as elements_proton
 from pyscf import __config__
 
 libdft = lib.load_library('libdft')
@@ -292,7 +294,7 @@ def gen_atomic_grids(mol, atom_grid={}, radi_method=radi.gauss_chebyshev,
         symb = mol.atom_symbol(ia)
 
         if symb not in atom_grids_tab:
-            chg = gto.charge(symb)
+            chg = elements_proton(_std_symbol_without_ghost(symb))
             atom_config = atom_grid.get(symb, default)
             if atom_config is not None:
                 n_rad, n_ang = atom_config
