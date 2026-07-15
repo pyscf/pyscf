Add Gradients of GTH PP for molecules (fix #3198) (#3278)
* Add Gradients of GTH PP for molecules (fix #3198)

* Update tests

pyscf/gto/pp_int.py
@@ -36,7 +36,7 @@ def get_gth_pp(mol):
         charges = fakemol.atom_charges()
         atmlst = fakemol._bas[:,ATOM_OF]
         v = incore.aux_e2(mol, fakemol, 'int3c2e', aosym='s2', comp=1)
-        vpploc = np.einsum('...i,i->...', v, -charges[atmlst])
+        vpploc = np.einsum('pi,i->p', v, -charges[atmlst])
 
     intors = ('int3c2e', 'int3c1e', 'int3c1e_r2_origk',
               'int3c1e_r4_origk', 'int3c1e_r6_origk')
@@ -50,17 +50,8 @@ def get_gth_pp(mol):
         vpploc = lib.unpack_tril(vpploc)
 
     fakemol, hl_blocks = pp_int.fake_cell_vnl(mol)
-    hl_dims = np.array([len(hl) for hl in hl_blocks])
-    _bas = fakemol._bas
-    ppnl_half = []
     intors = ('int1e_ovlp', 'int1e_r2_origi', 'int1e_r4_origi')
-    for i, intor in enumerate(intors):
-        fakemol._bas = _bas[hl_dims>i]
-        if fakemol.nbas > 0:
-            ppnl_half.append(intor_cross(intor, fakemol, mol))
-        else:
-            ppnl_half.append(None)
-    fakemol._bas = _bas
+    ppnl_half = _int_vnl(mol, fakemol, hl_blocks, intors)
 
     nao = mol.nao
     offset = [0] * 3
@@ -78,3 +69,101 @@ def get_gth_pp(mol):
             offset[i] = p0 + nd
         vpploc += np.einsum('ilp,ij,jlq->pq', ilp.conj(), hl, ilp)
     return vpploc
+
+def _int_vnl(mol, fakemol, hl_blocks, intors, comp=None):
+    hl_dims = np.array([len(hl) for hl in hl_blocks])
+    _bas = fakemol._bas
+    ppnl_half = []
+    for i, intor in enumerate(intors):
+        fakemol._bas = _bas[hl_dims>i]
+        if fakemol.nbas > 0:
+            ppnl_half.append(intor_cross(intor, fakemol, mol, comp=comp))
+        else:
+            ppnl_half.append(None)
+    fakemol._bas = _bas
+    return ppnl_half
+
+def vpploc_nuc_grad(mol, dm):
+    '''
+    Nuclear gradients of the local part (part2) of the GTH pseudo potential,
+    contracted with the density matrix.
+    '''
+    from pyscf.pbc.gto import Cell
+    from pyscf.pbc.gto.pseudo import pp_int
+    from pyscf.pbc.gto import build_neighbor_list_for_shlpairs, free_neighbor_list
+    from pyscf.pbc.gto.cell import rcut_by_shells
+    from pyscf.pbc.df.incore import int3c1e_nuc_grad
+
+    # The gradients of get_pp_loc_part1(mol) are computed in grad.rhf.get_hcore
+    # and grad.rhf.hcore_generator
+
+    intors = ('int3c2e_ip1', 'int3c1e_ip1', 'int3c1e_ip1_r2_origk',
+              'int3c1e_ip1_r4_origk', 'int3c1e_ip1_r6_origk')
+
+    Ls = np.zeros((1, 3))
+    as_cell = mol.view(Cell)
+    as_cell.get_lattice_Ls = lambda *args: Ls
+    as_cell.precision = 1e-12
+
+    grad = np.zeros((mol.natm, 3))
+    rcut = as_cell.rcut_by_shells()
+    for cn in range(1, 5):
+        fakecell = pp_int.fake_cell_vloc(mol, cn)
+        fakecell.precision = 1e-8
+        if fakecell.nbas > 0:
+            neighbor_list = build_neighbor_list_for_shlpairs(
+                as_cell, None, Ls, ish_rcut=rcut, jsh_rcut=rcut)
+            grad += int3c1e_nuc_grad(
+                as_cell, fakecell, dm, intors[cn], neighbor_list=neighbor_list)
+            free_neighbor_list(neighbor_list)
+    grad *= -2
+    return grad
+
+def vppnl_nuc_grad(mol, dm):
+    '''
+    Nuclear gradients of the non-local part of the GTH pseudo potential,
+    contracted with the density matrix.
+    '''
+    from pyscf.pbc.gto.pseudo import pp_int
+    assert dm.ndim == 2
+    fakemol, hl_blocks = pp_int.fake_cell_vnl(mol)
+    ppnl_half = _int_vnl(mol, fakemol, hl_blocks,
+                         ('int1e_ovlp', 'int1e_r2_origi', 'int1e_r4_origi'))
+    ppnl_half_ip2 = _int_vnl(
+        mol, fakemol, hl_blocks,
+        ('int1e_ipovlp', 'int1e_r2_origi_ip2', 'int1e_r4_origi_ip2'), comp=3)
+
+    # int1e_ipovlp computes ip1 so multiply -1 to get ip2
+    if len(ppnl_half_ip2[0]) > 0:
+        ppnl_half_ip2[0] *= -1
+
+    grad = np.zeros((mol.natm, 3))
+    nao = dm.shape[-1]
+    dppnl = np.zeros((3, nao, nao))
+    offset = [0] * 3
+    for ib, hl in enumerate(hl_blocks):
+        l = fakemol.bas_angular(ib)
+        nd = 2 * l + 1
+        hl_dim = hl.shape[0]
+
+        ilp = np.empty((hl_dim, nd, nao))
+        dilp = np.empty((hl_dim, 3, nd, nao))
+        for i in range(hl_dim):
+            p0 = offset[i]
+            if len(ppnl_half[i]) > 0:
+                ilp[i] = ppnl_half[i][p0:p0+nd]
+                dilp[i] = ppnl_half_ip2[i][:,p0:p0+nd]
+            offset[i] = p0 + nd
+
+        dppnl_i = lib.einsum('idlp,ij,jlq->dpq', dilp, hl, ilp)
+        dppnl += dppnl_i
+
+        i_pp_atom = fakemol._bas[ib, 0]
+        grad[i_pp_atom] += np.einsum('dpq,qp->d', dppnl_i, dm)
+
+    aoslices = mol.aoslice_by_atom()
+    for ia in range(mol.natm):
+        p0, p1 = aoslices[ia,2:]
+        grad[ia] -= np.einsum('dpq,qp->d', dppnl[:,p0:p1,:], dm[:,p0:p1])
+    grad *= 2
+    return grad

pyscf/pbc/grad/uhf.py
@@ -29,7 +29,7 @@ from pyscf.pbc.dft.multigrid import MultiGridNumInt2
 
 def grad_elec(mf_grad, mo_energy=None, mo_coeff=None, mo_occ=None, atmlst=None, kpt=np.zeros(3)):
     mf = mf_grad.base
-    mol = mf_grad.mol
+    cell = mf_grad.cell
     if mo_energy is None: mo_energy = mf.mo_energy
     if mo_occ is None:    mo_occ = mf.mo_occ
     if mo_coeff is None:  mo_coeff = mf.mo_coeff
@@ -42,12 +42,15 @@ def grad_elec(mf_grad, mo_energy=None, mo_coeff=None, mo_occ=None, atmlst=None, 
         ni = mf.with_df
         raise NotImplementedError
 
-    s1 = mf_grad.get_ovlp(mol, kpt)
+    if cell._ecp:
+        raise NotImplementedError('ECP Gradients with PBC')
+
+    s1 = mf_grad.get_ovlp(cell, kpt)
     dm0 = mf.make_rdm1(mo_coeff, mo_occ)
 
     t0 = (logger.process_clock(), logger.perf_counter())
     log.debug('Computing Gradients of NR-HF Coulomb repulsion')
-    vhf = mf_grad.get_veff(mol, dm0, kpt)
+    vhf = mf_grad.get_veff(cell, dm0, kpt)
     log.timer('gradients of 2e part', *t0)
 
     dme0 = mf_grad.make_rdm1e(mo_energy, mo_coeff, mo_occ)
@@ -55,14 +58,14 @@ def grad_elec(mf_grad, mo_energy=None, mo_coeff=None, mo_occ=None, atmlst=None, 
     dme0_sf = dme0[0] + dme0[1]
 
     if atmlst is None:
-        atmlst = range(mol.natm)
+        atmlst = range(cell.natm)
 
     if gamma_point(kpt):
-        h1ao = -mol.pbc_intor('int1e_ipkin', kpt=kpt)
-        if mol._pseudo:
+        h1ao = -cell.pbc_intor('int1e_ipkin', kpt=kpt)
+        if cell._pseudo:
             de  = ni.vpploc_part1_nuc_grad(dm0_sf, kpts=kpt.reshape(-1,3))
-            de += pp_int.vpploc_part2_nuc_grad(mol, dm0_sf)
-            de += pp_int.vppnl_nuc_grad(mol, dm0_sf)
+            de += pp_int.vpploc_part2_nuc_grad(cell, dm0_sf)
+            de += pp_int.vppnl_nuc_grad(cell, dm0_sf)
             if hasattr(ni, 'vpplocG_part1'):
                 if ni.vpplocG_part1 is None:
                     h1ao -= ni.get_vpploc_part1_ip1(kpts=kpt.reshape(-1,3))
@@ -83,10 +86,10 @@ def grad_elec(mf_grad, mo_energy=None, mo_coeff=None, mo_occ=None, atmlst=None, 
 
     if log.verbose >= logger.DEBUG:
         log.debug('gradients of electronic part')
-        _write(log, mol, de, atmlst)
+        _write(log, cell, de, atmlst)
     return de
 
-def get_veff(mf_grad, mol, dm, kpt=np.zeros(3)):
+def get_veff(mf_grad, cell, dm, kpt=np.zeros(3)):
     mf = mf_grad.base
     xc_code = getattr(mf, 'xc', None)
     kpts = kpt.reshape(-1,3)
@@ -94,10 +97,10 @@ def get_veff(mf_grad, mol, dm, kpt=np.zeros(3)):
 
 class Gradients(rhf_grad.GradientsBase):
     '''Non-relativistic Gamma-point restricted Hartree-Fock gradients'''
-    def get_veff(self, mol=None, dm=None, kpt=np.zeros(3)):
-        if mol is None: mol = self.mol
+    def get_veff(self, cell=None, dm=None, kpt=np.zeros(3)):
+        if cell is None: cell = self.cell
         if dm is None: dm = self.base.make_rdm1()
-        return get_veff(self, mol, dm, kpt)
+        return get_veff(self, cell, dm, kpt)
 
     make_rdm1e = mol_uhf.Gradients.make_rdm1e
     grad_elec = grad_elec