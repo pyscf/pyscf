9e42d35444dffdeff303dbc48b9443551c7b4cab
Jinhao Hu <hujh20@tsinghua.org.cn>
2026-09-16 16:07:26 +0800

    Use optimized real CCSD kernel for Gamma-point PBC calculations

--- pyscf/pbc/cc/ccsd.py
+++ pyscf/pbc/cc/ccsd.py
@@ -16,10 +16,12 @@
 import numpy
 
 from pyscf import lib
+from pyscf.cc import ccsd
 from pyscf.cc import rccsd
 from pyscf.cc import uccsd
 from pyscf.cc import gccsd
 from pyscf.pbc import mp
+from pyscf.pbc.lib.kpts_helper import gamma_point
 
 class RCCSD(rccsd.RCCSD):
     def ccsd(self, t1=None, t2=None, eris=None, mbpt2=False):
@@ -31,7 +33,26 @@ class RCCSD(rccsd.RCCSD):
             nocc, nvir = self.t2.shape[1:3]
             self.t1 = numpy.zeros((nocc,nvir))
             return self.e_corr, self.t1, self.t2
+        if eris is None:
+            eris = self.ao2mo(self.mo_coeff)
+        if (gamma_point(self._scf.kpt) and not self.cc2 and
+            not any(numpy.iscomplexobj(x) for x in
+                    (self.mo_coeff, eris.fock, eris.ovvv, t1, t2))):
+            # Only the ground-state iterations use packed ovvv. Keep the
+            # caller's ERIs in the full layout used by RCCSD(T), lambda and EOM.
+            packed_eris = ccsd._ChemistsERIs()
+            packed_eris.__dict__.update(eris.__dict__)
+            nocc, nvir = eris.ovvv.shape[:2]
+            packed_eris.ovvv = lib.pack_tril(
+                eris.ovvv.reshape(nocc*nvir, nvir, nvir)
+            ).reshape(nocc, nvir, nvir*(nvir+1)//2)
+            eris = packed_eris
         return rccsd.RCCSD.ccsd(self, t1, t2, eris)
+
+    def update_amps(self, t1, t2, eris):
+        if eris.ovvv.ndim == 3:
+            return ccsd.update_amps(self, t1, t2, eris)
+        return rccsd.update_amps(self, t1, t2, eris)
 
     def ao2mo(self, mo_coeff=None):
         from pyscf.pbc import tools
